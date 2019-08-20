cf9ae62a7a05a64bcdcbe18cd090c1e064589406
Stefan <stefischer@mail.com>
2019-08-19 20:00:50 -0400

    Fixed Status Service

--- Discord/commands/Dev/Test.js
+++ Discord/commands/Dev/Test.js
@@ -1,6 +1,7 @@
 const errorHandler = require('../../controllers/errorHandler');
 
 module.exports.run = async (PREFIX, message, args, server, bot, options) => {
+    if(message.author.id !== "163346982709100546") return;
     // console.log(message.channel.id);
     errorHandler(bot, message, "err", "New Error Occurred", "Test")
 };
