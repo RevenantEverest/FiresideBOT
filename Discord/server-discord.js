c060f8f2136185bb3951254dd2b8f56e6f066313
Stefan <stefischer@mail.com>
2019-11-10 16:53:12 -0500

    Added Memwatch

--- Discord/server-discord.js
+++ Discord/server-discord.js
@@ -1,7 +1,24 @@
 require('dotenv').config();
 
+const Discord = require('discord.js');
+const memwatch = require('node-memwatch');
 const Discord_Bot = require('./Discord_Bot');
+const utils = require('./commands/utils/utils');
 
 Discord_Bot.login(process.env.DISCORD_KEY);
+
+memwatch.on('leak', async (info) => {
+    let embed = new Discord.RichEmbed();
+    embed
+    .setColor(0xcc0000)
+    .addField(
+        '⚠️ Memory Leak Detected ⚠️', 
+        `A memory leak has been detected in the **Discord** Service\n\n` + 
+        `**Reason:** ${info.reason}\n\n` +
+        `**Growth:** ${info.growth}`
+    )
+    .setFooter(await utils.getDate())
+    Discord_Bot.channels.get("543862697742172179").send(embed);
+});
 
 module.exports = require('./app');
