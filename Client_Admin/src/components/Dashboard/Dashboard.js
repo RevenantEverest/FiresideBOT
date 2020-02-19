50d15a9a06805d5cdda283a4bc0e5a1d97291312
Stefan <stefischer@mail.com>
2020-02-19 07:07:54 -0500

    Update Dashboard for SideNav

--- Client_Admin/src/components/Dashboard/Dashboard.js
+++ Client_Admin/src/components/Dashboard/Dashboard.js
@@ -1,80 +1,56 @@
 import React, { Component } from 'react';
 import './Dashboard.css';
 
-import { Container, Row, Col } from 'react-bootstrap';
-import {
-    MDBCard,
-    MDBCardBody, 
-    MDBBreadcrumb,
-    MDBBreadcrumbItem
-} from 'mdbreact';
+import { Link } from 'react-router-dom';
+import { Container, Row, Col, Spinner } from 'react-bootstrap';
 
-import WeeklyWrapUp from '../Sections/WeeklyWrapUp/WeeklyWrapUp';
-import TopCommandsToday from '../DataSets/TopCommandsToday/TopCommandsToday';
-import TopCommandsMonth from '../DataSets/TopCommandsMonth/TopCommandsMonth';
-import CommandsOvertime from '../DataSets/CommandsOvertime/CommandsOvertime';
+import WeeklyWrapUp from '../WeeklyWrapUp/WeeklyWrapUp';
 
-import TopGuildsToday from '../DataSets/TopGuildsToday/TopGuildsToday';
-import TopGuildsMonth from '../DataSets/TopGuildsMonth/TopGuildsMonth';
-
-
+import commandLogServices from '../../services/commandLogServices';
 
 class Dashboard extends Component {
 
+    _isMounted = false;
+
     constructor(props) {
         super(props);
-        this.state = {
-            userData: this.props.userData,
-            logs: this.props.logs
-        }
+        this.state = {}
+    }
+
+    componentDidMount() {
+        this._isMounted = true;
+        this.getLogs();
+    }
+
+    componentWillUnmount = () => this._isMounted = false;
+
+    getLogs() {
+        if(!this._isMounted) return;
+        commandLogServices.getCommandLogs()
+        .then(logs => this.setState({ logs: logs.data.data, dataReceived: true }))
+        .catch(err => console.error(err));
     }
 
     render() {
         return(
-            <div id="Dashboard" style={{ marginTop: "5%", marginBottom: "5%" }}>
-            <Container fluid>
-                <Row className="justify-content-md-center" style={{ marginBottom: "2%" }}>
-                    <Col md={9}>
-                        <MDBCard style={{ background: "#0c0c0c" }}>
-                            <MDBCardBody className="d-flex align-items-center justify-content-between" style={{ color: "inherit", margin: "0" }}>
-                                <MDBBreadcrumb style={{ background: "#151515" }}>
-                                    <MDBBreadcrumbItem>Home</MDBBreadcrumbItem>
-                                    <MDBBreadcrumbItem active>Dashboard</MDBBreadcrumbItem>
-                                </MDBBreadcrumb>
-                            </MDBCardBody>
-                        </MDBCard>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <WeeklyWrapUp logs={this.props.logs} />
-                    </Col>
-                </Row>
-
-                {/* Commands */}
-                <Row className="justify-content-md-center" style={{ marginBottom: "2%" }}>
-                    <Col md={5}>
-                        <TopCommandsToday logs={this.props.logs} />
-                    </Col>
-                </Row>
-                <Row className="justify-content-md-center" style={{ marginBottom: "2%" }}>
-                    <Col md={5}>
-                        <TopCommandsMonth logs={this.props.logs} />
-                    </Col>
-                    <Col md={5}>
-                        <CommandsOvertime logs={this.props.logs} />
-                    </Col>
-                </Row>
-
-                {/* Guilds */}
-                <Row className="justify-content-md-center" style={{ marginBottom: "2%" }}>
-                    <Col md={5}>
-                        <TopGuildsToday logs={this.props.logs} />
-                    </Col>
-                    <Col md={5}>
-                        <TopGuildsMonth logs={this.props.logs} />
-                    </Col>
-                </Row>
+            <div id="Dashboard" style={{ marginBottom: "5%" }}>
+            <Container>
+            <Row>
+                <Col>
+                    <h1 className="Component-Header">Dashboard</h1>
+                </Col>
+            </Row>
+            <Row style={{ marginBottom: "5%" }}>
+                <Col>
+                    <Link to="/"><p className="Component-Breadcrumb">Home </p></Link>
+                    <p className="Component-Breadcrumb Component-Breadcrumb-Main">/ Dashboard</p>
+                </Col>
+            </Row>
+            <Row>
+                <Col style={{ paddingLeft: 0, paddingRight: 0 }}>
+                {this.state.dataReceived ? <WeeklyWrapUp logs={this.state.logs} /> : <Spinner animation="border" role="status" style={{ display: "inline-block" }}><span className="sr-only">Loading...</span></Spinner>}
+                </Col>
+            </Row>
             </Container>
             </div>
         )
---
+++ Client_Admin/src/components/DashboardFooter/DashboardFooter.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+import './DashboardFooter.css';
+
+import { Link, withRouter } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Container, Row, Col } from 'react-bootstrap';
+import { MDBFooter } from 'mdbreact';
+
+class DashboardFooter extends Component {
+
+    render() {
+        return(
+            <div id="DashboardFooter">
+                <MDBFooter style={{ background: "#060606" }} className="font-small pt-4 mt-4">
+                <Container fluid className="text-center text-md-left" style={{ marginBottom: "2%" }}>
+                    <Row>
+                    <Col md={6} className="Footer-Col__Sec1">
+                        <img src="https://i.imgur.com/KR9xQdZ.png" alt="" className="Footer-Logo" />
+                        <h5 className="title Footer-Logo-Text">FiresideBOT</h5>
+                        <Row style={{ marginTop: "2%" }}>
+                        <Col className="Footer-Col" style={{ marginBottom: "2%" }}>
+                            <a target="_blank" rel="noopener noreferrer" href="https://discord.gg/TqKHVUa">
+                                <FontAwesomeIcon className="Footer-Icon" icon={['fab', 'discord']} />
+                            </a>
+                            <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/RevenantEeverest/">
+                            <FontAwesomeIcon className="Footer-Icon" icon={['fab', 'facebook']} />
+                            </a>
+                            <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/RevenantEverest">
+                            <FontAwesomeIcon className="Footer-Icon" icon={['fab', 'twitter']} />
+                            </a>
+                            <a target="_blank" rel="noopener noreferrer" href="https://github.com/RevenantEverest/FiresideBOT">
+                            <FontAwesomeIcon className="Footer-Icon" icon={['fab', 'github']} />
+                            </a>
+                        </Col>
+                        </Row>
+                        <Row>
+                            <Col className="Footer-Col">
+                                <p>Enjoying</p><p className="Footer-Fireside"> FiresideBOT</p><p>? Give us a vote over at </p>
+                                <a id="Footer-DBL-Link" target="_blank" rel="noopener noreferrer" href="https://discordbots.org/bot/441338104545017878"> DBL</a> 
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col lg={2}>
+                            <a className="Footer-DBL" href="https://discordbots.org/bot/441338104545017878" >
+                            <img src="https://discordbots.org/api/widget/status/441338104545017878.svg?noavatar=true" alt="FiresideBOT" />
+                            </a>
+                            </Col>
+                            <Col lg={2}>
+                            <a className="Footer-DBL" href="https://discordbots.org/bot/441338104545017878" >
+                            <img src="https://discordbots.org/api/widget/servers/441338104545017878.svg?noavatar=true" alt="FiresideBOT" />
+                            </a>
+                            </Col>
+                            <Col lg={2}>
+                            <a className="Footer-DBL" href="https://discordbots.org/bot/441338104545017878" >
+                            <img src="https://discordbots.org/api/widget/upvotes/441338104545017878.svg?noavatar=true" alt="FiresideBOT" />
+                            </a>
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col md={2} style={{ marginBottom: "2%" }}>
+                        <h5 className="title Footer-UL-Title__Contact">Contact</h5>
+                        <ul className="Footer-UL" style={{ paddingLeft: "0" }}>
+                        <li className="list-unstyled Footer-LI">
+                            <p>support@firesidebot.com</p>
+                        </li>
+                        <li className="list-unstyled Footer-LI">
+                            <p>1 (800) 555-5555</p>
+                        </li>
+                        </ul>
+                    </Col>
+                    <Col md={2} style={{ marginBottom: "2%" }}>
+                        <h5 className="title Footer-UL-Title__UsefulLinks">Useful Links</h5>
+                        <ul className="Footer-UL" style={{ paddingLeft: "0" }}>
+                        <li className="list-unstyled Footer-LI">
+                            <Link to="/faq" className="Footer-Link">Frequently Asked Questions</Link>
+                        </li>
+                        <li className="list-unstyled Footer-LI">
+                            <Link to="/premium" className="Footer-Link">Premium</Link>
+                        </li>
+                        <li className="list-unstyled Footer-LI">
+                            <Link to="/changelogs" className="Footer-Link">Changelogs</Link>
+                        </li>
+                        </ul>
+                    </Col>
+                    <Col md={2} style={{ marginBottom: "2%" }}>
+                        <h5 className="title Footer-UL-Title__UsefulLinks">Resources</h5>
+                        <ul className="Footer-UL" style={{ paddingLeft: "0" }}>
+                        <li className="list-unstyled Footer-LI">
+                            <a className="Footer-Link" target="_blank" rel="noopener noreferrer" href="https://help.firesidebot.com">Support</a>
+                        </li>
+                        <li className="list-unstyled Footer-LI">
+                            <a className="Footer-Link" target="_blank" rel="noopener noreferrer" href="https://help.firesidebot.com">Documentation</a>
+                        </li>
+                        </ul>
+                    </Col>
+                    </Row>
+                </Container>
+                <div className="footer-copyright text-center py-3" style={{ background: "#0a0a0a" }}>
+                    <Container fluid>
+                    &copy; {new Date().getFullYear()} Copyright: <a href="https://firesidebot.com"> FiresideBOT.com </a>
+                    </Container>
+                </div>
+                </MDBFooter>
+            </div>
+        );
+    }
+};
+
+export default withRouter(DashboardFooter);
