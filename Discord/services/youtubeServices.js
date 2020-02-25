4c9f010beaef12f24726d2f16bae7801f1eaba1b
Stefan <stefischer@mail.com>
2020-02-25 10:03:21 -0500

    Update youtubeServices.js

--- Discord/services/youtubeServices.js
+++ Discord/services/youtubeServices.js
@@ -6,7 +6,7 @@ const services = {};
 
 
 services.youtubeSearch = (data) => {
-  return axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&order=relevance&q=${data}&type=video&key=${process.env.GOOGLE_KEY}`)
-}
+  return axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&order=relevance&q=${data}&type=video&key=${process.env.GOOGLE_KEY}`);
+};
 
 module.exports = services;
