deec83e3ae899fc602ff13122802da5cc8997524
Stefan <stefischer@mail.com>
2020-03-26 22:28:41 -0400

    Moved Update Guilds to controller

--- Background/controllers/DiscordEvents/Guild/onGuildUpdate.js
+++ Background/controllers/DiscordEvents/Guild/onGuildUpdate.js
@@ -1,23 +1,8 @@
 const Discord = require('discord.js');
 const moment = require('moment');
+const guildsController = require('../../guildsController');
 
 module.exports = async (bot, oldGuild, newGuild) => {
     if(oldGuild.name !== newGuild.name)
-        guildsDB.update({ guild_id: newGuild.id, name: newGuild.name })
-        .then(() => {
-            let embed = new Discord.RichEmbed();
-
-            embed
-            .setTitle(`**Guild Updated**`)
-            .addBlankField()
-            .setColor(0xff9900)
-            .addField('Old Name:', oldGuild.name, true)
-            .addField('New Name:', newGuild.name, true)
-            .addField('ID:', newGuild.id, true)
-            .addField('Member Count:', parseInt(newGuild.memberCount, 10).toLocaleString())
-            .setFooter(`Guild Updated: ${moment().format("LLLL") + " EST"}`)
-
-            if(process.env.ENVIRONMENT !== "DEV") bot.channels.get('538528459190829064').send(embed);
-        })
-        .catch(err => console.error(err));
+        guildsController.updateGuild(bot, oldGuild, newGuild)
 };
--- Background/controllers/guildsController.js
+++ Background/controllers/guildsController.js
@@ -67,4 +67,24 @@ services.saveGuild = async (bot, guild) => {
     .catch(err => console.error(err));
 };
 
+services.updateGuild = async (bot, oldGuild, newGuild) => {
+    guildsDB.update({ guild_id: newGuild.id, name: newGuild.name })
+    .then(() => {
+        let embed = new Discord.RichEmbed();
+
+        embed
+        .setTitle(`**Guild Updated**`)
+        .addBlankField()
+        .setColor(0xff9900)
+        .addField('Old Name:', oldGuild.name, true)
+        .addField('New Name:', newGuild.name, true)
+        .addField('ID:', newGuild.id, true)
+        .addField('Member Count:', parseInt(newGuild.memberCount, 10).toLocaleString())
+        .setFooter(`Guild Updated: ${moment().format("LLLL") + " EST"}`)
+
+        if(process.env.ENVIRONMENT !== "DEV") bot.channels.get('538528459190829064').send(embed);
+    })
+    .catch(err => console.error(err));
+};
+
 module.exports = services;
