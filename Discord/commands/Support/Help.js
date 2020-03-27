69796a725dffc3d1913fd3ce56206a28c8743712
Stefan <stefischer@mail.com>
2020-03-26 23:53:49 -0400

    Updated to use reuseable functions and allow for category and command help search

--- Discord/commands/Support/Help.js
+++ Discord/commands/Support/Help.js
@@ -7,48 +7,60 @@ const pgp = require('pg-promise')();
 const QRE = pgp.errors.QueryResultError;
 const qrec = pgp.errors.queryResultErrorCode;
 
-function helpSpec(PREFIX, message, args, bot, contentArr) {
-    let embed = new Discord.RichEmbed();
-    embed.setColor(0xcc00ff);
-    let flavorText = "`<param>` is a required param and `[param]` is an optional param. For more information on a command use `" + PREFIX + "help <command>` ™";
+async function parseCategoryFilter(categories, commands) {
+    let counter = 0;
+    let temp = [];
+    let commandsTemp = [];
+    let categoryFilter = categories.map(el => {
+        return { name: el.name, desc: el.desc, commands: commands.filter(command => command.config.category === el.name) }
+    });
 
-    if(contentArr[contentArr.map(e => e.category[0].toLowerCase()).indexOf(args[1].toLowerCase())]) {
-      let helpInfo = contentArr[contentArr.map(e => e.category[0].toLowerCase()).indexOf(args[1].toLowerCase())];
-      embed.addField(`**${helpInfo.category[0]}**`, `${helpInfo.category[1]}`).addBlankField();
-      helpInfo.fields.forEach(el => embed.addField(`${el.field}`, `${el.value}`, el.inline));
-    } 
-    else if(bot.commands.get(args[1].toLowerCase()) || bot.commands.get(bot.aliases.get(args[1].toLowerCase()))) {
-        let helpInfo = bot.commands.get(bot.aliases.get(args[1].toLowerCase())) || bot.commands.get(args[1].toLowerCase());
-        
-        /* Variable redefinition required for aliases to apply */
-        helpInfo = helpInfo.config;
-
-        embed
-        .addField(`**${helpInfo.d_name}** - ${helpInfo.category} ${helpInfo.params ? (helpInfo.params.required ? '`<param>`' : '`[param]`') : ''}`, `${helpInfo.desc}`)
-        .addBlankField()
-        
-        if(helpInfo.params) embed.addField('Params: ', helpInfo.params.params, true)
-
-        if(helpInfo.aliases.length > 0) {
-        let aliasText = '';
-        helpInfo.aliases.forEach(el => aliasText += "`" + el + "` ");
-        embed.addField('Aliases:', aliasText, true)
+    categoryFilter.forEach(el => {
+        if((el.commands.length - 1) <= 5) {
+            temp.push({ name: el.name, desc: el.desc, commands: el.commands });
         }
+        else el.commands.forEach((command, idx) => {
+            if(idx % 5 === 0 && idx !== 0) {
+                counter ++;
+                let name = `${el.name} ${counter}/${Math.ceil(el.commands.length / 5)}`;
+                temp.push({name: name, desc: el.desc, commands: commandsTemp });
+                commandsTemp = [];
+            } 
+            else  commandsTemp.push(command);
 
-        if(helpInfo.flags) {
-        let flagText = '';
-        helpInfo.flags.forEach(el => flagText += "`" + el + "` ");
-        embed.addField('Flags:', flagText, true)
-        } 
+            if(idx === (el.commands.length - 1)) {
+                counter ++;
+                let name = `${el.name} ${counter}/${Math.ceil(el.commands.length / 5)}`;
+                temp.push({name: name, desc: el.desc, commands: commandsTemp });
+                commandsTemp = [];
+                counter = 0;
+            }
+        });
+    });
 
-        embed
-        .addField('Example:', "`" + PREFIX + helpInfo.example + "`")
-        .addField(`More Info:`, `[Click Here](https://help.firesidebot.com)`)
-    }
-    else
-      return message.channel.send('Invalid Command or Category');
+    return temp;
+};
 
-    message.channel.send(flavorText, embed);
+async function parseContentArr(contentArr, categoryFilter, dCommands) {
+    let category = [];
+    let fields = [];
+    contentArr = [];
+    categoryFilter.forEach(el => {
+        category = [`**${el.name}**`, el.desc];
+        el.commands.forEach(command => {
+            command = command.config;
+            params = command.params;
+            fields.push({
+                field: `**${command.d_name}** ${params ? (params.required ? '`<param>`' : '`[param]`') : ""} ${dCommands.includes(command.name) ? "<:Cross:624336361633218580>*Disabled*" : ""}`,
+                value: command.desc === "" ? "*N/A*" : command.desc,
+                inline: false
+            });
+        });
+        contentArr.push({ category: category, fields: fields });
+        fields = [];
+    });
+
+    return contentArr;
 };
 
 module.exports.run = async (PREFIX, message, args, server, bot, options, userstate) => {
@@ -85,57 +97,73 @@ module.exports.run = async (PREFIX, message, args, server, bot, options, usersta
         else console.error(err);
     });
 
-    categories = categories.map(el => {
-        return { name: el.name, desc: el.desc, commands: commands.filter(command => command.config.category === el.name) }
-    });
+    let categoryFilter = await parseCategoryFilter(categories, commands);
 
-    let counter = 0;
-    let temp = [];
-    let commandsTemp = [];
-    categories.forEach(el => {
-        if((el.commands.length - 1) <= 5) {
-            temp.push({ name: el.name, desc: el.desc, commands: el.commands });
+    contentArr = await parseContentArr(contentArr, categoryFilter, dCommands);
+
+    if(!args[1]) 
+        return pagination(message, bot, contentArr, { flavorText: flavorText, color: 0xcc00ff });
+    else if(categories.filter(el => el.name.toLowerCase() === args[1].toLowerCase()).length > 0) 
+        return handleCategory();
+    else if(bot.commands.get(args[1].toLowerCase()) || bot.commands.get(bot.aliases.get(args[1].toLowerCase()))) 
+        return handleCommand();
+
+    async function handleCommand() {
+        let command = bot.commands.get(args[1].toLowerCase()).config || bot.commands.get(bot.aliases.get(args[1].toLowerCase())).config;
+        let embed = new Discord.RichEmbed();
+
+        embed
+        .addField(`**${command.d_name}** - ${command.category} ${command.params ? (command.params.required ? '`<param>`' : '`[param]`') : ''}`, `${command.desc}`)
+        .addBlankField()
+        
+        if(command.params) embed.addField('Params: ', command.params.params, true)
+
+        if(command.aliases.length > 0) {
+            let aliasText = '';
+            command.aliases.forEach(el => aliasText += "`" + el + "` ");
+            embed.addField('Aliases:', aliasText, true)
         }
-        else el.commands.forEach((command, idx) => {
-            if(idx % 5 === 0 && idx !== 0 || idx === (el.commands.length - 1)) {
-                counter ++;
-                let name = `${el.name} ${counter}/${Math.ceil(el.commands.length / 5)}`;
-                temp.push({name: name, desc: el.desc, commands: commandsTemp });
-                commandsTemp = [];
-            } 
-            else  commandsTemp.push(command);
 
-            if(idx === (el.commands.length - 1)) {
-                counter ++;
-                let name = `${el.name} ${counter}/${Math.ceil(el.commands.length / 5)}`;
-                temp.push({name: name, desc: el.desc, commands: commandsTemp });
-                commandsTemp = [];
-                counter = 0;
-            }
+        if(command.flags) {
+            let flagText = '';
+            command.flags.forEach(el => flagText += "`" + el + "` ");
+            embed.addField('Flags:', flagText, true)
+        } 
+
+        embed
+        .addField('Example:', "`" + PREFIX + command.example + "`")
+        .addField(`More Info:`, `[Click Here](https://help.firesidebot.com)`)
+        .setColor(0xcc00ff);
+
+        return message.channel.send(flavorText, embed);
+    };
+
+    async function handleCategory() {
+        let category = categories.filter(el => el.name.toLowerCase() === args[1].toLowerCase());
+        let embed = new Discord.RichEmbed();
+        if(commands.filter(el => el.config.category === category[0].name).length > 5) {
+            contentArr = [];
+            categoryFilter = await parseCategoryFilter(category, commands);
+            contentArr = await parseContentArr(contentArr, categoryFilter, dCommands);
+
+            return pagination(message, bot, contentArr, { flavorText: flavorText, color: 0xcc00ff });
+        }
+
+        embed
+        .addField(`**${category[0].name}**`, `${category[0].desc}`)
+        .addBlankField()
+        .setColor(0xcc00ff);
+
+        commands.filter(el => el.config.category === category[0].name).forEach(el => {
+            let command = el.config;
+            embed.addField(
+                `**${command.d_name}** ${params ? (command.params.required ? '`<param>`' : '`[param]`') : ""} ${dCommands.includes(command.name) ? "<:Cross:624336361633218580>*Disabled*" : ""}`,
+                command.desc === "" ? "*N/A*" : command.desc
+            );
         });
-    });
 
-    categories = temp;
-
-    let category = [];
-    let fields = [];
-    categories.forEach((el, idx) => {
-        category = [`**${el.name}**`, el.desc];
-        el.commands.forEach(command => {
-            command = command.config;
-            params = command.params;
-            fields.push({
-                field: `**${command.d_name}** ${params ? (params.required ? '`<param>`' : '`[param]`') : ""} ${dCommands.includes(command.name) ? "<:Cross:624336361633218580>*Disabled*" : ""}`,
-                value: command.desc === "" ? "*N/A*" : command.desc,
-                inline: false
-            });
-        });
-        contentArr.push({ category: category, fields: fields });
-        fields = [];
-    });
-
-    if(!args[1]) pagination(message, bot, contentArr, { flavorText: flavorText, color: 0xcc00ff });
-    else helpSpec(PREFIX, message, args, bot, contentArr);
+        return message.channel.send(flavorText, embed);
+    };
 };
 
 module.exports.config = {
