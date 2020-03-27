f0911c426fb6e4e90f3817a9d52d9efcf8cfc78b
Stefan <stefischer@mail.com>
2020-03-26 21:34:45 -0400

    Updated Help to separate categories into separate pages

--- Discord/commands/Support/Help.js
+++ Discord/commands/Support/Help.js
@@ -18,34 +18,32 @@ function helpSpec(PREFIX, message, args, bot, contentArr) {
       helpInfo.fields.forEach(el => embed.addField(`${el.field}`, `${el.value}`, el.inline));
     } 
     else if(bot.commands.get(args[1].toLowerCase()) || bot.commands.get(bot.aliases.get(args[1].toLowerCase()))) {
-      let helpInfo = bot.commands.get(bot.aliases.get(args[1].toLowerCase())) || bot.commands.get(args[1].toLowerCase());
-      
-      /* Variable redefinition required for aliases to apply */
-      helpInfo = helpInfo.config;
+        let helpInfo = bot.commands.get(bot.aliases.get(args[1].toLowerCase())) || bot.commands.get(args[1].toLowerCase());
+        
+        /* Variable redefinition required for aliases to apply */
+        helpInfo = helpInfo.config;
 
-      embed
-      .addField(`**${helpInfo.d_name}** - ${helpInfo.category} ${helpInfo.params ? (helpInfo.params.required ? '`<param>`' : '`[param]`') : ''}`, `${helpInfo.desc}`)
-      .addBlankField()
-      
-      if(helpInfo.params)
         embed
-        .addField('Params: ', helpInfo.params.params, true)
+        .addField(`**${helpInfo.d_name}** - ${helpInfo.category} ${helpInfo.params ? (helpInfo.params.required ? '`<param>`' : '`[param]`') : ''}`, `${helpInfo.desc}`)
+        .addBlankField()
+        
+        if(helpInfo.params) embed.addField('Params: ', helpInfo.params.params, true)
 
-      if(helpInfo.aliases.length > 0) {
+        if(helpInfo.aliases.length > 0) {
         let aliasText = '';
         helpInfo.aliases.forEach(el => aliasText += "`" + el + "` ");
         embed.addField('Aliases:', aliasText, true)
-      }
+        }
 
-      if(helpInfo.flags) {
+        if(helpInfo.flags) {
         let flagText = '';
         helpInfo.flags.forEach(el => flagText += "`" + el + "` ");
         embed.addField('Flags:', flagText, true)
-      } 
+        } 
 
-      embed
-      .addField('Example:', "`" + PREFIX + helpInfo.example + "`")
-      .addField(`More Info:`, `[Click Here](https://help.firesidebot.com)`)
+        embed
+        .addField('Example:', "`" + PREFIX + helpInfo.example + "`")
+        .addField(`More Info:`, `[Click Here](https://help.firesidebot.com)`)
     }
     else
       return message.channel.send('Invalid Command or Category');
@@ -54,7 +52,8 @@ function helpSpec(PREFIX, message, args, bot, contentArr) {
 };
 
 module.exports.run = async (PREFIX, message, args, server, bot, options, userstate) => {
-    let categories = config.categories.filter(el => el.name !== "DEV");
+    let categories = config.categories.filter(el => el.name !== "Dev");
+    let flavorText = "`<param>` is a required param and `[param]` is an optional param. For more information on a command use `" + PREFIX + "help <command>` ™";
     let contentArr = [
       {
         category: [`Welcome to the FiresideBOT Help Command`, 'Need immediate help? Message Fireside to open a ticket'],
@@ -86,22 +85,57 @@ module.exports.run = async (PREFIX, message, args, server, bot, options, usersta
         else console.error(err);
     });
 
-    categories.forEach(el => contentArr.push({ category: [el.name, el.desc], fields: [] }));
-    commands.forEach(el => {
-      if(el.config.category && contentArr[contentArr.map(e => e.category[0]).indexOf(el.config.category)])
-        contentArr[contentArr.map(e => e.category[0]).indexOf(el.config.category)].fields.push({
-          field: `${el.config.d_name}${el.config.params ? (el.config.params.required ? '` <param> `' : '` [param] `') : ''}${dCommands.includes(el.config.name) ? '<:Cross:624336361633218580>*Disabled*' : ''}`, 
-          value: (el.config.desc === '' ? '*N/A*' : el.config.desc),
-          inline: false
+    categories = categories.map(el => {
+        return { name: el.name, desc: el.desc, commands: commands.filter(command => command.config.category === el.name) }
+    });
+
+    let counter = 0;
+    let temp = [];
+    let commandsTemp = [];
+    categories.forEach(el => {
+        if((el.commands.length - 1) <= 5) {
+            temp.push({ name: el.name, desc: el.desc, commands: el.commands });
+        }
+        else el.commands.forEach((command, idx) => {
+            if(idx % 5 === 0 && idx !== 0 || idx === (el.commands.length - 1)) {
+                counter ++;
+                let name = `${el.name} ${counter}/${Math.ceil(el.commands.length / 5)}`;
+                temp.push({name: name, desc: el.desc, commands: commandsTemp });
+                commandsTemp = [];
+            } 
+            else  commandsTemp.push(command);
+
+            if(idx === (el.commands.length - 1)) {
+                counter ++;
+                let name = `${el.name} ${counter}/${Math.ceil(el.commands.length / 5)}`;
+                temp.push({name: name, desc: el.desc, commands: commandsTemp });
+                commandsTemp = [];
+                counter = 0;
+            }
         });
     });
 
-    let flavorText = "`<param>` is a required param and `[param]` is an optional param. For more information on a command use `" + PREFIX + "help <command>` ™";
+    categories = temp;
 
-    if(!args[1])
-        pagination(message, bot, contentArr, { flavorText: flavorText, color: 0xcc00ff });
-    else 
-        helpSpec(PREFIX, message, args, bot, contentArr);
+    let category = [];
+    let fields = [];
+    categories.forEach((el, idx) => {
+        category = [`**${el.name}**`, el.desc];
+        el.commands.forEach(command => {
+            command = command.config;
+            params = command.params;
+            fields.push({
+                field: `**${command.d_name}** ${params ? (params.required ? '`<param>`' : '`[param]`') : ""} ${dCommands.includes(command.name) ? "<:Cross:624336361633218580>*Disabled*" : ""}`,
+                value: command.desc === "" ? "*N/A*" : command.desc,
+                inline: false
+            });
+        });
+        contentArr.push({ category: category, fields: fields });
+        fields = [];
+    });
+
+    if(!args[1]) pagination(message, bot, contentArr, { flavorText: flavorText, color: 0xcc00ff });
+    else helpSpec(PREFIX, message, args, bot, contentArr);
 };
 
 module.exports.config = {
