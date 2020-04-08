ba5ac6ae5b06b5c51f404773a1fa54645e50faad
Stefan <stefischer@mail.com>
2020-04-08 06:59:42 -0400

    Fixed indentation and unused variable

--- TwitchTracker/controllers/flavorTextParser.js
+++ TwitchTracker/controllers/flavorTextParser.js
@@ -3,20 +3,14 @@ module.exports =async (flavorText, trackerData, streamer) => {
         let variables = flavorText.match(/\${([^}]*)}/g);
 
         variables.forEach(async (el, idx) => {
-                let exec = /\${([^}]*)}/.exec(el)[1];
+            let exec = /\${([^}]*)}/.exec(el)[1];
 
-                if(exec.toLowerCase() === "streamer") flavorText = flavorText.replace("${" + exec + "}", streamer);
+            if(exec.toLowerCase() === "streamer") flavorText = flavorText.replace("${" + exec + "}", streamer);
 
             if(exec.toLowerCase() === "role" && trackerData.role_id !== "none") 
-                    flavorText = flavorText.replace("${" + exec + "}", `<@&${role_id}>`);
-                else if(exec.toLowerCase() === "role" && trackerData.role_id === "none")
-                    flavorText = flavorText.replace("${" + exec + "}", "");
-
-                if(exec.includes(",")) {
-                    let choices = exec.split(",");
-                    let randomResponse = choices[Math.floor(Math.random() * choices.length)];
-                    flavorText = flavorText.replace(/\${([^}]*)}/, randomResponse);
-                }
+                flavorText = flavorText.replace("${" + exec + "}", `<@&${role_id}>`);
+            else if(exec.toLowerCase() === "role" && trackerData.role_id === "none")
+                flavorText = flavorText.replace("${" + exec + "}", "");
         });
 
         return flavorText;
