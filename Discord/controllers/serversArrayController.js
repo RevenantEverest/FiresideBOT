a220f4a0b357be8ff37207ce2b0b97a81df3400c
Stefan <stefischer@mail.com>
2020-10-02 18:34:34 -0400

    Fixed failed endpoint call when guild is removed

--- Discord/controllers/serversArrayController.js
+++ Discord/controllers/serversArrayController.js
@@ -5,25 +5,27 @@ module.exports = {
     index(req, res, next) {
         let servers = config.servers.map(el => {
             let guild = bot.guilds.resolve(el.id);
-            return {
-                name: guild.name,
-                icon: guild.iconURL(),
-                memberCount: guild.memberCount,
-                id: el.id,
-                queue: {
-                    isPlaying: el.queue.isPlaying,
-                    isPaused: el.queue.isPaused,
-                    queueInfo: el.queue.queueInfo,
-                    currentSongInfo: el.queue.currentSongInfo,
-                    options: {
-                        volume: el.queue.options.volume,
-                        loop: el.queue.options.loop,
-                        recommendations: el.queue.options.recommendations,
-                        voteToSkip: el.queue.options.voteToSkip
-                    }
-                },
-                premium: el.premium
-            }
+            if(!guild) return console.log(guild);
+            else 
+                return {
+                    name: guild.name,
+                    icon: guild.iconURL(),
+                    memberCount: guild.memberCount,
+                    id: el.id,
+                    queue: {
+                        isPlaying: el.queue.isPlaying,
+                        isPaused: el.queue.isPaused,
+                        queueInfo: el.queue.queueInfo,
+                        currentSongInfo: el.queue.currentSongInfo,
+                        options: {
+                            volume: el.queue.options.volume,
+                            loop: el.queue.options.loop,
+                            recommendations: el.queue.options.recommendations,
+                            voteToSkip: el.queue.options.voteToSkip
+                        }
+                    },
+                    premium: el.premium
+                }
         });
         
         res.json({ data: servers });
