e621d01f8b7da73f4c1ab2d09b39b3993e424550
Stefan <stefischer@mail.com>
2020-10-08 08:58:03 -0400

    Removed old comment

--- Client_Admin/src/services/commandLogServices.js
+++ Client_Admin/src/services/commandLogServices.js
@@ -2,20 +2,6 @@ import axios from 'axios';
 import env from '../env';
 const services = {};
 
-/*
-
-    Remember to update this for once new API is sent to PROD
-
-*/
-
-// services.getCommandLogs = () => {
-//     return axios({
-//         method: "GET",
-//         url: `${env.TEST_API}/admin/logs/command`,
-//         headers: { "Authorization": `Bearer ${window.localStorage.token}` }
-//     });
-// };
-
 services.getCommandLogs = () => {
     return axios({
         method: "GET",
