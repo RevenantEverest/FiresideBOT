872caf23d4c17f98ae8a7975a5e4dfae13838ac6
Stefan <stefischer@mail.com>
2020-11-05 21:06:23 -0500

    Potentially fix unescaped characters error in YouTube services

--- Discord/services/youtubeServices.js
+++ Discord/services/youtubeServices.js
@@ -1,16 +1,15 @@
 const axios = require('axios');
 const config = require('../config/config');
-const KEY = config.Google_Key;
 const services = {};
 
 
 
 services.youtubeSearch = (data) => {
-  return axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&order=relevance&q=${data}&type=video&key=${process.env.GOOGLE_KEY}`);
+  return axios.get(encodeURI(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&order=relevance&q=${data}&type=video&key=${process.env.GOOGLE_KEY}`));
 };
 
 services.youtubePlaylistSearch = (data) => {
-  return axios.get(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=10&playlistId=${data}&key=${process.env.GOOGLE_KEY}`);
+  return axios.get(encodeURI(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=10&playlistId=${data}&key=${process.env.GOOGLE_KEY}`));
 };
 
 module.exports = services;
