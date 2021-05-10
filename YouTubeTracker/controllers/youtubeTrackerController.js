75cc7de0754ea4e11c98ea940c53167077e257ae
Stefan <Stefischer@mail.com>
2021-05-10 19:22:24 -0400

    Finalized main controller for the YouTube Tracker package

--- YouTubeTracker/controllers/youtubeTrackerController.js
+++ YouTubeTracker/controllers/youtubeTrackerController.js
@@ -1,49 +1,134 @@
 const Discord = require('discord.js');
 const youtubeServices = require('../services/youtubeServices');
+const db = require('../models/youtubeTrackersDB');
+const config = require('../config/config');
+const flavorTextParser = require('./flavorTextParser');
 const services = {};
 
-const isLiveRegex = /{"text":" watching"}/gi;
-const channelIdRegex = /(?<=\?channel_id=)(.*?)(?=")/gi;
-const channelNameRegex = /(?<=user\/)(.*?)(?=")/gi;
-const titleRegex = /(?<="simpleText":")(.*?)(?=")/gi;
-const thumbnailRegex = /(?<={"thumbnails":\[{"url":")(.*?)(?==)/gi;
-const profileImageRegex = /(?<={"url":"https:\/\/yt3\.ggpht\.com\/ytc\/)(.*?)(?==)/gi;
-const viewCountRegex = /(?<="viewCountText":{"runs":\[{"text":")(.*?)(?="})/gi;
-const videoIdRegex = /(?<={"videoId":")(.*?)(?=")/gi;
+const pgp = require('pg-promise')();
+const QRE = pgp.errors.QueryResultError;
+const qrec = pgp.errors.queryResultErrorCode;
+
+const isLiveRegex = /{"text":" watching"}/i;
+const channelIdRegex = /(?<=itemprop="channelId" content=")(.*?)(?=")/i;
+const channelNameRegex = /(?<="shortBylineText":{"runs":\[{"text":")(.*?)(?=")/i;
+const titleRegex = /(?<="simpleText":")(.*?)(?=")/i;
+const thumbnailRegex = /(?<={"thumbnails":\[{"url":")(.*?)(?=")/i;
+const profileImageRegex = /(?<={"url":"https:\/\/yt3\.ggpht\.com\/ytc\/)(.*?)(?==)/i;
+const viewCountRegex = /(?<="viewCountText":{"runs":\[{"text":")(.*?)(?="})/i;
+const videoIdRegex = /(?<={"videoId":")(.*?)(?=")/i;
 
 services.run = async (bot) => {
-    
-    // youtubeServices.getChannelPage(channelURL)
-    // .then(content => {
-    //     if(isLiveRegex.exec(content.data.toString())) return services.parseChannelInfo(bot, content.data.toString());
-    //     else console.log("No Match");
-    // })
-    // .catch(err => console.error(err));
-};
+    db.findAll()
+    .then(trackers => getStreamStatus(trackers))
+    .catch(err => err instanceof QRE && err.code === qrec.noData ? console.log("No Trackers") : console.error(err));
 
-services.parseChannelInfo = async (bot, content) => {
-    let streamData = {
-        channel_id: channelIdRegex.exec(content)[0],
-        channelName: channelNameRegex.exec(content)[0],
-        title: titleRegex.exec(content)[0],
-        profileImage: "https://yt3.ggpht.com/ytc/" + profileImageRegex.exec(content)[0] + "=s176-c-k-c0x00ffffff-no-rj",
-        thumbnail: thumbnailRegex.exec(content)[0],
-        viewCount: viewCountRegex.exec(content)[0],
-        videoURL: "https://youtube.com/watch?v=" + videoIdRegex.exec(content)[0]
+    function getStreamStatus(trackers) {
+        let temp = [];
+        trackers.forEach(el => {
+            if(temp.map(tracker => tracker.youtube_channel_id).includes(el.youtube_channel_id)) return;
+            let data = {
+                youtube_channel_id: el.youtube_channel_id,
+                guildInfo: trackers.filter(tracker => tracker.youtube_channel_id === el.youtube_channel_id)
+            };
+            temp.push(data);
+        });
+
+        let promises = [];
+        temp.forEach(el => promises.push(youtubeServices.getChannelPage(el.youtube_channel_id)));
+
+        Promise.all(promises)
+        .then(streams => {
+            streams = [].concat.apply([], streams.map(el => el.data).filter(el => isLiveRegex.exec(el)));
+            streams = streams.map(el => {
+                el = el.toString();
+                let channelIdResult = el.match(channelIdRegex);
+                let channelNameResult = el.match(channelNameRegex);
+                let titleResult = el.match(titleRegex);
+                let profileImageResult = el.match(profileImageRegex);
+                let thumbnailResult = el.match(thumbnailRegex);
+                let viewCountResult = el.match(viewCountRegex);
+                let videoIdResult = el.match(videoIdRegex);
+
+                let data = {
+                    youtube_channel_id: channelIdResult[0],
+                    channelName: channelNameResult[0],
+                    title: titleResult[0],
+                    profileImage: "https://yt3.ggpht.com/ytc/" + profileImageResult[0] + "=s176-c-k-c0x00ffffff-no-rj",
+                    thumbnail: encodeURI(thumbnailResult[0]),
+                    viewCount: viewCountResult[0],
+                    videoURL: "https://youtube.com/watch?v=" + videoIdResult[0]
+                };
+                return data;
+            });
+            checkChannelsLive(temp, streams);
+        })
+        .catch(err => console.error(err));
     };
 
-    let embed = new Discord.MessageEmbed();
+    function checkChannelsLive(trackers, streams) {
+        let currentLive = streams.map(el => el.youtube_channel_id);
+        trackers = trackers.filter(el => currentLive.includes(el.youtube_channel_id));
+        currentLive.forEach(tracker => {
+            if(config.channelsLive.includes(tracker))
+                trackers.splice(trackers.map(el => el.youtube_channel_id).indexOf(tracker), 1);
+            else config.channelsLive.push(tracker);
+        });
 
-    embed
-    .setColor(0xff0000)
-    .setAuthor(`${streamData.channelName} is now LIVE`, streamData.profileImage)
-    .setThumbnail(streamData.profileImage)
-    .setDescription(`[${streamData.title}](${streamData.videoURL})`)
-    .addField("Viewers:", streamData.viewCount.toLocaleString())
-    .setImage(streamData.thumbnail)
-    .setFooter("Powered by FiresideBOT", bot.user.avatarURL({ dynamic: true }))
+        config.channelsLive.forEach(el => {
+            if(!currentLive.includes(el))
+                config.channelsLive.splice(config.channelsLive.indexOf(el), 1);
+        });
 
-    bot.channels.resolve("427883469092159492").send(embed);
+        if(!trackers[0]) return;
+
+        parseData(trackers, streams);
+    };
+
+    function parseData(trackers, streams) {
+        let temp = [];
+        trackers.forEach(el => {
+            let streamData = streams.filter(stream => stream.youtube_channel_id === el.youtube_channel_id)[0];
+            streamData.guildInfo = el.guildInfo;
+
+            temp.push(streamData);
+        });
+
+        trackers = temp;
+
+        sendEmbed(trackers);
+    };
+
+    function sendEmbed(trackers) {
+        trackers.forEach(tracker => {
+            let embed = new Discord.MessageEmbed();
+    
+            embed
+            .setColor(0xff0000)
+            .setAuthor(`${tracker.channelName} is now LIVE`, tracker.profileImage)
+            .setThumbnail(tracker.profileImage)
+            .setDescription(`[${tracker.title}](${tracker.videoURL})`)
+            .addField("Viewers:", tracker.viewCount.toLocaleString())
+            .setImage(tracker.thumbnail)
+            .setFooter("Powered by FiresideBOT", bot.user.avatarURL({ dynamic: true }))
+
+            tracker.guildInfo.forEach(async guild => {
+                if(!bot.guilds.resolve(guild.guild_id)) return;
+                else if(!bot.guilds.resolve(guild.guild_id).channels.resolve(guild.channel_id)) return console.log("Invalid Channel", guild);
+
+                let role_mention = "";
+
+                // if(process.env.ENVIRONMENT === "DEV" && guild.guild_id !== "427883469092159490") return;
+                if(guild.flavor_text && guild.flavor_text !== "")
+                    guild.flavor_text = await flavorTextParser(guild.flavor_text, guild, tracker.channelName);
+                else if(guild.role_id && guild.role_id === "@everyone") role_mention = "@everyone";
+                else if(guild.role_id && guild.role_id === "none") role_mention = " ";
+                else if(guild.role_id) role_mention = `<@&${guild.role_id}>`;
+
+                bot.guilds.resolve(guild.guild_id).channels.resolve(guild.channel_id).send(role_mention ? role_mention : guild.flavor_text, embed);
+            });
+        });
+    }
 };
 
 module.exports = services;
