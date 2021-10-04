9b4c9ccb2f487fdad016b5c8838d16f7d36f28dc
Stefan Fischer <Stefischer@mail.com>
2021-10-03 23:52:00 -0400

    Add more screens and change default Drawer header

--- Mobile/src/navigation/DrawerNavigator.js
+++ Mobile/src/navigation/DrawerNavigator.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { colors, Icon } from 'react-native-elements';
+import { Header } from './Headers';
 
 import {
     HomeContainer,
@@ -13,10 +15,21 @@ function DrawerNavigator() {
     return(
         <Drawer.Navigator
         initialRouteName="Home"
+        screenOptions={{
+            header: (props) => (<Header {...props} />)
+        }}
         >
             <Drawer.Screen
             name="Home"
             component={HomeContainer}
+            />
+            <Drawer.Screen
+            name="Account"
+            component={AccountContainer}
+            />
+            <Drawer.Screen
+            name="Login"
+            component={LoginContainer}
             />
         </Drawer.Navigator>
     );
