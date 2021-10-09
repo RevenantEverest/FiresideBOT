4bc7bcd2078f40d883fcec59b05c2a979b1a7763
Stefan Fischer <Stefischer@mail.com>
2021-10-09 01:07:50 -0400

    Add Trackers container and screen

--- Mobile/src/containers/index.js
+++ Mobile/src/containers/index.js
@@ -5,6 +5,7 @@ export { default as DashboardContainer } from './Dashboard/DashboardContainer';
 export { default as EconomyContainer } from './Economy/EconomyContainer';
 export { default as ModerationContainer } from './Moderation/ModerationContainer';
 export { default as RanksContainer } from './Ranks/RanksContainer';
+export { default as TrackersContainer } from './Trackers/TrackersContainer';
 
 
 export { default as HomeContainer } from './Home/HomeContainer';
