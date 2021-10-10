ec9783c0dc4674dbf4776b104c22b7ee1a02f78b
Stefan Fischer <Stefischer@mail.com>
2021-10-10 11:31:13 -0400

    Implement Custom Drawer

--- Mobile/src/navigation/DrawerNavigator.js
+++ Mobile/src/navigation/DrawerNavigator.js
@@ -1,36 +1,42 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { colors, Icon } from 'react-native-elements';
+import { useTheme } from 'react-native-elements';
 import { Header } from './Headers';
+import { Drawer as CustomDrawer } from './Drawer';
 
 import {
-    HomeContainer,
-    AccountContainer,
-    LoginContainer
+    AnalyticsContainer,
+    CustomCommandsContainer,
+    DashboardContainer,
+    EconomyContainer,
+    ModerationContainer,
+    RanksContainer,
+    TrackersContainer,
+    ServerSettingsContainer
 } from '../containers';
 
 const Drawer = createDrawerNavigator();
 
 function DrawerNavigator() {
+
+    const { theme } = useTheme();
+
     return(
         <Drawer.Navigator
-        initialRouteName="Home"
+        initialRouteName="Dashboard"
         screenOptions={{
-            header: (props) => (<Header {...props} />)
+            header: (props) => <Header {...props} />
         }}
+        drawerContent={(props) => <CustomDrawer {...props} />}
         >
-            <Drawer.Screen
-            name="Home"
-            component={HomeContainer}
-            />
-            <Drawer.Screen
-            name="Account"
-            component={AccountContainer}
-            />
-            <Drawer.Screen
-            name="Login"
-            component={LoginContainer}
-            />
+            <Drawer.Screen name="Dashboard" component={DashboardContainer} />
+            <Drawer.Screen name="CustomCommands" component={CustomCommandsContainer} />
+            <Drawer.Screen name="Analytics" component={AnalyticsContainer} />
+            <Drawer.Screen name="Ranks" component={RanksContainer} />
+            <Drawer.Screen name="Economy" component={EconomyContainer} />
+            <Drawer.Screen name="Moderation" component={ModerationContainer} />
+            <Drawer.Screen name="Trackers" component={TrackersContainer} />
+            <Drawer.Screen name="ServerSettings" component={ServerSettingsContainer} />
         </Drawer.Navigator>
     );
 };
