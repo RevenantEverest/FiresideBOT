499fce66e7d9aee070ce7c69b2cd79154c4a36ec
Stefan Fischer <Stefischer@mail.com>
2021-10-17 02:15:39 -0400

    Create container for main Home page

---
+++ Client/src/containers/Home/HomeContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { HomePage } from '../../pages/Home';
+
+function HomeContainer(props) {
+    return(
+        <HomePage {...props} />
+    );
+};
+
+export default HomeContainer;
