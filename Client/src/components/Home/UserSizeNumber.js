90533e5d080f32b831eddfed83f9faa405b11fce
Stefan Fischer <Stefischer@mail.com>
2021-10-23 16:34:20 -0400

    Create UserSizeNumber component for handling bot user size

---
+++ Client/src/components/Home/UserSizeNumber.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import CountUp from 'react-countup';
+
+function UserSizeButton({ api, delay=1, ...props }) {
+
+    const timeoutDuration = delay * 1000;
+    const [userSize, setUserSize] = useState(0);
+    const [renderCounter, setRenderCounter] = useState(false);
+
+    useEffect(() => {
+        api.getDiscordUserSize()
+        .then(size => setUserSize(size.data.data))
+        .catch(err => console.error(err));
+    }, [api]);
+
+    useEffect(() => {
+        let timeout = null;
+        timeout = setTimeout(() => {
+            setRenderCounter(true);
+        }, timeoutDuration);
+
+        return () => {
+            clearTimeout(timeout);
+        };
+    }, [userSize]);
+
+    return(
+        <div 
+        {...props}
+        style={{
+            flex: 1,
+            alignItems: "center !important"
+        }}
+        >
+            {renderCounter ? <CountUp duration={2} separator="," end={userSize}  /> : 0}
+        </div>
+    );
+};
+
+export default UserSizeButton;
