53a296fc9edce43a86e5146ccaac796d724f7e7e
Stefan Fischer <Stefischer@mail.com>
2021-10-24 21:13:07 -0400

    Remove text in Footer

--- Client/src/navigation/Footer/Footer.js
+++ Client/src/navigation/Footer/Footer.js
@@ -24,7 +24,7 @@ function Footer({ changeTheme }) {
             <Row className="py-4 d-flex align-items-center">
                 <Col md="6" lg="5" className="text-center text-md-left mb-4 mb-md-0">
                 <h6 className="mb-0 white-text">
-                    Get connected with us on social networks!
+                    
                 </h6>
                 </Col>
                 <Col md="6" lg="7" className="text-center text-md-right">
