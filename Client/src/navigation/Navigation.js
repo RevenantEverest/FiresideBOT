72cbb5feddf8bf587124dd19623af6056e81bdd1
Stefan Fischer <Stefischer@mail.com>
2021-10-24 21:14:20 -0400

    Implement more complex route handling for Navbar routes and SideNav routes

--- Client/src/navigation/Navigation.js
+++ Client/src/navigation/Navigation.js
@@ -2,22 +2,56 @@ import React from 'react';
 import { Route, withRouter } from 'react-router-dom';
 import { makeStyles } from '@fluentui/react-theme-provider';
 import { Navbar } from './Navbar';
+import { SideNav } from './Sidenav';
 import { Footer } from './Footer';
 import {
-    HomeContainer,
-    GettingStartedContainer
+    DashboardContainer,
+    GettingStartedContainer,
+    HomeContainer
 } from '../containers';
+
+import { _HomeRoutes, _DashboardRoutes } from './_Routes';
 
 function Navigation(props) {
 
-    const styles = useStyles();;
+    const styles = useStyles();
+    const mainPath = `/${props.location.pathname.split("/")[1]}`;
 
-    return(
-        <div className={"app " + styles.app}>
+    const calculateNavbar = () => {
+        const homeRoutes = _HomeRoutes.map(({ path, subRoutes }) => {
+            return path || subRoutes.map(subRoute => subRoute.path);
+        }).flat();
+        const dashboardRoutes = _DashboardRoutes.map(({ path, subRoutes }) => {
+            return path || subRoutes.map(subRoute => subRoute.path);
+        }).flat();
+
+        if(homeRoutes.map(path => `/${path.split("/")[1]}`).includes(mainPath))
+            return renderHomeRoutes();
+        else if(dashboardRoutes.map(path => `/${path.split("/")[1]}`).includes(mainPath))
+            return renderDashboardRoutes();
+    };
+
+    const renderHomeRoutes = () => (
+        <div>
             <Navbar {...props} />
             <Route exact path="/" component={HomeContainer} />
             <Route exact path="/getting_started" component={GettingStartedContainer} />
             <Footer {...props} />
+        </div>
+    );
+
+    const renderDashboardRoutes = () => (
+        <div>
+        <SideNav {...props}>
+            <Route exact path="/dashboard" component={DashboardContainer} />
+            <Footer {...props} />
+        </SideNav>
+        </div>
+    );
+
+    return(
+        <div className={"app " + styles.app}>
+            {calculateNavbar()}
         </div>
     );
 };
