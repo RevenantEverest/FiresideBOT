d7fd2c6de8c2917cb231b20776fdeb001e7d0e21
Stefan Fischer <Stefischer@mail.com>
2021-11-12 01:50:23 -0500

    Implement Premium components inside PremiumPage

---
+++ Client/src/components/Premium/index.js
@@ -0,0 +1,2 @@
+export { default as PremiumPricing } from './PremiumPricing/PremiumPricing';
+export { default as PremiumTable } from './PremiumTable/PremiumTable';
--- Client/src/pages/Premium/PremiumPage.js
+++ Client/src/pages/Premium/PremiumPage.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import {
+    MDBContainer as Container,
+    MDBRow as Row,
+    MDBAnimation
+} from 'mdbreact';
+import { PremiumPricing, PremiumTable } from '../../components/Premium';
 
 function PremiumPage({ userData }) {
     
     return(
-        <div className="app-page">
+        <div className="app-page mb-5">
+            <Container>
+            <Row>
+                <PremiumPricing />
+            </Row>
+            <MDBAnimation reveal type="fadeInLeft" duration="1.3s">
+                <Row>
+                    <PremiumTable />
+                </Row>
+            </MDBAnimation>
+            </Container>
         </div>
     );
 };
