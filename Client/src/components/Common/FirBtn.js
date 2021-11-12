0ded7d99870fdcaaf7e486c6663dc794b01f6c88
Stefan Fischer <Stefischer@mail.com>
2021-11-12 01:48:53 -0500

    Update FirBtn to have an on hover color change

--- Client/src/components/Common/FirBtn.js
+++ Client/src/components/Common/FirBtn.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { makeStyles } from '@fluentui/react-theme-provider';
 import { useTheme } from '@fluentui/react-theme-provider';
 import { MDBBtn } from 'mdbreact';
+import { colors } from '../../utils';
 
 function FirBtn(props) {
 
     const { children, className, ...restProps } = props;
     const theme = useTheme();
+    const styles = useStyles();
 
     return(
-        <MDBBtn className={[className, theme.classNames.button].join(" ")} {...restProps}>
+        <MDBBtn className={[className, theme.classNames.button, styles.button].join(" ")} {...restProps}>
             {children}
         </MDBBtn>
     );
 };
+
+const useStyles = makeStyles((theme) => ({
+    button: {
+        ':hover': {
+            backgroundColor: `${colors.hexToRgba(theme.colors.primary, .8)} !important`
+        }
+    }
+}));
 
 FirBtn.defaultProps = {
     action: false,
