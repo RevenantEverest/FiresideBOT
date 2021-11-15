d32053167c232c741d87504914186dd3b3669257
Stefan Fischer <Stefischer@mail.com>
2021-11-14 23:18:03 -0500

    Create RouteAnimation wrapper component

---
+++ Client/src/components/Common/RouteAnimation.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+function RouteAnimation({ children }) {
+
+    const { initial, animate, exit, transition } = {
+        initial: { x: "-100vw" },
+        animate: { x: 0 },
+        exit: { x: "100vw" },
+        transition: { duration: .8 }
+    };
+
+    return(
+        <motion.div initial={initial} animate={animate} exit={exit} transition={transition}>
+            {children}
+        </motion.div>
+    );
+};
+
+export default RouteAnimation;
