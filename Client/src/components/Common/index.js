cd4b4265eb359c1331695a33045324e318082fd4
Stefan Fischer <Stefischer@mail.com>
2021-11-15 01:00:05 -0500

    Create ScrollToTop common component

--- Client/src/components/Common/index.js
+++ Client/src/components/Common/index.js
@@ -1,3 +1,4 @@
 export { default as Breadcrumb } from './Breadcrumb';
 export { default as FirBtn } from './FirBtn';
 export { default as RouteAnimation } from './RouteAnimation';
+export { default as ScrollToTop } from './ScrollToTop';
