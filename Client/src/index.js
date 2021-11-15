ca624e668c7a5342d01eaa060da4a2e59613eb53
Stefan Fischer <Stefischer@mail.com>
2021-11-15 01:02:12 -0500

    Implement redux-persist wrapper and RooterContainer in app index

--- Client/src/index.js
+++ Client/src/index.js
@@ -4,15 +4,21 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 import './index.css';
-import App from './App';
+import { RootContainer } from './containers';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './store';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
     <Provider store={store}>
         <React.StrictMode>
-            <App />
+            <Router>
+                <PersistGate persistor={persistor}>
+                    <RootContainer />
+                </PersistGate>
+            </Router>
         </React.StrictMode>
     </Provider>,
     document.getElementById('root')
