4b3993010677bd560c7620d835008bb517af9498
Stefan Fischer <Stefischer@mail.com>
2021-11-17 01:51:54 -0500

    Implement CascadingCard component with dummy data

--- Client/src/pages/Dashboard/DashboardPage.js
+++ Client/src/pages/Dashboard/DashboardPage.js
@@ -4,7 +4,7 @@ import {
     MDBRow as Row,
     MDBCol as Col
 } from 'mdbreact';
-import { Breadcrumb } from '../../components/Common';
+import { Breadcrumb, CascadingCard } from '../../components/Common';
 
 function DashboardPage({ userData }) {
     
@@ -12,8 +12,30 @@ function DashboardPage({ userData }) {
         <div className="dashboard-page">
             <Container fluid>
             <Breadcrumb routes={[ { title: "Dashboard", path: "/dashboard" } ]} />
-            <Row>
-                <Col>
+            <Row className="my-5">
+                <Col md="4" className="mb-4">
+                    <CascadingCard 
+                    title="New Guild Members" 
+                    number={20}
+                    iconName="users" 
+                    color="#FF5005" 
+                    />
+                </Col>
+                <Col md="4" className="mb-4">
+                    <CascadingCard 
+                    title="Commands Used" 
+                    number={20}
+                    iconName="magic" 
+                    color="#00B39A" 
+                    />
+                </Col>
+                <Col md="4" className="mb-4">
+                    <CascadingCard 
+                    title="Songs Played" 
+                    number={20}
+                    iconName="music" 
+                    color="#CC8218" 
+                    />
                 </Col>
             </Row>
             </Container>
