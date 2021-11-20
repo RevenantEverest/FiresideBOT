e2fa995024b9eaefb13147e8ba1bcbf53b74f428
Stefan Fischer <Stefischer@mail.com>
2021-11-20 17:19:47 -0500

    Add changelogs redux state and getChangelogs changelogsAction to DashboardContainer

--- Client/src/containers/Dashboard/DashboardContainer.js
+++ Client/src/containers/Dashboard/DashboardContainer.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { changelogActions } from '../../store';
 import { RouteAnimation } from '../../components/Common';
 import { DashboardPage } from '../../pages/Dashboard';
 
 function mapStateToProps(state) {
     return {
-        userData: state.auth.user
+        userData: state.auth.user,
+        changelogs: state.changelogs.changelogs
     };
 };
 
 function mapDispatchToProps(dispatch) {
     return {
         api: {
-            
+            getChangelogs: () => {
+                dispatch(changelogActions.getChangelogs());
+            }
         }
     };
 };
