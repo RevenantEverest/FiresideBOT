ab2449c3557fa64f9f88c7024992e482c4a3907e
Stefan Fischer <Stefischer@mail.com>
2022-01-03 20:31:05 -0500

    Import new utils to utils index

--- Discord/utils/index.js
+++ Discord/utils/index.js
@@ -1,11 +1,14 @@
 const arrays = require("./arrays");
+const canvasUtils = require("./canvas");
 const dates = require("./dates");
 const fortunes = require("./fortunes");
+const numbers = require("./numbers");
 const pagination = require("./pagination");
 const playingCards = require("./playingCards");
 const quotes = require("./quotes");
 const rankUtils = require("./ranks");
 const strings = require("./strings");
+const text = require("./text");
 const time = require("./time");
 const voiceConnection = require("./voiceConnection");
 const youtube = require("./youtube");
@@ -13,13 +16,16 @@ const youtube = require("./youtube");
 
 module.exports = {
     arrays,
+    canvasUtils,
     dates,
     fortunes,
+    numbers,
     pagination,
     playingCards,
     quotes,
     rankUtils,
     strings,
+    text,
     time,
     voiceConnection,
     youtube
