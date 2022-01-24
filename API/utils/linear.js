f22ceced856be30c44b98d1e174522dc3ed95315
Stefan Fischer <Stefischer@mail.com>
2022-01-24 16:56:37 -0500

    Update linear util method to return a restructured object housing a username and avatarURL

--- API/utils/linear.js
+++ API/utils/linear.js
@@ -6,7 +6,7 @@ services.getLinearUserDiscord = async (bot, linearUser) => {
         case "d800f88f-a378-4dd7-9a89-ce1698d50f45":
             return bot.users.fetch("163346982709100546");
         default:
-            return bot.users.fetch("163346982709100546");
+            return { username: "Linear App", avatarURL: () => ("https://i.imgur.com/hnL8LTj.png") };
     }
 };
 
