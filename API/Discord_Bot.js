010a31502f3415e55a89911b3a960555cb0ba460
Stefan Fischer <Stefischer@mail.com>
2022-01-31 17:31:57 -0500

    Implement Top.gg stat autoposter in Discord_Bot

--- API/Discord_Bot.js
+++ API/Discord_Bot.js
@@ -1,6 +1,11 @@
 const config = require('./config/config');
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const { AutoPoster } = require('topgg-autoposter');
+const ap = process.env.ENVIRONMENT !== "DEV" ? AutoPoster(process.env.TOPGG_TOKEN, bot) : null;
+
+const chalk = require('chalk');
+
 const twitchTokensController = require('./controllers/twitchTokensController');
 const { dateUtils } = require('./utils');
 
@@ -17,20 +22,45 @@ bot.on("ready", async () => {
         let embed = new Discord.MessageEmbed();
         embed.setColor(0xff9900).setTitle("API Tokens Set").setFooter(await dateUtils.getDate());
 
-        if(process.env.ENVIRONMENT !== "DEV") bot.channels.resolve("543862697742172179").send(embed);
+        if(process.env.ENVIRONMENT !== "DEV") 
+            return bot.channels.resolve("543862697742172179").send(embed);
     });
 
     let embed = new Discord.MessageEmbed();
     embed.setColor(0xff9900).setTitle("API Ready").setFooter(await dateUtils.getDate());
 
-    if(process.env.ENVIRONMENT !== "DEV") bot.channels.resolve("543862697742172179").send(embed);
+    if(process.env.ENVIRONMENT !== "DEV") 
+        return bot.channels.resolve("543862697742172179").send(embed);
 });
 
 bot.on("error", async () => {
     let embed = new Discord.MessageEmbed();
     embed.setColor(0xff0000).setTitle("CLIENT ERROR - API").setFooter(await dateUtils.getDate());
 
-    bot.channels.resolve("543862697742172179").send(embed);
+    return bot.channels.resolve("543862697742172179").send(embed);
 });
+
+/* Top.gg AutoPoster events */
+if(ap) {
+    ap.on("posted", async () => {
+        let embed = new Discord.MessageEmbed();
+        embed.setColor(0xff9900).setTitle("API Ready").setFooter(await dateUtils.getDate());
+
+        if(process.env.ENVIRONMENT === "DEV")
+            return console.log(chalk.hex('#ff9900')("[LOG]") + " Top.gg Stats Posted!")
+        else 
+            return bot.channels.resolve("543862697742172179").send(embed);
+    });
+
+    ap.on("error", async (err) => {
+        let embed = new Discord.MessageEmbed();
+        embed.setColor(0xff0000).setTitle("CLIENT ERROR - API").setFooter(await dateUtils.getDate());
+
+        console.error(chalk.hex('#ff9900')("[ERROR]") + " Top.gg AutoPoster Error", err);
+
+        if(process.env.ENVIRONMENT !== "DEV")
+            return bot.channels.resolve("543862697742172179").send(embed);
+    });
+};
 
 module.exports = bot;
