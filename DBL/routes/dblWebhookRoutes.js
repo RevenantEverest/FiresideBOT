5a29b6f440a9418d4b93cf5ffd2220ef33de0de5
Stefan Fischer <Stefischer@mail.com>
2022-02-17 16:28:54 -0500

    Remove DBL directory

--- DBL/routes/dblWebhookRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const controller = require('../controllers/dblWebhookController');
-const router = express.Router();
-
-router.route("/")
-.post(controller.handleVote)
-
-module.exports = router;
