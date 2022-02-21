6877f07657c7a2b9b2e6354f49d7835a86295ad3
Stefan Fischer <Stefischer@mail.com>
2022-02-21 17:32:01 -0500

    Replace console log with logs utils method and import discord bot into server

--- API/src/server.ts
+++ API/src/server.ts
@@ -1,18 +1,22 @@
 import "reflect-metadata";
-import dbConfig from "./config/dbConfig.js";
+import { dbConfig } from "./config/index.js";
 
 import dotenv from 'dotenv';
 import { createConnection } from 'typeorm';
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
-import chalk from 'chalk';
 
 import waitForPostgres from './db/waitForPostgres.js';
+import bot from './discordBot.js';
+
+import { logs, colors } from './utils/index.js';
 
 dotenv.config();
 
 waitForPostgres(createConnection, dbConfig);
+
+bot.login(process.env.DISCORD_KEY);
 
 const PORT = process.env.API_PORT || 3001;
 const app = express();
@@ -25,6 +29,6 @@ app.set("trust proxy", true);
 app.set("trust proxy", "loopback");
 
 app.listen(PORT, () => {
-    return console.log(chalk.hex("#00ff00")(`[HTTP]`) +  ` Fireside-API: Listening on port ${PORT}`)
+    return logs.log({ color: colors.success, type: "HTTP", message: `Fireside-API: Listening on port ${PORT}` });
 });
 
