e2049bc7274b330a869b261f9fe5e13b01c654e5
Stefan Fischer <Stefischer@mail.com>
2022-02-20 21:50:41 -0500

    Add offset to extractPaginationParams middleware

--- api/src/middleware/extractPaginationParams.ts
+++ api/src/middleware/extractPaginationParams.ts
@@ -3,19 +3,29 @@ import { DEFAULTS } from '../constants/index.js';
 
 function extractPaginationParams(req:Request, res:Response, next:NextFunction) {
     
-    let page = 0;
-    let limit = DEFAULTS.LIMIT;
+    let page:number = 1;
+    let limit:number = DEFAULTS.LIMIT;
+    let offset:number = 0;
 
     if(req.query && typeof req.query.page === "string") {
         page = parseInt(req.query.page, 10);
     }
 
-    if(req.query && typeof req.query.limit === "string") {
-        limit = parseInt(req.query.limit, 10);
+    if(page < 1) {
+        page = 1;
+    }
+
+    if(page === 2) {
+        offset = limit;
+    }
+
+    if(page > 2) {
+        offset = limit * page;
     }
 
     res.locals.page = page;
     res.locals.limit = limit;
+    res.locals.offset = offset;
 
     next();
 };
