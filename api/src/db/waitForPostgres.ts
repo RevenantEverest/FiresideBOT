5a0c3db9edf1376a4892cb309bc1aada8e5a7a16
Stefan Fischer <Stefischer@mail.com>
2022-02-21 17:30:26 -0500

    Replace bare console log with logs utils method

--- api/src/db/waitForPostgres.ts
+++ api/src/db/waitForPostgres.ts
@@ -1,11 +1,12 @@
 import { ConnectionOptions, getConnection } from 'typeorm';
+import { logs, colors } from '../utils/index.js';
 
 async function waitForPostgres(createConnection:Function, dbConfig:ConnectionOptions) {
     let retries = 5;
     while(retries) {
         try {
             await createConnection(dbConfig);
-            console.log("Connected to Postgres");
+            logs.log({ color: colors.success, message: "Connected to Postgres" });
             break;
         }
         catch(err) {
