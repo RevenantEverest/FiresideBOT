3e4e80d7c557dea6ac85c77cea714df43d033418
Stefan Fischer <Stefischer@mail.com>
2022-02-21 16:03:50 -0500

    Add log channel id to constants

--- api/src/constants/index.ts
+++ api/src/constants/index.ts
@@ -1,1 +1,2 @@
 export * as DEFAULTS from './defaults.js';
+export * as LOG_CHANNELS from './logChannels.js';
---
+++ api/src/constants/logChannels.ts
@@ -0,0 +1,4 @@
+const isDev = process.env.ENVIRONMENT === "DEV";
+
+export const LOGS = "543862697742172179";
+export const LINEAR = isDev ? "427883469092159492" : "896220035247394826";
