6dcf42acf45579dd20f555d9dc77b8a835abbe35
Stefan Fischer <Stefischer@mail.com>
2022-03-07 17:16:22 -0500

    Create method for error logging in logs

--- api/src/utils/logs.ts
+++ api/src/utils/logs.ts
@@ -1,29 +1,20 @@
-import Discord, { Client, TextChannel } from 'discord.js';
+import Discord, { TextChannel } from 'discord.js';
 import chalk from 'chalk';
 import { LOG_CHANNELS } from '../constants/index.js';
 import * as dates from './dates.js';
+import { logTypes } from '../types/index.js';
 
-type LogColor = number | string;
-type EmbedColor = number;
-
-interface LogOptions {
-    color: LogColor,
-    type?: string,
-    message: string
-};
-
-interface PostToLogsChannelOptions {
-    bot: Client,
-    color: EmbedColor,
-    title: string
-};
-
-export async function log({ color, type="LOG", message }: LogOptions) {
+export async function log({ color, type="LOG", message="" }: logTypes.LogOptions) {
     const logType = "[" + type + "]";
     return console.log(chalk.hex(color.toString())(logType) + " " + message);
 };
 
-export async function postToLogsChannel({ bot, color, title }: PostToLogsChannelOptions) {
+export async function error({ color, type="ERROR", message="" }: logTypes.ErrorLogOptions) {
+    const logType = "[" + type + "]";
+    return console.error(chalk.hex(color.toString())(logType) + " " + message)
+};
+
+export async function postToLogsChannel({ bot, color, title }: logTypes.PostToLogsChannelOptions) {
     const { date, time } = await dates.getTimestampAndFormat();
     const logChannel = await bot.channels.fetch(LOG_CHANNELS.LOGS) as TextChannel;
 
