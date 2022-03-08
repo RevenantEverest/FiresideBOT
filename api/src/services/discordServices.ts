0af5519dff3d2dcd424ebaedd54a42ee286a29b4
Stefan Fischer <Stefischer@mail.com>
2022-03-08 11:32:45 -0500

    Update discordServices to reflect env constants changes

--- api/src/services/discordServices.ts
+++ api/src/services/discordServices.ts
@@ -5,8 +5,8 @@ import { URLS, ENV } from '../constants/index.js';
 export async function getToken(code: string, redirect: string): Promise<AxiosResponse> {
 
     const data = new URLSearchParams([
-        ["client_id", ENV.CLIENT_ID],
-        ["client_secret", ENV.CLIENT_SECRET],
+        ["client_id", ENV.DISCORD.CLIENT_ID],
+        ["client_secret", ENV.DISCORD.CLIENT_SECRET],
         ["grant_type", "authorization_code"],
         ["code", code],
         ["redirect_uri", redirect] 
@@ -25,8 +25,8 @@ export async function refreshToken(refreshToken: string, redirect: string): Prom
         url: `${URLS.DISCORD}/oauth2/token`,
         params: {
             grant_type: "refresh_token",
-            client_id: ENV.CLIENT_ID,
-            client_secret: ENV.CLIENT_SECRET,
+            client_id: ENV.DISCORD.CLIENT_ID,
+            client_secret: ENV.DISCORD.CLIENT_SECRET,
             refresh_token: refreshToken,
             scope: "guilds identify guilds.join email messages.read",
             redirect_uri: redirect
