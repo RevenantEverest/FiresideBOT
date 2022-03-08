5691d3a1330a895e08028dee3dcc57ae9f241979
Stefan Fischer <Stefischer@mail.com>
2022-03-08 11:28:50 -0500

    Restructure environment constants

--- api/src/constants/environment.ts
+++ api/src/constants/environment.ts
@@ -1,2 +1,13 @@
-export const CLIENT_ID = process.env.DISCORD_CLIENT_ID as string;
-export const CLIENT_SECRET = process.env.DISCORD_CLIENT_SECRET as string;
+export const IS_DEV = process.env.ENVIRONMENT === "DEV";
+
+export const API_PORT = process.env.API_PORT as string;
+
+export const DISCORD = {
+    CLIENT_ID: process.env.DISCORD_CLIENT_ID as string,
+    CLIENT_SECRET: process.env.CLIENT_SECRET as string,
+    KEY: process.env.DISCORD_KEY as string,
+    REDIRECT_URI: process.env.DISCORD_BACKEND_REDIRECT as string,
+    ADMIN_REDIRECT_URI: process.env.DISCORD_ADMIN_BACKEND_REDIRECT as string
+};
+
+export const TOPGG_TOKEN = process.env.TOPGG_TOKEN as string;
