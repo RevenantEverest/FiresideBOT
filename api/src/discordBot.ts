931ae14400035958c16e307602ff0cc7754be381
Stefan Fischer <Stefischer@mail.com>
2022-03-08 11:29:54 -0500

    Use environment constants and logs.error method in discordBot

--- api/src/discordBot.ts
+++ api/src/discordBot.ts
@@ -1,6 +1,7 @@
 import Discord, { ClientOptions } from 'discord.js';
 import { AutoPoster } from 'topgg-autoposter';
 
+import { ENV } from './constants/index.js';
 import { colors, logs } from './utils/index.js';
 
 const options:ClientOptions = {
@@ -18,12 +19,11 @@ const options:ClientOptions = {
     ]
 };
 const bot = new Discord.Client(options);
-const topggToken = process.env.TOPGG_TOKEN as string;
-const autoPoster = process.env.ENVIRONMENT !== "DEV" ? AutoPoster(topggToken, bot, { interval: 7200000 }) : null;
+const autoPoster = !ENV.IS_DEV ? AutoPoster(ENV.TOPGG_TOKEN, bot, { interval: 7200000 }) : null;
 
 bot.on("ready", async () => {
 
-    if(process.env.ENVIRONMENT === "DEV") {
+    if(ENV.IS_DEV) {
         return logs.log({ color: colors.success, message: "Discord bot ready" });
     }
 
@@ -32,9 +32,11 @@ bot.on("ready", async () => {
 
 bot.on("error", async (err:Error) => {
 
-    console.error(err);
+    const message = "CLIENT ERROR - API";
+    
+    logs.error({ color: colors.error, err, message: message });
 
-    if(process.env.ENVIRONMENT === "DEV") return;
+    if(ENV.IS_DEV) return;
 
     logs.postToLogsChannel({ bot: bot, color: colors.error, title: "CLIENT ERROR - API" });
 });
@@ -45,20 +47,22 @@ if(autoPoster) {
 
         const title = "Top.gg Stats Posted";
 
-        if(process.env.ENVIRONMENT === "DEV") {
+        if(ENV.IS_DEV) {
             return logs.log({ color: colors.warning, type: "TOPGG", message: title });
         }
 
         logs.postToLogsChannel({ bot: bot, color: colors.topgg, title: title });
     });
 
-    autoPoster.on("error", async (err) => {
+    autoPoster.on("error", async (err: Error) => {
 
-        console.error(err);
+        const message = "Top.gg AutoPoster Error";
 
-        if(process.env.ENVIRONMENT === "DEV") return;
+        logs.error({ color: colors.error, err, message: message });
 
-        logs.postToLogsChannel({ bot: bot, color: colors.error, title: "Top.gg AutoPoster Error" });
+        if(ENV.IS_DEV) return;
+
+        logs.postToLogsChannel({ bot: bot, color: colors.error, title: message });
     });
 };
 
