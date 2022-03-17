f37f66c3e826b6eb3afaad2511c847f514115323
Stefan Fischer <Stefischer@mail.com>
2022-03-17 18:25:59 -0400

    Create server.ts

---
+++ api/src/server.ts
@@ -0,0 +1,34 @@
+import "reflect-metadata";
+import { dbConfig } from "./config/index.js";
+
+import dotenv from 'dotenv';
+import { createConnection } from 'typeorm';
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+import waitForPostgres from './db/waitForPostgres.js';
+import bot from './discordBot.js';
+
+import { logs, colors } from './utils/index.js';
+
+dotenv.config();
+
+waitForPostgres(createConnection, dbConfig);
+
+bot.login(process.env.DISCORD_KEY);
+
+const PORT = process.env.API_PORT || 3001;
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.set("trust proxy", true);
+app.set("trust proxy", "loopback");
+
+app.listen(PORT, () => {
+    return logs.log({ color: colors.success, type: "HTTP", message: `Fireside-API: Listening on port ${PORT}` });
+});
+
