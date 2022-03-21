5d85c96c76e30c48a31b4bc2aa0f8dd8b6750281
Stefan Fischer <Stefischer@mail.com>
2022-03-21 11:43:25 -0400

    Add common and discord utils to index file

--- api/src/utils/index.ts
+++ api/src/utils/index.ts
@@ -5,3 +5,5 @@ export * as logs from './logs.js';
 export * as promises from './promises.js';
 export * as errors from './errors.js';
 export * as entities from './entities.js';
+export * as common from './common.js';
+export * as discord from './discord.js';
