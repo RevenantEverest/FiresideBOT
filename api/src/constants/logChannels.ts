a6f876f731a0d28ed368a2d6ed04a53561f54f7e
Stefan Fischer <Stefischer@mail.com>
2022-03-21 11:41:36 -0400

    Added vote log channel to logChannels constants

--- api/src/constants/logChannels.ts
+++ api/src/constants/logChannels.ts
@@ -2,3 +2,4 @@ const isDev = process.env.ENVIRONMENT === "DEV";
 
 export const LOGS = "543862697742172179";
 export const LINEAR = isDev ? "427883469092159492" : "896220035247394826";
+export const VOTES = isDev ? "427883469092159492" : "539303187342032896";
