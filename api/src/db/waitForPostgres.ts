63b71049b14f914d58094c667cf93dd710c978ee
Stefan Fischer <Stefischer@mail.com>
2022-03-21 11:59:16 -0400

    Add error handler for waitForPostgres

--- api/src/db/waitForPostgres.ts
+++ api/src/db/waitForPostgres.ts
@@ -1,18 +1,27 @@
 import { ConnectionOptions, getConnection } from 'typeorm';
 import { logs, colors } from '../utils/index.js';
 
+function handleError(err: Error) {
+    const errString = err.toString();
+
+    if(!errString.split(" ").includes("ECONNREFUSED")) {
+        logs.error({ color: colors.warning, type: "DB", err });
+    }
+};
+
 async function waitForPostgres(createConnection:Function, dbConfig:ConnectionOptions) {
     let retries = 5;
     while(retries) {
         try {
             await createConnection(dbConfig);
-            logs.log({ color: colors.success, message: "Connected to Postgres" });
+            logs.log({ color: colors.success, message: "Connected to Postgres! 💅✨" });
             break;
         }
         catch(err) {
-            console.log(err);
+            const error = err as Error;
+            handleError(error);
             retries -= 1;
-            console.log(`Retries left: ${retries}`);
+            logs.log({ color: colors.warning, type: "DB", message: `Retries left: ${retries}` });
             await new Promise(res => setTimeout(res, 5000));
         }
     };
