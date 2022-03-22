ba4c32fe16cc4ac293656bd5f9f5a281b0b78f15
Stefan Fischer <Stefischer@mail.com>
2022-03-21 21:27:09 -0400

    Add index, indexAndCount and findOne methods to entities utils

--- api/src/utils/entities.ts
+++ api/src/utils/entities.ts
@@ -1,11 +1,13 @@
 import { BaseEntity, EntityTarget, getManager, DeepPartial } from 'typeorm';
 import * as promises from './promises.js';
 import { entityTypes, promiseTypes } from '../types';
+import { DEFAULTS } from '../constants/index.js';
 
 type OperationReturn<T> = entityTypes.OperationReturn<T>;
 type HandleReturn<T> = promiseTypes.HandleReturn<T>;
 type Target<T> = EntityTarget<T>;
 type Data<T> = DeepPartial<T>;
+type IndexOptions = entityTypes.IndexOptions;
 
 export async function save<T extends BaseEntity>(entity: Target<T>, data: Data<T>): Promise<T> {
     const manager = getManager();
@@ -68,3 +70,51 @@ export async function findOrSave<T extends BaseEntity>(entity: Target<T>, condit
     const entityObject = manager.create(entity, findRes as Data<T>);
     return [entityObject, undefined];
 };
+
+export async function index<T extends BaseEntity>(entity: Target<T>, options: IndexOptions): Promise<HandleReturn<T[]>> {
+    const manager = getManager();
+
+    const promise = manager.find(entity, {
+        skip: options.offset ?? 0,
+        take: options.limit ?? DEFAULTS.LIMIT
+    });
+    const [res, err] = await promises.handle<T[]>(promise);
+
+    if(err) {
+        return [undefined, err];
+    }
+
+    return [res, undefined];
+};
+
+export async function indexAndCount<T extends BaseEntity>(entity: Target<T>, options: IndexOptions): Promise<HandleReturn<[T[], number]>> {
+    const manager = getManager();
+
+    const promise = manager.findAndCount<T>(entity, {
+        skip: options.offset ?? 0,
+        take: options.limit ?? DEFAULTS.LIMIT
+    });
+    const [res, err] = await promises.handle<[T[], number]>(promise);
+
+    if(err) {
+        return [undefined, err];
+    }
+
+    return [res, undefined];
+};
+
+export async function findOne<T extends BaseEntity>(entity: Target<T>, conditional: object): Promise<HandleReturn<T>> {
+    const manager = getManager();
+
+    const promise = manager.findOne(entity, {
+        where: conditional
+    });
+    const [res, err] = await promises.handle<object | undefined>(promise);
+
+    if(err) {
+        return [undefined, err];
+    }
+
+    const entityObject = res ? manager.create(entity, res as Data<T>) : undefined;
+    return [entityObject, undefined];
+}
