524195e53573b3495b8562844ad3ddc4cf6302da
Stefan Fischer <Stefischer@mail.com>
2022-03-23 20:47:26 -0400

    Add errorMessages constants

--- api/src/constants/index.ts
+++ api/src/constants/index.ts
@@ -2,3 +2,4 @@ export * as DEFAULTS from './defaults.js';
 export * as LOG_CHANNELS from './logChannels.js';
 export * as ENV from './environment.js';
 export * as URLS from './urls.js';
+export * as ERRORS from './errorMessages.js';
