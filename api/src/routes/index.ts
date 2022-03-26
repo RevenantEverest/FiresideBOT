eaf6984f584eb28ef7fddf7c4fd801a096e9d3d9
Stefan Fischer <Stefischer@mail.com>
2022-03-26 19:23:12 -0400

    Add playlistRoutes to routes index

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,1 +1,3 @@
 export { default as authRoutes } from './auth/index.js';
+export { default as playlistRoutes } from './playlists/index.js';
+export { default as webhookRoutes } from './webhooks/index.js';
