6fedd98c16298562cb289c9b1e1eb6e95af6ab41
Stefan Fischer <Stefischer@mail.com>
2022-03-28 00:57:03 -0400

    Add error status code to validateId middleware

--- api/src/middleware/validateId.ts
+++ api/src/middleware/validateId.ts
@@ -5,7 +5,7 @@ async function validateId(req: Request, res: Response, next: NextFunction) {
     const id: number = parseInt(req.params.id, 10);
 
     if(!id || !Number.isInteger(id)) {
-        return errors.sendResponse({ res, message: "Invalid ID Parameter" });
+        return errors.sendResponse({ res, status: 400, message: "Invalid ID Parameter" });
     }
     else {
         res.locals.params = {
