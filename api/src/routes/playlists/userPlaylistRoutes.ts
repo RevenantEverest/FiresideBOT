dffc3df65e006c4f4d259c5929e1760c301c6f7f
Stefan Fischer <Stefischer@mail.com>
2022-03-28 01:02:42 -0400

    Update middleware and routes in userPlaylistRoutes

--- api/src/routes/playlists/userPlaylistRoutes.ts
+++ api/src/routes/playlists/userPlaylistRoutes.ts
@@ -1,14 +1,18 @@
 import express from 'express';
 import { userPlaylistsController } from '../../controllers/playlists/index.js';
-import { validateId } from '../../middleware/index.js';
+import { extractPaginationParams, validateId, validateDiscordId } from '../../middleware/index.js';
 
 const router = express.Router();
 
 router.route("/")
+.get(extractPaginationParams, userPlaylistsController.index)
 .post(userPlaylistsController.create)
 
-router.route("/:id")
+router.route("/id/:id")
 .put(validateId, userPlaylistsController.update)
 .delete(validateId, userPlaylistsController.destroy)
+
+router.route("/discord_id/:discordId")
+.get(extractPaginationParams, validateDiscordId, userPlaylistsController.getByDiscordId)
 
 export default router;
