fb6ac58ed56571c18777088576a324284f2aa3bd
Stefan Fischer <Stefischer@mail.com>
2022-03-29 19:11:05 -0400

    Update jest.config

--- API/jest.config.ts
+++ API/jest.config.ts
@@ -3,8 +3,10 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
   preset: "ts-jest",
   testEnvironment: "node",
+  testMatch: ["**.**.*.test.ts"],
   verbose: true,
-  automock: true
+  automock: true,
+  forceExit: true
 };
 
 export default config;
