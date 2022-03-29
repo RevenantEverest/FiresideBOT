ed27f9b6f3cabfebbf9836559d6a0208800db358
Stefan Fischer <Stefischer@mail.com>
2022-03-29 19:15:29 -0400

    Add getOne method to userPlaylistsController

--- api/src/controllers/playlists/userPlaylists/index.ts
+++ api/src/controllers/playlists/userPlaylists/index.ts
@@ -1,4 +1,5 @@
 export { default as index } from './index.action.js';
+export { default as getOne } from './getOne.action.js';
 export { default as getByDiscordId } from './getByDiscordId.action.js';
 export { default as create } from './create.action.js';
 export { default as update } from './update.action.js';
