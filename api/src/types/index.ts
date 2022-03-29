341e04b835f2a44255442862c10266a78f8cd3f6
Stefan Fischer <Stefischer@mail.com>
2022-03-29 19:12:10 -0400

    Add youtubeTypes

--- api/src/types/index.ts
+++ api/src/types/index.ts
@@ -5,3 +5,4 @@ export * as promiseTypes from './promises.js';
 export * as errorTypes from './errors.js';
 export * as entityTypes from './entities.js';
 export * as discordTypes from './discord.js';
+export * as youtubeTypes from './youtube.js';
---
+++ api/src/types/youtube.ts
@@ -0,0 +1,7 @@
+export interface SongInfo {
+    title: string,
+    videoId: string,
+    author: string,
+    duration: number,
+    thumbnail_url: string
+};
