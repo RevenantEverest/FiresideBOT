8725e36101017d8814876c8b5903e2558f2cae75
Stefan Fischer <Stefischer@mail.com>
2022-04-03 03:08:08 -0400

    Add temp solution to jest test errors in topggRoutes

--- api/src/routes/webhooks/topggRoutes.ts
+++ api/src/routes/webhooks/topggRoutes.ts
@@ -5,9 +5,15 @@ import { topggController } from '../../controllers/webhooks/index.js';
 import { ENV } from '../../constants/index.js';
 
 const router = express.Router();
-const webhook = new Topgg.Webhook(ENV.TOPGG_WEBHOOK_AUTH)
 
-router.route("/")
-.post(webhook.listener(topggController.handleVote));
+/*
+    Temporary solution for jest tests to run
+*/
+if(!ENV.IS_TEST) {
+    const webhook = new Topgg.Webhook(ENV.TOPGG_WEBHOOK_AUTH)
+
+    router.route("/")
+    .post(webhook.listener(topggController.handleVote));
+}
 
 export default router;
