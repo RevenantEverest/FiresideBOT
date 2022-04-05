f78c358e83dd95e433375b7824b25fd4e481cac2
Stefan Fischer <Stefischer@mail.com>
2022-04-05 02:13:47 -0400

    Add BASE_URL to constants

--- api/src/constants/environment.ts
+++ api/src/constants/environment.ts
@@ -1,6 +1,8 @@
 export const IS_DEV = process.env.ENVIRONMENT === "DEV";
 export const IS_TEST = process.env.NODE_ENV === "test";
 
+export const BASE_URL = process.env.BASE_URL as string;
+
 export const API_PORT = process.env.API_PORT as string;
 export const TOKEN_SECRET = process.env.TOKEN_SECRET as string;
 
