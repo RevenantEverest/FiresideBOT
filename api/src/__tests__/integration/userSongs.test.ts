7208b40d1423c528b2d63a145ef0fbf5c11b5e67
Stefan Fischer <Stefischer@mail.com>
2022-04-05 20:30:39 -0400

    Create userSongs test skeleton

---
+++ api/src/__tests__/integration/userSongs.test.ts
@@ -0,0 +1,55 @@
+import { createConnection, getConnection } from 'typeorm';
+import supertest from 'supertest';
+
+import initializeApp from '../../app.js';
+import waitForPostgres from '../../db/waitForPostgres.js';
+import issueToken from '../support/login.support.js';
+import dbConfig from '../support/dbConfig.support.js';
+import UserSong from '../../entities/UserSong.js';
+
+const authPayload = issueToken();
+const authHeader = {
+    "Authorization": `Bearer ${authPayload.token}`
+};
+
+const app = initializeApp();
+const baseEndpoint = "/playlist/user/songs";
+
+describe("user songs", () => {
+
+    beforeAll(async () => {
+        await waitForPostgres(createConnection, dbConfig);
+    }, 5 * 5000);
+
+    afterAll(() => {
+        const connection = getConnection();
+        connection.close();
+    });
+
+    /*
+    
+        POST
+    
+    */
+    describe("create user song route", () => {
+
+    });
+
+    /*
+    
+        UPDATE
+    
+    */
+    describe("update user song route", () => {
+    
+    });
+
+    /*
+    
+        GET
+    
+    */
+    describe("get user song route", () => {
+    
+    });
+});
