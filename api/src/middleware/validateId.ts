6c65103feb4c0a351506f8d1b833a042ed95877f
Stefan Fischer <Stefischer@mail.com>
2022-04-06 16:02:41 -0400

    Update validateId middlware to account for all id param types

--- api/src/middleware/validateId.ts
+++ api/src/middleware/validateId.ts
@@ -2,17 +2,36 @@ import { Request, Response, NextFunction } from 'express';
 import { errors } from '../utils/index.js';
 
 async function validateId(req: Request, res: Response, next: NextFunction) {
-    const id: number = parseInt(req.params.id, 10);
+    const re: RegExp = new RegExp("({*.+}?)?((Id|_id)$|(^id$))", "gi");
+
+    const paramKeys: string[] = Object.keys(req.params);
+    const paramKeysString: string = paramKeys.join(" ");
+    const paramKeysTest: boolean = re.test(paramKeysString)
+    const idParamKeyMatch = paramKeysString.match(re);
+
+    const errorFunc: Function = () => {
+        return errors.sendResponse({ 
+            res, 
+            status: 400, 
+            message: "Invalid ID Parameter" 
+        });
+    };
+
+    if(paramKeys.length <= 0 || !paramKeysTest || !idParamKeyMatch) {
+        return errorFunc();
+    }
+
+    const idParamKey: string = idParamKeyMatch[0];
+    const id: number = parseInt(req.params[idParamKey], 10);
 
     if(!id || !Number.isInteger(id)) {
-        return errors.sendResponse({ res, status: 400, message: "Invalid ID Parameter" });
+        return errorFunc();
     }
-    else {
-        res.locals.params = {
-            id
-        };
-        next();
-    }
+
+    res.locals.params = {
+        [idParamKey]: id
+    };
+    next();
 };
 
 export default validateId;
