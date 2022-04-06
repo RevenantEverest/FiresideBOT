701105aaf926e9bfe5b03c4c80e95666baecd115
Stefan Fischer <Stefischer@mail.com>
2022-04-05 20:04:54 -0400

    Add whitespace and existing name check to userPlaylists update action

--- api/src/controllers/playlists/userPlaylists/update.action.ts
+++ api/src/controllers/playlists/userPlaylists/update.action.ts
@@ -5,6 +5,12 @@ import { entities, errors } from '../../../utils/index.js';
 
 async function update(req: Request, res: Response, next: NextFunction) {
 
+    const playlistName: string = req.body.name;
+
+    if(playlistName.includes(" ")) {
+        return errors.sendResponse({ res, status: 400, message: "Playlist Name Cannot Contain White Space" });
+    }
+
     const { id } = res.locals.params;
 
     const [findRes, findErr] = await entities.findOne<UserPlaylist>(UserPlaylist, {
@@ -22,10 +28,14 @@ async function update(req: Request, res: Response, next: NextFunction) {
         return errors.sendResponse({ res, next, message: "UserPlaylist doesn't exist" });
     }
 
+    if(findRes.name === playlistName) {
+        return errors.sendResponse({ res, status: 400, next, message: "Playlist name already exists" });
+    }
+
     const [updatedUserPlaylist, updateErr] = await entities.update<UserPlaylist>(UserPlaylist, {
         ...findRes,
         discord_id: res.locals.auth.discord_id,
-        name: req.body.name
+        name: playlistName
     });
 
     if(updateErr) {
