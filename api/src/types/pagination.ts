f100217e6f84f222beca1da284ddae724a14cbc0
Stefan Fischer <Stefischer@mail.com>
2022-04-06 23:11:56 -0400

    Add PaginatedResponse type to pagination types

--- api/src/types/pagination.ts
+++ api/src/types/pagination.ts
@@ -1,1 +1,7 @@
-export type PaginationUrl = String | null;
+export type PaginationUrl = string | null;
+export interface PaginatedResponse<T> {
+    count: number,
+    next: PaginationUrl,
+    previous: PaginationUrl,
+    results: T[]
+};
