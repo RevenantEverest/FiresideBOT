80ab7f332e057e033e5092daeacd1e81dccdf414
Stefan Fischer <Stefischer@mail.com>
2022-04-06 23:12:18 -0400

    Add truthy method to common utils

--- api/src/utils/common.ts
+++ api/src/utils/common.ts
@@ -1,3 +1,9 @@
+import { Truthy } from '../types/common.js';
+
 export function RNG(num: number) {
     return Math.floor(Math.random() * num);
+};
+
+export function truthy<T>(value: T): value is Truthy<T> {
+    return !!value;
 };
