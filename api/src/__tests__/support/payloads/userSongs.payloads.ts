1589f0f359007455b34a359785fef71c8af0f1e0
Stefan Fischer <Stefischer@mail.com>
2022-04-24 21:09:39 -0400

    Add more mock payloads to userSongs payloads

--- api/src/__tests__/support/payloads/userSongs.payloads.ts
+++ api/src/__tests__/support/payloads/userSongs.payloads.ts
@@ -1,4 +1,5 @@
 import { SongInfo } from '../../../types/youtube.js';
+import { PREMIUM_LIMITS } from '../../../constants/index.js';
 
 export const INVALID = {
     playlist_id: 99875,
@@ -15,5 +16,21 @@ export const MOCK_RETURN: SongInfo = {
     videoId: "d7hGaK98sHH",
     author: "Jest",
     duration: 123,
+    thumbnail_url: "thumbnail_image"
+};
+
+export const INVALID_NON_PREMIUM_MOCK_RETURN: SongInfo = {
+    title: "AnotherTrack",
+    videoId: "jjh36hh1knu",
+    author: "MyFavoriteAuthor",
+    duration: PREMIUM_LIMITS.SONG_DURATION + 1,
+    thumbnail_url: "thumbnail_image"
+};
+
+export const VALID_NON_PREMIUM_MOCK_RETURN: SongInfo = {
+    title: "MyFavoriteTrack",
+    videoId: "lka89op12fv",
+    author: "TheWorstAuthor",
+    duration: PREMIUM_LIMITS.SONG_DURATION,
     thumbnail_url: "thumbnail_image"
 };
