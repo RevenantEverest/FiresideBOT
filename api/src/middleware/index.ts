b3c4266d22157a9d37e6f13c8a2fdc63649d2cb0
Stefan Fischer <Stefischer@mail.com>
2022-04-27 12:52:23 -0400

    Update middleware index to use file paths

--- api/src/middleware/index.ts
+++ api/src/middleware/index.ts
@@ -1,5 +1,6 @@
 export { default as extractPaginationParams } from './extractPaginationParams.js';
 export { default as issueToken } from './issueToken.js';
 export { default as verifyToken } from './verifyToken.js';
-export { default as validateId } from './validateId.js';
-export { default as validateDiscordId } from './validateDiscordId.js';
+export { default as validateId } from './validation/validateId.js';
+export { default as validateDiscordId } from './validation/validateDiscordId.js';
+export { default as validateGuildId } from './validation/validateGuildId.js';
