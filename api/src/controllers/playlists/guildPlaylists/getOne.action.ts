e452d711eec98f17d0017efc7e99ad69843598e3
Stefan Fischer <Stefischer@mail.com>
2022-04-27 22:37:16 -0400

    Create getOne action to guildPlaylists controller

---
+++ api/src/controllers/playlists/guildPlaylists/getOne.action.ts
@@ -0,0 +1,47 @@
+import { Request, Response, NextFunction } from 'express';
+import { Permissions } from 'discord.js';
+
+import { GuildPlaylist } from '../../../entities/index.js';
+
+import { errors, entities, discord } from '../../../utils/index.js';
+
+async function getOne(req: Request, res: Response, next: NextFunction) {
+
+    const guildId: string = res.locals.params.guildId;
+    const id: number = res.locals.params.id;
+
+    console.log(guildId, req.params.guildId);
+
+    const [hasPermission, hasPermissionErr] = await discord.checkMemberPermissions({
+        guildId: guildId,
+        discordId: res.locals.auth.discord_id,
+        permission: Permissions.FLAGS.ADMINISTRATOR
+    });
+
+    if(hasPermissionErr) {
+        return errors.sendResponse({ res, status: 500, err: hasPermissionErr, message: hasPermissionErr.message });
+    }
+
+    if(!hasPermission) {
+        return errors.sendResponse({ res, status: 403, message: "Unauthorized" });
+    }
+
+    const [guildPlaylist, err] = await entities.findOne<GuildPlaylist>(GuildPlaylist, {
+        where: {
+            id: id,
+            guild_id: guildId
+        }
+    });
+
+    if(err) {
+        return errors.sendResponse({ res, status: 500, err, message: "Error Finding GuildPlaylist" });
+    }
+
+    if(!guildPlaylist) {
+        return errors.sendResponse({ res, status: 404, message: "No GuildPlaylist Found" });
+    }
+
+    return res.json({ results: guildPlaylist });
+};
+
+export default getOne;
