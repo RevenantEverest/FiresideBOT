5f6ecf2022b022a1ebc45911dcff568a39230240
Stefan Fischer <Stefischer@mail.com>
2022-04-30 18:56:41 -0400

    Update login support to support different payloads

--- api/src/__tests__/support/login.support.ts
+++ api/src/__tests__/support/login.support.ts
@@ -3,22 +3,27 @@ import JWT from 'jsonwebtoken';
 import { ENV } from '../../constants/index.js';
 import { AuthPayload } from '../../types/auth.js';
 
-const authPayload: AuthPayload = {
-    id: "2c00407c-6f8b-40c1-801d-818f847f99ad",
-    username: "tester",
-    discriminator: 1234,
-    discord_id: "253238262776799587",
-    avatar: "a_dc5vv4dn77xdkcx2und65cp4ke656a95"
+interface AuthHeader {
+    Authorization: string
 };
+interface AuthTestingPayload extends AuthPayload {
+    header: AuthHeader
+}
 
-function issueToken(): AuthPayload {
+function issueToken(authPayload: AuthPayload): AuthTestingPayload {
     const token = JWT.sign(authPayload, ENV.TOKEN_SECRET, {
         expiresIn: "5m"
     });
 
-    authPayload.token = token;
+    const returnPayload: AuthTestingPayload = {
+        ...authPayload,
+        token,
+        header: {
+            Authorization: `Bearer ${token}`
+        }
+    };
 
-    return authPayload;
+    return returnPayload;
 };
 
 export default issueToken;
