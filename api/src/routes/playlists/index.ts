bf854a8fadba711fbc5bb808c4bfe1f8ea9e2d60
Stefan Fischer <Stefischer@mail.com>
2022-04-30 18:24:35 -0400

    Add guildPlaylistsRoutes to playlists route index

--- api/src/routes/playlists/index.ts
+++ api/src/routes/playlists/index.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+
+import guildPlaylistRoutes from './guildPlaylistRoutes.js';
+
 import userPlaylistRoutes from './userPlaylistRoutes.js';
 import userSongRoutes from './userSongRoutes.js';
 
 const router = express.Router();
+
+router.use("/guild", guildPlaylistRoutes);
 
 router.use("/user", userPlaylistRoutes);
 router.use("/user", userSongRoutes);
