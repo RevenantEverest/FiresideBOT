cfc7c38d91ca87b6d94b5cd79bfdaf2db57220c2
Stefan Fischer <Stefischer@mail.com>
2022-04-30 18:39:11 -0400

    Create index file for all guildPlaylist actions

---
+++ api/src/controllers/playlists/guildPlaylists/index.ts
@@ -0,0 +1,6 @@
+export { default as index } from './index.action.js';
+export { default as getOne } from './getOne.action.js';
+export { default as getByGuildId } from './getByGuildId.action.js';
+export { default as create } from './create.action.js';
+export { default as update } from './update.action.js';
+export { default as destroy } from './destroy.action.js';
