35ccfb7d90374c22ec60349702d89c3147b9cf46
Stefan Fischer <Stefischer@mail.com>
2022-04-30 18:39:22 -0400

    Import guildPlaylistsController to playlists index

--- api/src/controllers/playlists/index.ts
+++ api/src/controllers/playlists/index.ts
@@ -1,2 +1,4 @@
+export * as guildPlaylistsController from './guildPlaylists/index.js';
+
 export * as userPlaylistsController from './userPlaylists/index.js';
 export * as userSongsController from './userSongs/index.js';
