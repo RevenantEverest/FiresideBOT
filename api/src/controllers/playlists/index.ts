7fe54b33e1a12bed66d7c5498f14312e07848122
Stefan Fischer <Stefischer@mail.com>
2022-05-01 00:00:32 -0400

    Add guildPlaylistRolesController to playlists controller index

--- api/src/controllers/playlists/index.ts
+++ api/src/controllers/playlists/index.ts
@@ -1,3 +1,4 @@
+export * as guildPlaylistRolesController from './guildPlaylistRoles/index.js';
 export * as guildPlaylistsController from './guildPlaylists/index.js';
 
 export * as userPlaylistsController from './userPlaylists/index.js';
