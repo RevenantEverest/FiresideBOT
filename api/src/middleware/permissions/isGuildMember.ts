a48442ed6c4948363dbca2196746031dfb2ac3b0
Stefan Fischer <Stefischer@mail.com>
2022-05-02 11:29:42 -0400

    Update isGuildMember middleware to use discord isGuildMember method

--- api/src/middleware/permissions/isGuildMember.ts
+++ api/src/middleware/permissions/isGuildMember.ts
@@ -1,37 +1,20 @@
 import { Request, Response, NextFunction } from 'express';
-import bot from '../../discordBot.js';
 
 import { AuthPayload } from '../../types/auth.js';
 
-import { errors, promises } from '../../utils/index.js';
+import { errors, discord } from '../../utils/index.js';
 
 async function isGuildMember(req: Request, res: Response, next: NextFunction) {
     const { discord_id }: AuthPayload = res.locals.auth;
     const guildId: string = res.locals.params ? res.locals.params.guildId : req.body.guild_id;
 
-    const guildPromise = bot.guilds.fetch(guildId);
-    const [guild, guildErr] = await promises.handle(guildPromise);
+    const [isMember, err] = await discord.isGuildMember({ guildId, discordId: discord_id });
 
-    if(guildErr) {
-        return errors.sendResponse({ res, next, err: guildErr, message: "Error Fetching Guild" });
+    if(err) {
+        errors.sendResponse({ res, next, err: err, message: err.message });
     }
 
-    if(!guild) {
-        return errors.sendResponse({ res, status: 404, message: "Invalid Guild" });
-    }
-
-    const guildMemberPromise = guild.members.fetch(discord_id);
-    const [guildMember, guildMemberErr] = await promises.handle(guildMemberPromise);
-
-    if(guildMemberErr) {
-        if(guildMemberErr.message === "Unknown Member") {
-            return errors.sendResponse({ res, status: 401, message: "Unauthorized" });
-        }
-        
-        return errors.sendResponse({ res, next, err: guildMemberErr, message: "Error Fetching Guild Member" });
-    }
-
-    if(!guildMember) {
+    if(!isMember) {
         return errors.sendResponse({ res, status: 401, message: "Unauthorized" });
     }
 
