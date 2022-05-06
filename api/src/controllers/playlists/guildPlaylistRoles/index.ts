a629a59434348e2752e00623636e598bdf6c339f
Stefan Fischer <stefischer@mail.com>
2022-05-06 02:33:39 -0400

    Update index in guildPlaylistRolesController

--- api/src/controllers/playlists/guildPlaylistRoles/index.ts
+++ api/src/controllers/playlists/guildPlaylistRoles/index.ts
@@ -1,5 +1,5 @@
 export { default as index } from './index.action.js';
 export { default as getOne } from './getOne.action.js';
-export { default as getByGuildId } from './getByGuildId.action.js';
+export { default as getByGuildIdAndPlaylistId } from './getByGuildIdAndPlaylistId.action.js';
 export { default as create } from './create.action.js';
 export { default as destroy } from './destroy.action.js';
