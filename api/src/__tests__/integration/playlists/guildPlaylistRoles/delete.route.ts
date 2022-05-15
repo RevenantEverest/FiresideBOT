3a013c91e77f1156cfa85ac31d882560a4c9705c
Stefan Fischer <stefischer@mail.com>
2022-05-14 23:31:17 -0400

    Create delete test skeleton for guild playlist roles

---
+++ api/src/__tests__/integration/playlists/guildPlaylistRoles/delete.route.ts
@@ -0,0 +1,13 @@
+import supertest from 'supertest';
+import { Application } from 'express';
+
+import * as PAYLOADS from '../../../support/payloads/guildPlaylistRoles.payloads.js';
+
+import { AuthTestingPayload } from '../../../support/types/auth.js';
+import { GuildPlaylistRoleExtraParams } from '../../../support/types/extraParams.js';
+
+function deleteRoute(baseEndpoint: string, app: Application, authPayload: AuthTestingPayload, extraParams: GuildPlaylistRoleExtraParams) {
+
+};
+
+export default deleteRoute;
