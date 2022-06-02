90942293af6cfc2cd284601526b0351f25f409fb
Stefan Fischer <stefischer@mail.com>
2022-06-02 05:47:42 -0400

    Create guildSong test spec skeletons

---
+++ api/src/__tests__/integration/playlists/guildSongs/post.route.ts
@@ -0,0 +1,13 @@
+import supertest from 'supertest';
+import { Application } from 'express';
+
+import * as PAYLOADS from '../../../support/payloads/guildSong.payloads.js';
+
+import { AuthTestingPayload } from '../../../support/types/auth.js';
+import { GuildSongExtraParams } from '../../../support/types/extraParams/index.js';
+
+function postRoute(baseEndpoint: string, app: Application, authPayload: AuthTestingPayload, extraParams: GuildSongExtraParams) {
+
+};
+
+export default postRoute;
