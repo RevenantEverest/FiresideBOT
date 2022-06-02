eca03654ad248e16d911a78fb3ddb97936d90933
Stefan Fischer <stefischer@mail.com>
2022-06-02 07:11:30 -0400

    Remove all old discord module files

--- discord/utils/dates.js
+++
@@ -1,9 +0,0 @@
-const services = {};
-
-services.getDate = async () => {
-    let date = new Date();
-    let options = { timezone: 'EST', weekday: 'long', day: 'numeric', month: 'long', hour: 'numeric', minute: 'numeric' };
-    return `${date.toLocaleString('en-US', options)} EST`;
-};
-
-module.exports = services;
