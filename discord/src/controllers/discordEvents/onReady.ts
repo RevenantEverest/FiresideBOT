0b5db1eca379052e234c00638daab2df39fba670
Stefan Fischer <stefischer@mail.com>
2022-06-03 09:13:24 -0400

    Remove console log in onReady

--- discord/src/controllers/discordEvents/onReady.ts
+++ discord/src/controllers/discordEvents/onReady.ts
@@ -4,12 +4,12 @@ import { ENV } from '../../constants/index.js';
 import { logs, colors } from '../../utils/index.js';
 
 async function onReady(bot: Client) {
+
     if(ENV.IS_DEV) {
         return logs.log({ color: colors.success, message: "Discord bot ready" });
     }
 
-    console.log(process.env.ENVIRONMENT, ENV.IS_DEV);
-    // logs.postToLogsChannel({ bot: bot, color: colors.success, title: "API Ready" });
+    logs.postToLogsChannel({ bot: bot, color: colors.success, title: "API Ready" });
 };
 
 export default onReady;
