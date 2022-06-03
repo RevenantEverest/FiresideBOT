f8c7cbeb282f15526303f87cd524c610ac993e26
Stefan Fischer <stefischer@mail.com>
2022-06-03 09:10:14 -0400

    Create fileSystem util methods

--- discord/src/utils/index.ts
+++ discord/src/utils/index.ts
@@ -1,4 +1,6 @@
 export * as colors from './colors.js';
 export * as common from './common.js';
 export * as dates from './dates.js';
+export * as fileSystem from './fileSystem.js';
 export * as logs from './logs.js';
+export * as promises from './promises.js';
