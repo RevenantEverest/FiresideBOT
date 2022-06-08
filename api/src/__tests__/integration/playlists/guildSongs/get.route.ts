33932251052d2868ec1ec50232d2fcb503a3d4bb
Stefan Fischer <stefischer@mail.com>
2022-06-07 20:19:18 -0400

    Populate guild song get route tests

--- api/src/__tests__/integration/playlists/guildSongs/get.route.ts
+++ api/src/__tests__/integration/playlists/guildSongs/get.route.ts
@@ -7,7 +7,31 @@ import { AuthTestingPayload } from '../../../support/types/auth.js';
 import { GuildSongExtraParams } from '../../../support/types/extraParams/index.js';
 
 function getRoute(baseEndpoint: string, app: Application, authPayload: AuthTestingPayload, extraParams: GuildSongExtraParams) {
+    describe("given the user is not logged in", () => {
+        it("should return a 403 status", async () => {
+            await supertest(app)
+            .get(`${baseEndpoint}/${extraParams.guildId}/songs`)
+            .expect(403)
 
+            await supertest(app)
+            .get(`${baseEndpoint}/${extraParams.guildId}/id/1/songs`)
+            .expect(403)
+
+            await supertest(app)
+            .get(`${baseEndpoint}/${extraParams.guildId}/id/1/songs/id/1`)
+            .expect(403)
+        });
+    });
+
+    describe("given the user is logged in", () => {
+        describe("given the playlist id as a param", () => {
+            
+            
+            describe("given the song id as a param", () => {
+
+            });
+        });
+    });
 };
 
 export default getRoute;
