8e5add6ce3c55d0dc69b8cd9523fe69bf5405f1d
Stefan Fischer <stefischer@mail.com>
2022-06-07 20:18:49 -0400

    Import guild song tests to index test file

--- api/src/__tests__/integration/playlists/__index__.ts
+++ api/src/__tests__/integration/playlists/__index__.ts
@@ -1,5 +1,6 @@
 import guildPlaylistTests from './guildPlaylists/__index__.js';
 import guildPlaylistRolesTests from './guildPlaylistRoles/__index__.js';
+import guildSongTests from './guildSongs/__index__.js';
 
 import userPlaylistTests from './userPlaylists/__index__.js';
 import userSongTests from './userSongs/__index__.js';
@@ -7,6 +8,7 @@ import userSongTests from './userSongs/__index__.js';
 export default () => {
     describe("guild playlist routes", guildPlaylistTests);
     describe("guild playlist role routes", guildPlaylistRolesTests);
+    describe("guild playlist song routes", guildSongTests);
     describe("user playlist routes", userPlaylistTests);
     describe("user song routes", userSongTests);
 };
