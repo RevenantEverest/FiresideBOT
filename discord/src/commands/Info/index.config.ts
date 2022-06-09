50e639737dbd49d5892bf19ee00afd40c507928a
Stefan Fischer <stefischer@mail.com>
2022-06-09 08:35:45 -0400

    Create Info command directory with CommandCategoryConfig file

---
+++ discord/src/commands/Info/index.config.ts
@@ -0,0 +1,8 @@
+import { CommandCategoryConfig } from '../../types/commands.js';
+
+const config: CommandCategoryConfig = {
+    name: "Info",
+    description: "This is a placeholder"
+};
+
+export default config;
