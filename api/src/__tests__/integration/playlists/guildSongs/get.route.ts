3d5768b3e366a6653c6742c000b9755ea6733779
Stefan Fischer <stefischer@mail.com>
2022-06-09 07:19:00 -0400

    Add tests to guild song get route spec

--- api/src/__tests__/integration/playlists/guildSongs/get.route.ts
+++ api/src/__tests__/integration/playlists/guildSongs/get.route.ts
@@ -24,11 +24,117 @@ function getRoute(baseEndpoint: string, app: Application, authPayload: AuthTesti
     });
 
     describe("given the user is logged in", () => {
-        describe("given the playlist id as a param", () => {
-            
-            
-            describe("given the song id as a param", () => {
+        describe("given the user is a guild member", () => {
+            describe("given the guild id and playlist id as a param", () => {
+                describe("given the guild id doesn't exist", () => {
+                    it("should return a 200 status and an empty paginated resonse", async () => {
+                        extraParams.mocks.isGuildMember(true);
 
+                        const endpoint = `${baseEndpoint}/81726110987/id/1/songs`;
+                        const { body, statusCode } = await supertest(app)
+                        .get(endpoint)
+                        .set(authPayload.header)
+                        .send()
+
+                        expect(statusCode).toBe(200);
+                        expect(body.results).not.toBe(null);
+
+                        const { results } = body;
+
+                        expect(body.count).not.toBeNull();
+                        expect(results.length).toBe(0);
+                    });
+                });
+
+                describe("given the playlist id doesn't exist", () => {
+                    it("should return a 200 status and an empty paginated response", async () => {
+                        extraParams.mocks.isGuildMember(true);
+
+                        const endpoint = `${baseEndpoint}/${extraParams.guildId}/id/81625/songs`;
+                        const { body, statusCode } = await supertest(app)
+                        .get(endpoint)
+                        .set(authPayload.header)
+                        .send()
+
+                        expect(statusCode).toBe(200);
+                        expect(body.results).not.toBeNull();
+
+                        const { results } = body;
+
+                        expect(body.count).not.toBeNull();
+                        expect(results.length).toBe(0);
+                    });
+                });
+                
+                describe("given the song id as a param", () => {
+                    describe("given the song id doesn't exist", () => {
+                        it("should return a 404 status", async () => {
+                            extraParams.mocks.isGuildMember(true);
+
+                            const endpoint = `${baseEndpoint}/${extraParams.guildId}/id/1/songs/918273`;
+                            await supertest(app)
+                            .get(endpoint)
+                            .set(authPayload.header)
+                            .expect(404)
+                        });
+                    });
+
+                    describe("given the song id exists", () => {
+                        it("should return the guild song", async () => {
+                            extraParams.mocks.isGuildMember(true);
+
+                            const endpoint = `${baseEndpoint}/${extraParams.guildId}/id/1/songs/id/${extraParams.createdSong?.id}`;
+                            const { body, statusCode } = await supertest(app)
+                            .get(endpoint)
+                            .set(authPayload.header)
+                            .send()
+
+                            expect(statusCode).toBe(200);
+                            expect(body.results).not.toBeNull();
+
+                            const { results } = body;
+
+                            expect(results).toEqual({
+                                id: extraParams.createdSong?.id,
+                                title: extraParams.createdSong?.title,
+                                author: extraParams.createdSong?.author,
+                                video_id: extraParams.createdSong?.video_id,
+                                duration: extraParams.createdSong?.duration,
+                                thumbnail_url: extraParams.createdSong?.thumbnail_url,
+                                created_at: extraParams.createdSong?.created_at
+                            });
+                        });
+                    });
+                });
+
+                it("should return paginated songs", async () => {
+                    extraParams.mocks.isGuildMember(true);
+
+                    const endpoint = `${baseEndpoint}/${extraParams.guildId}/id/1/songs`;
+                    const { body, statusCode } = await supertest(app)
+                    .get(endpoint)
+                    .set(authPayload.header)
+                    .send()
+
+                    expect(statusCode).toBe(200);
+                    expect(body.results).not.toBeNull();
+
+                    const { results } = body;
+
+                    expect(body.count).not.toBeNull();
+                    expect(results[0].id).not.toBeNull();
+                    expect(results[0].created_at).not.toBeNull();
+
+                    expect(results[0]).toEqual({
+                        id: extraParams.createdSong?.id,
+                        title: extraParams.createdSong?.title,
+                        author: extraParams.createdSong?.author,
+                        video_id: extraParams.createdSong?.video_id,
+                        duration: extraParams.createdSong?.duration,
+                        thumbnail_url: extraParams.createdSong?.thumbnail_url,
+                        created_at: extraParams.createdSong?.created_at
+                    });
+                });
             });
         });
     });
