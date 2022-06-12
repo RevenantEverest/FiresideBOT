6f7d18806281f5d1cd2a22806846853385096693
Stefan Fischer <stefischer@mail.com>
2022-06-12 03:44:04 -0400

    Update promise types to use QueryFailedError

--- api/src/types/promises.ts
+++ api/src/types/promises.ts
@@ -1,2 +1,3 @@
+import { QueryFailedError } from 'typeorm';
 
-export type HandleReturn<T> = Promise<[T | null | undefined, Error | undefined]>;
+export type HandleReturn<T> = Promise<[T | null | undefined, Error | QueryFailedError | undefined]>;
