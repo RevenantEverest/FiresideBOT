0a093718e4831b34febd43d2e54e45876efd8133
Stefan Fischer <stefischer@mail.com>
2022-06-12 03:47:35 -0400

    Add variables to environment constants

--- discord/src/constants/environment.ts
+++ discord/src/constants/environment.ts
@@ -5,7 +5,10 @@ dotenv.config();
 export const IS_DEV = process.env.ENVIRONMENT === "DEV";
 export const IS_TEST = process.env.NODE_ENV === "test";
 
+export const API_URL = process.env.API_URL as string;
 export const API_PORT = process.env.API_PORT as string;
+
+export const TOKEN_SECRET = process.env.TOKEN_SECRET as string;
 
 export const DISCORD = {
     CLIENT_ID: process.env.DISCORD_CLIENT_ID as string,
