e72ae378eeff769faf21adfabe33c06dbec9e2ab
Stefan Fischer <stefischer@mail.com>
2022-06-12 03:45:44 -0400

    Add QueryFailedError to entities types

--- api/src/types/entities.ts
+++ api/src/types/entities.ts
@@ -1,4 +1,6 @@
-export type OperationReturn<T> = Promise<[T | undefined, Error | undefined]>;
+import { QueryFailedError } from 'typeorm';
+
+export type OperationReturn<T> = Promise<[T | undefined, Error | QueryFailedError | undefined]>;
 export interface IndexOptions {
     offset?: number,
     limit?: number,
