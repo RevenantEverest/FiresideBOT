9e97e37f18368d878ce5542244181e7597d5a4cb
Stefan Fischer <stefischer@mail.com>
2022-06-12 03:40:42 -0400

    Add setting default to default constants

--- api/src/constants/defaults.ts
+++ api/src/constants/defaults.ts
@@ -1,1 +1,17 @@
-export const LIMIT = 10;
+export const LIMIT = 10; // Pagination Limit
+
+export const GENERAL_SETTINGS = {
+    PREFIX: "?",
+    VOLUME: 50
+};
+
+export const RANK_SETTINGS = {
+    GENERAL_INCREASE_RATE: 10,
+    COMPLEXITY: 2,
+    RANK_CHANNEL: null
+};
+
+export const ECONOMY_SETTINGS = {
+    CURRENCY_NAME: "Kindling",
+    INCREASE_RATE: 10
+};
