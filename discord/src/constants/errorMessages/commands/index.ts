ec9993800eed289d1f1dc04f29514a9cf5871d03
Stefan Fischer <stefischer@mail.com>
2022-07-14 16:27:50 -0400

    Add premium limits, urls and command error messages to constants

---
+++ discord/src/constants/errorMessages/commands/index.ts
@@ -0,0 +1,4 @@
+export * as PLAY from './play.js';
+export * as VOLUME from './volume.js';
+
+export const NOT_IN_VOICE_CHANNEL = "You must be in a voice channel";
---
+++ discord/src/constants/errorMessages/premium.ts
@@ -0,0 +1,2 @@
+export const REQUEST_DURATION = "Non premium requests limited to 1 hour";
+export const PLAYLIST_SONG_DURATION = "Non Premium Playlist Songs Limited To 10 Minutes";
