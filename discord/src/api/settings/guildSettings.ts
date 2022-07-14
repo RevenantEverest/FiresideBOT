42bf7c79e19d4fa706b4a2c6cf08f7efa3b98372
Stefan Fischer <stefischer@mail.com>
2022-07-14 16:44:49 -0400

    Add update method to guildSettings api services

--- discord/src/api/settings/guildSettings.ts
+++ discord/src/api/settings/guildSettings.ts
@@ -10,10 +10,32 @@ import { promises } from '../../utils/index.js';
 
 const baseEndpoint = ENV.API_URL + "/settings/guild";
 
-export async function getGuildSettings(guildId: string, message: Message): HandleReturn<GuildSettings> {
+export async function get(guildId: string, message: Message): HandleReturn<GuildSettings> {
     const token = await issueToken(message);
     
     const request = axios.get(`${baseEndpoint}/${guildId}`, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
+
+    const [res, err] = await promises.handle<AxiosResponse>(request);
+
+    if(err) {
+        return [undefined, err];
+    }
+
+    if(!res || !res.data.results) {
+        return [undefined, new Error("No Guild Settings Returned")];
+    }
+
+    return [(res.data.results as GuildSettings), undefined];
+};
+
+export async function update(guildId: string, message: Message, guildSettings: GuildSettings): HandleReturn<GuildSettings> {
+    const token = await issueToken(message);
+
+    const request = axios.put(`${baseEndpoint}/${guildId}`, guildSettings, {
         headers: {
             Authorization: `Bearer ${token}`
         }
