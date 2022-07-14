75446dff75ea7d804121790cbb1fd54fc2c68bd9
Stefan Fischer <stefischer@mail.com>
2022-07-14 16:43:38 -0400

    Update permissions for guildSettingRoutes

--- api/src/routes/settings/guildSettingRoutes.ts
+++ api/src/routes/settings/guildSettingRoutes.ts
@@ -9,7 +9,7 @@ router.route("/")
 .get(extractPaginationParams, guildSettingsController.index)
 
 router.route("/:guildId")
-.get(validation.guildId, permissions.isGuildAdmin, guildSettingsController.getByGuildId)
+.get(validation.guildId, permissions.isGuildMember, guildSettingsController.getByGuildId)
 .post(validation.guildId, permissions.isGuildAdmin, guildSettingsController.create)
 .put(validation.guildId, permissions.isGuildAdmin, guildSettingsController.update)
 
