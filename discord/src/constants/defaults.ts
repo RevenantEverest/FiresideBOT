ef55fa9ac0d90659ab43dfe78d5103b49e0736d5
Stefan Fischer <stefischer@mail.com>
2022-07-14 16:22:47 -0400

    Update types for default constants

--- discord/src/constants/defaults.ts
+++ discord/src/constants/defaults.ts
@@ -1,19 +1,20 @@
 import { GuildResolvable } from 'discord.js';
+import { GuildSettings } from '../types/entities/GuildSettings.js';
 import config from '../config/index.js';
 
 export const VOLUME = 50;
 
-export async function generateDefaultServer(guildId: GuildResolvable) {
+export async function generateDefaultServer(guildId: GuildResolvable, guildSettings: GuildSettings) {
     config.servers.push({
         id: guildId,
         premium: false,
         queue: {
             playing: false,
             info: [],
-            currentSongEmbed: [],
+            currentSongEmbed: null,
             genres: [],
             options: {
-                volume: VOLUME,
+                volume: guildSettings.volume ? guildSettings.volume : VOLUME,
                 loop: false,
                 recommendations: false,
                 voteToSkip: false
