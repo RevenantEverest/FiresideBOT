a7901068837bdb699731d9663f593b1a98f497b5
Stefan Fischer <stefischer@mail.com>
2022-07-16 18:55:26 -0400

    Fix start staging script

--- scripts/start_staging.sh
+++ scripts/start_staging.sh
@@ -4,4 +4,4 @@ cd "$(dirname "$0")/.." || exit
 
 docker-compose stop
 
-docker-compose -f docker-compose.yml -f override.dev.yml build api discord && docker-compose -f docker-compose.yml -f override.dev.yml up api discord -d
+docker-compose -f docker-compose.yml -f override.dev.yml build api discord && docker-compose -f docker-compose.yml -f override.dev.yml up -d api discord
