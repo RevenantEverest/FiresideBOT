6415afc20d3fa17a6d38ffa69303de4ce641a975
Stefan Fischer <stefischer@mail.com>
2022-07-16 02:07:07 -0400

    Update Pong command to use slashCommands

--- discord/src/commands/Info/Ping/index.ts
+++ discord/src/commands/Info/Ping/index.ts
@@ -1,13 +1,18 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
 import { CommandParams, CommandConfigParams } from '../../../types/commands.js';
 
-function Ping({ message }: CommandParams) {
-    return message.reply("Pong");
+async function Ping({ dispatch }: CommandParams) {
+    return await dispatch.reply("Pong");
 };
 
 export const config: CommandConfigParams = {
     aliases: [],
-    description: "",
+    description: "Check Fireside's ping",
     example: ""
 };
+
+export const slashCommand = new SlashCommandBuilder()
+.setName("ping")
+.setDescription(config.description);
 
 export default Ping;
