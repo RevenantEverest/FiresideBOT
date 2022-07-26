1fe7d51b92d86431051ff8275b31a4dce8a93814
Stefan Fischer <stefischer@mail.com>
2022-07-26 13:28:42 -0400

    Test application_stop script

--- scripts/deployment/application_stop.sh
+++ scripts/deployment/application_stop.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Stopping Docker Containers..."
 
-if [ $( docker ps -a | wc -l ) -gt 1 ]; then
-  echo docker stop $(docker container ls -q) && docker system prune -a -f
-else
-  echo "No docker containers running, skipping..."
-fi
+# if [ $( docker ps -a | wc -l ) -gt 1 ]; then
+#   echo docker stop $(docker container ls -q) && docker system prune -a -f
+# else
+#   echo "No docker containers running, skipping..."
+# fi
