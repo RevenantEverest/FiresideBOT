ec5dfb40feb4b56fdc6e1d225ab4bec50c62bb06
Stefan Fischer <stefischer@mail.com>
2022-07-27 04:41:14 -0400

    Add paginated embed support in embed utils

--- discord/src/utils/embeds.ts
+++ discord/src/utils/embeds.ts
@@ -1,9 +1,11 @@
-import Discord, { MessageEmbed } from 'discord.js';
+import Discord, { Message, MessageEmbed, MessageReaction, User } from 'discord.js';
 
 import { CommandDispatch } from '../types/commands.js';
 import { Server } from '../types/server.js';
+import { PaginatedEmbed } from '../types/embeds.js';
+import { GuildInteraction } from '../types/interaction.js';
 
-import { URLS } from '../constants/index.js';
+import { URLS, DEFAULTS } from '../constants/index.js';
 import * as colors from './colors.js';
 import * as dates from './dates.js';
 
@@ -28,8 +30,94 @@ export function createCurrentSongEmbed(dispatch: CommandDispatch, server: Server
     dispatch.channel.send({ embeds: [embed] });
 };
 
-export function createPaginatedEmbed(dispatch: CommandDispatch, embeds: MessageEmbed[], options: Object) {
+function generateEmbed(index: number, paginatedEmbed: PaginatedEmbed): MessageEmbed {
+    const embed = new Discord.MessageEmbed({ color: paginatedEmbed.color });
+
+    if(paginatedEmbed.title) {
+        embed.setTitle(paginatedEmbed.title);
+    }
+
+    if(paginatedEmbed.author) {
+        embed.setAuthor(paginatedEmbed.author);
+    }
+
+    if(paginatedEmbed.thumbnail) {
+        embed.setThumbnail(paginatedEmbed.thumbnail);
+    }
+
+    if(paginatedEmbed.content.length > 1) {
+        embed.setFooter({
+            text: `Page ${index + 1}/${paginatedEmbed.content.length}`
+        });
+    }
+
+    paginatedEmbed.content[index].fields?.forEach(field => embed.addField(field.name, field.value, field.inline));
+
+    return embed;
+};
+
+async function handleReactionPagination(message: Message, index: number, paginatedEmbed: PaginatedEmbed) {
+    const navigatorEmojis = {
+        next: "▶️",
+        stop: "⏹️",
+        prev: "◀️"
+    };
+
+    await message.react(navigatorEmojis.prev);
+    await message.react(navigatorEmojis.stop);
+    await message.react(navigatorEmojis.next);
+
+    const collector = new Discord.ReactionCollector(message, { 
+        time: (paginatedEmbed.time ?? DEFAULTS.PAGINATED_EMBED_TIME) * 60000 
+    });
+
+    collector.on("collect", async (reaction: MessageReaction, user: User) => {
+        if(user.bot) return;
+        
+        const contentLength = paginatedEmbed.content.length - 1;
+
+        switch(reaction.emoji.name) {
+            case navigatorEmojis.next:
+                index === contentLength ? index = 0 : index++;
+                break;
+            case navigatorEmojis.prev:
+                index === 0 ? index = contentLength : index--;
+                break;
+            case navigatorEmojis.stop: 
+                return collector.stop();
+            default:
+                return;
+        };
+
+        const embed = generateEmbed(index, paginatedEmbed);
+        reaction.message.edit({
+            content: paginatedEmbed.flavorText,
+            embeds: [embed]
+        });
+    });   
+    
+    collector.on("end", async () => {
+        message.reactions.removeAll();
+    });
+};
+
+export async function pagination(dispatch: CommandDispatch, paginatedEmbed: PaginatedEmbed, interaction?: GuildInteraction) {
 
     const index = 0;
-    // const embed = 
+    const embed = generateEmbed(index, paginatedEmbed);
+
+    if(paginatedEmbed.content.length <= 1) {
+        return dispatch.reply({ embeds: [embed] });
+    }
+
+    if(interaction) {
+        dispatch.reply("Generating queue...");
+        return dispatch.channel.send({ embeds: [embed] })
+        .then((msg: Message) => handleReactionPagination(msg, index, paginatedEmbed))
+        .catch((err: Error) => console.error(err));
+    }
+
+    dispatch.reply({ embeds: [embed] })
+    .then((msg: Message) => handleReactionPagination(msg, index, paginatedEmbed))
+    .catch((err: Error) => console.error(err));
 };
