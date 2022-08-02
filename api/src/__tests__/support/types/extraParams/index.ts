63da36b34a9da7c1e14ab1e932dc56e9037ae7da
Stefan Fischer <stefischer@mail.com>
2022-08-02 02:33:53 -0400

    Add GuildSettings extra params

--- api/src/__tests__/support/types/extraParams/index.ts
+++ api/src/__tests__/support/types/extraParams/index.ts
@@ -2,5 +2,7 @@ export * from './guildPlaylist.params.js';
 export * from './guildPlaylistRole.params.js';
 export * from './guildSong.params.js';
 
+export * from './guildSettings.params.js';
+
 export * from './userPlaylist.params.js';
 export * from './userSong.params.js';
