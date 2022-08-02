eaf61798cf0b7354d89313d6a436eeb8d3c7cf04
Stefan Fischer <stefischer@mail.com>
2022-08-02 02:32:52 -0400

    Lint guildSettingsRoutes

--- api/src/routes/settings/guildSettingRoutes.ts
+++ api/src/routes/settings/guildSettingRoutes.ts
@@ -1,4 +1,3 @@
-
 import express from 'express';
 import { guildSettingsController } from '../../controllers/settings/index.js';
 import { extractPaginationParams, permissions, validation } from '../../middleware/index.js';
