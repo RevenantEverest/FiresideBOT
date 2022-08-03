5e02c81eaa2aeb3441eaba5a08619b19a0eca07e
Stefan Fischer <33422278+RevenantEverest@users.noreply.github.com>
2022-08-03 00:29:32 -0400

    Remove 1.0 code base common js modules (#65)
    Remove 1.0 common js modules

--- background/controllers/DiscordEvents/Guild/onGuildUpdate.js
+++
@@ -1,8 +0,0 @@
-const Discord = require('discord.js');
-const moment = require('moment');
-const guildsController = require('../../guildsController');
-
-module.exports = async (bot, oldGuild, newGuild) => {
-    if(oldGuild.name !== newGuild.name)
-        guildsController.updateGuild(bot, oldGuild, newGuild)
-};
--- youtube_tracker/Discord_Bot.js
+++
@@ -1,7 +0,0 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
-
-bot.on("ready", () => require('./controllers/DiscordEvents/onReady')(bot));
-bot.on("error", (err) => require('./controllers/DiscordEvents/onError')(bot, err));
-
-module.exports = bot;
