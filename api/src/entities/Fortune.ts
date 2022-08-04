03ab8fcacf90470604c8872280df15817216f9c2
Stefan Fischer <stefischer@mail.com>
2022-08-04 11:54:38 -0400

    Remove updated_at column in Fortune entity

--- api/src/entities/Fortune.ts
+++ api/src/entities/Fortune.ts
@@ -3,8 +3,7 @@ import {
     BaseEntity,
     PrimaryGeneratedColumn,
     Column,
-    CreateDateColumn,
-    UpdateDateColumn
+    CreateDateColumn
 } from 'typeorm';
 
 @Entity('fortunes')
@@ -15,8 +14,7 @@ class Fortune extends BaseEntity {
         guild_id: string,
         fortune: string,
         created_by: string,
-        created_at: Date,
-        updated_at: Date
+        created_at: Date
     ) {
         super();
         this.id = id;
@@ -24,27 +22,23 @@ class Fortune extends BaseEntity {
         this.fortune = fortune;
         this.created_by = created_by;
         this.created_at = created_at;
-        this.updated_at = updated_at;
     };
 
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column({ type: "varchar", length: 20, unique: true })
+    @Column({ type: "varchar", length: 20 })
     guild_id: string;
 
     @Column({ type: "varchar", length: 1024, unique: true })
     fortune: string;
 
     /* Discord ID of the user who created the Fortune */
-    @Column({ type: "varchar", length: 20, unique: true })
+    @Column({ type: "varchar", length: 20 })
     created_by: string;
 
     @CreateDateColumn()
     created_at: Date;
-
-    @UpdateDateColumn()
-    updated_at: Date;
 };
 
 export default Fortune;
