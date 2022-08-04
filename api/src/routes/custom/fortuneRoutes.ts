0b0fbe8f3e24aaf8c40114ca45997e9ef463ff33
Stefan Fischer <stefischer@mail.com>
2022-08-04 11:54:24 -0400

    Import and implement fortunesController in fortuneRoutes

--- api/src/routes/custom/fortuneRoutes.ts
+++ api/src/routes/custom/fortuneRoutes.ts
@@ -1,18 +1,19 @@
 import express from 'express';
+import { fortunesController } from '../../controllers/custom/index.js';
 import { extractPaginationParams, permissions, validation } from '../../middleware/index.js';
 
 const router = express.Router();
 const validationMiddleware = [validation.guildId, validation.id];
 
 router.route("/")
-.get(extractPaginationParams)
+.get(extractPaginationParams, fortunesController.index)
 
 router.route("/guild/:guildId")
-.get(extractPaginationParams, validation.guildId, permissions.isGuildMember)
-.post(validation.guildId, permissions.isGuildAdmin)
+.get(extractPaginationParams, validation.guildId, permissions.isGuildMember, fortunesController.getByGuildId)
+.post(validation.guildId, permissions.isGuildAdmin, fortunesController.create)
 
 router.route("/guild/:guildId/id/:id")
-.get(validationMiddleware, permissions.isGuildMember)
-.delete(validationMiddleware, permissions.isGuildAdmin)
+.get(validationMiddleware, permissions.isGuildMember, fortunesController.getOne)
+.delete(validationMiddleware, permissions.isGuildAdmin, fortunesController.destroy)
 
 export default router;
