9131daa7e51aaeb5f7a36afa8535f5c332a7ab41
Stefan Fischer <stefischer@mail.com>
2022-08-12 07:14:30 -0400

    Update pagination utils next and prev page url strings

--- api/src/utils/pagination.ts
+++ api/src/utils/pagination.ts
@@ -4,6 +4,8 @@ import { ENV } from '../constants/index.js';
 
 export function paginateResponse<T>(req: Request, res: Response, results: [T[], number]): PaginatedResponse<T> {
 
+    const baseUrl = ENV.IS_DEV ? `${ENV.BASE_URL}:${ENV.API_PORT}` : ENV.BASE_URL;
+
     const { page, limit } = res.locals;
     const regEx: RegExp = /(?=\?)(.*)/gi;
     const requestedUrl: string = req.originalUrl.replace(regEx, "");
@@ -13,11 +15,11 @@ export function paginateResponse<T>(req: Request, res: Response, results: [T[], 
     let prev: PaginationUrl = null;
 
     if((page * limit) < count) {
-        next = ENV.BASE_URL! + requestedUrl + `?page=${page + 1}`;
+        next = baseUrl + requestedUrl + `?page=${page + 1}`;
     }
 
     if(page > 1) {
-        prev = ENV.BASE_URL! + requestedUrl + `?page=${page - 1}`;
+        prev = baseUrl + requestedUrl + `?page=${page - 1}`;
     }
 
     return {
