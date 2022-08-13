b7e012b5e55bfc428d921f0fffa4ec5014e40a09
Stefan Fischer <stefischer@mail.com>
2022-08-13 03:36:30 -0400

    Pass deferredReply param to voiceConnection utils

--- discord/src/utils/voiceConnection.ts
+++ discord/src/utils/voiceConnection.ts
@@ -6,18 +6,18 @@ import { Server } from '../types/server.js';
 
 import * as audio from './audio.js';
 
-export function createConnection(bot: Client, dispatch: CommandDispatch, server: Server,) {
+export function createConnection(bot: Client, dispatch: CommandDispatch, server: Server,  deferredReply?: boolean) {
     if(!server.queue.connection) {
         if(!dispatch.member.voice.channel) {
-            return dispatch.reply("Voice Channel is unavailable");
+            return dispatch.reply("Voice Channel is unavailable", deferredReply);
         }
 
         if(!dispatch.member.voice.channel.joinable) {
-            return dispatch.reply("Fireside is unable to join this voice channel");
+            return dispatch.reply("Fireside is unable to join this voice channel", deferredReply);
         }
 
         const connection = joinVoiceChannel({
-            guildId: dispatch.guildId,
+            guildId: (dispatch.guildId as string),
             channelId: dispatch.member.voice.channel.id,
             adapterCreator: dispatch.guild.voiceAdapterCreator
         });
