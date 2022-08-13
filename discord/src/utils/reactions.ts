a3b7819dd3642eeb287cc070e833614091500eef
Stefan Fischer <stefischer@mail.com>
2022-08-13 03:35:56 -0400

    Update reactions navigator method to handle api pagination only in forward navigation

--- discord/src/utils/reactions.ts
+++ discord/src/utils/reactions.ts
@@ -1,30 +1,110 @@
-import Discord, { MessageReaction, User } from 'discord.js';
-import { GuildMessage } from '../types/message';
+import Discord, { MessageReaction, User, Message } from 'discord.js';
+import { PaginatedEmbed } from '../types/embeds.js';
+import { ApiPaginationOptions, GetPageResponse } from '../types/pagination.js';
 
-export async function createReactionNavigator(message: GuildMessage, handleNext: Function, handlePrev: Function) {
+import { DEFAULTS } from '../constants/index.js';
+import * as embeds from './embeds.js';
 
-    const navigatorEmojis = {
-        next: "◀️",
-        stop: "⏹️",
-        prev: "▶️"
+function setUpdatedPaginationOptions<T>(getPageRes: GetPageResponse<T>, paginationOptions: ApiPaginationOptions<T>): ApiPaginationOptions<T> {
+    const { hasMore, data, nextPageIndex, prevPageIndex } = getPageRes;
+
+    return {
+        ...paginationOptions,
+        hasMore, 
+        data, 
+        nextPageIndex, 
+        prevPageIndex
     };
-    const collector = new Discord.ReactionCollector(message);
+};
+
+function getContentLength<T>(paginatedEmbed: PaginatedEmbed, paginationOptions?: ApiPaginationOptions<T>): number {
+    if(paginationOptions) {
+        return Math.ceil(paginationOptions?.count / paginationOptions?.amountPerPage) - 1;
+    }
+
+    return paginatedEmbed.content.length - 1;
+};
+
+export async function createReactionNavigator<T>(message: Message, index: number, paginatedEmbed: PaginatedEmbed, paginationOptions?: ApiPaginationOptions<T>) {
+    const navigatorEmojis = {
+        next: "▶️",
+        stop: "⏹️",
+        prev: "◀️"
+    };
+
+    await message.react(navigatorEmojis.prev);
+    await message.react(navigatorEmojis.stop);
+    await message.react(navigatorEmojis.next);
+
+    const collector = new Discord.ReactionCollector(message, { 
+        time: (paginatedEmbed.time ?? DEFAULTS.PAGINATED_EMBED_TIME) * 60000 
+    });
 
     collector.on("collect", async (reaction: MessageReaction, user: User) => {
-        await message.react(navigatorEmojis.prev);
-        await message.react(navigatorEmojis.stop);
-        await message.react(navigatorEmojis.next);
+        if(user.bot) return;
+        
+        const apiRequestInterval = Math.ceil(DEFAULTS.API_PAGINATION.LIMIT / (paginationOptions?.amountPerPage ?? 0));
 
         switch(reaction.emoji.name) {
             case navigatorEmojis.next:
-                return handleNext();
+
+                if(index % apiRequestInterval - 1 === 0 && paginationOptions?.hasMore && paginationOptions.nextPageIndex) {
+                    const { nextPageIndex } = paginationOptions;
+
+                    const [res, err] = await paginationOptions.getPage(nextPageIndex, paginationOptions.data);
+
+                    if(!res || err) {
+                        return collector.stop();
+                    }
+
+                    paginationOptions = setUpdatedPaginationOptions<T>(res, paginationOptions);
+                    paginatedEmbed = res.paginatedEmbed;
+                }
+
+                index === getContentLength(paginatedEmbed, paginationOptions) ? index = 0 : index++;
+                break;
             case navigatorEmojis.prev:
-                return handlePrev();
+
+                /*
+                
+                    Edge Case:
+
+                    If a user naviagtes backwards, handle getting proper pagination page
+
+                    If the user coninues to scroll back get more updated pages
+                    Try to find a way to keep track of the pages called by checking against highest 
+                
+                */
+                const adjustedIndex = index === getContentLength(paginatedEmbed, paginationOptions) ? 0 : index + 1;
+                const shouldRequestPage = apiRequestInterval % adjustedIndex === 0;
+
+                console.log("Checking conditonal => ", adjustedIndex, shouldRequestPage);
+                console.log("Second check => ", paginationOptions?.data.length, paginationOptions?.count);
+
+                if(paginationOptions && paginationOptions.data.length !== paginationOptions.count || shouldRequestPage && paginationOptions?.hasMore) {
+                    const [res, err] = await paginationOptions.getPage(2, paginationOptions.data);
+
+                    if(!res || err) {
+                        return collector.stop();
+                    }
+
+                    paginationOptions = setUpdatedPaginationOptions<T>(res, paginationOptions);
+                    paginatedEmbed = res.paginatedEmbed;
+                }
+
+                index === 0 ? index = getContentLength(paginatedEmbed, paginationOptions) : index--;
+                break;
             case navigatorEmojis.stop: 
                 return collector.stop();
             default:
                 return;
         };
+
+        const embed = embeds.generateEmbed(index, paginatedEmbed, paginationOptions);
+        reaction.message.edit({
+            content: paginatedEmbed.flavorText,
+            embeds: [embed]
+        });
     });   
     
     collector.on("end", async () => {
