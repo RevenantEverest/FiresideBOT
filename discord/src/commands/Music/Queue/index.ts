3b8e27c53ed77d00e50462a61b14d291a6bbed6d
Stefan Fischer <stefischer@mail.com>
2022-08-13 03:40:55 -0400

    Update Queue command to use new pagination elements

--- discord/src/commands/Music/Queue/index.ts
+++ discord/src/commands/Music/Queue/index.ts
@@ -1,43 +1,42 @@
-import Discord, { MessageEmbedAuthor, MessageEmbedOptions, ColorResolvable } from 'discord.js';
 import { SlashCommandBuilder } from '@discordjs/builders';
 
 import { CommandParams, CommandConfigParams } from '../../../types/commands.js';
 import { PaginatedEmbed } from '../../../types/embeds.js';
+import { ServerSongInfo } from '../../../types/server.js';
+
 import { colors, dates, embeds } from '../../../utils/index.js';
 
-async function Queue({ dispatch, server, interaction}: CommandParams) {
+async function Queue({ dispatch, server}: CommandParams) {
     if(server.queue.info.length < 1) {
         return dispatch.reply("No other songs in queue.");
     }
 
     const queue = server.queue.info;
     const overalLength = queue.map(info => info.duration).reduce((a, b) => a + b);
-    
+
+    const amountPerPage: number = 5;
     const paginatedEmbed: PaginatedEmbed = {
         title: `**Queue** (${dates.parseSeconds(overalLength)})`,
         author: {
-            url: dispatch.guild.iconURL({ dynamic: true }) ?? "",
+            iconURL: dispatch.guild.iconURL({ dynamic: true }) ?? "",
             name: dispatch.guild.name
             
         },
         color: colors.queueEmbed,
-        content: []
-    };
+        content: embeds.generatePaginatedEmbedFields<ServerSongInfo>({
+            data: queue,
+            amountPerPage,
+            setFieldName: (songInfo: ServerSongInfo, index: number, startIndex: number): string => {
+                return `${(startIndex + index) + 1}. ${songInfo.title}`
+            },
+            setFieldValue: (songInfo: ServerSongInfo) => {
+                const { author, duration } = songInfo;
+                return `**Author:** ${author}\n**Duration:** ${dates.parseSeconds(duration)}`;
+            }
+        })
+    }
 
-    for(let i = 0; i < Math.ceil(queue.length / 5); i++) {
-        const startIndex = ((i + 1) * 5) - 5;
-        const endIndex = ((i + 1) * 5) - 1;
-        paginatedEmbed.content.push({
-            fields: queue.slice(startIndex, endIndex).map((info, index) => {
-                return{
-                    name: `${(startIndex + index) + 1}. ${info.title}`,
-                    value: `**Author:** ${info.author}\n**Duration:** ${dates.parseSeconds(info.duration)}`
-                }
-            })
-        });
-    };
-
-    return embeds.pagination(dispatch, paginatedEmbed, interaction);
+    return embeds.pagination<ServerSongInfo>(dispatch, paginatedEmbed);
 };
 
 export const config: CommandConfigParams = {
