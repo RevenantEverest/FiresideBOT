420fb596c8b019caad299334415781fb5b0e85b9
Stefan Fischer <stefischer@mail.com>
2022-08-13 03:35:03 -0400

    Create pagination types

---
+++ discord/src/types/pagination.ts
@@ -0,0 +1,19 @@
+import { HandleReturn } from './promises.js';
+import { PaginatedEmbed } from './embeds.js';
+
+export interface PartialApiPaginationOptions<T> {
+    hasMore: boolean,
+    data: T[],
+    count: number,
+    nextPageIndex: number | null,
+    prevPageIndex: number | null
+};
+
+export interface ApiPaginationOptions<T> extends PartialApiPaginationOptions<T> {
+    amountPerPage: number,
+    getPage: (page: number, data: T[]) => HandleReturn<GetPageResponse<T>>,
+};
+
+export interface GetPageResponse<T> extends PartialApiPaginationOptions<T> {
+    paginatedEmbed: PaginatedEmbed
+}
