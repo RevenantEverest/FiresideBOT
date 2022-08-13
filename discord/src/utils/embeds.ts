6c20b9e9f67886f4ce8ddd3dbbf1d9d1129c45ca
Stefan Fischer <stefischer@mail.com>
2022-08-13 03:31:17 -0400

    Add generatePaginatedEmbedFields method to embeds utils

--- discord/src/utils/embeds.ts
+++ discord/src/utils/embeds.ts
@@ -1,13 +1,42 @@
-import Discord, { Message, MessageEmbed, MessageReaction, User } from 'discord.js';
+import Discord, { Message, MessageEmbed, MessageEmbedOptions } from 'discord.js';
 
 import { CommandDispatch } from '../types/commands.js';
 import { Server } from '../types/server.js';
 import { PaginatedEmbed } from '../types/embeds.js';
-import { GuildInteraction } from '../types/interaction.js';
+import { ApiPaginationOptions } from '../types/pagination.js';
 
-import { URLS, DEFAULTS } from '../constants/index.js';
+import { URLS } from '../constants/index.js';
 import * as colors from './colors.js';
 import * as dates from './dates.js';
+import * as reactions from './reactions.js';
+
+type SetFieldElement<T> = (element: T, index: number, startIndex: number, endIndex: number) => string;
+
+interface GeneratePaginatedEmbedFieldsParams<T> {
+    data: T[],
+    amountPerPage: number,
+    setFieldName: SetFieldElement<T>,
+    setFieldValue: SetFieldElement<T>
+};
+
+export function generatePaginatedEmbedFields<T>({ data, amountPerPage, setFieldName, setFieldValue }: GeneratePaginatedEmbedFieldsParams<T>): MessageEmbedOptions[] {
+    let arr = [];
+    for(let i = 0; i < Math.ceil(data.length / amountPerPage); i++) {
+        const startIndex = ((i + 1) * amountPerPage) - amountPerPage;
+        const endIndex = ((i + 1) * amountPerPage);
+        
+        arr.push({
+            fields: data.slice(startIndex, endIndex).map((element: T, index: number) => {
+                return{
+                    name: setFieldName(element, index, startIndex, endIndex),
+                    value: setFieldValue(element, index, startIndex, endIndex)
+                }
+            })
+        });
+    };
+
+    return arr;
+};
 
 export function createCurrentSongEmbed(dispatch: CommandDispatch, server: Server) {
     const currentSong = server.queue.info[0];
@@ -30,7 +59,7 @@ export function createCurrentSongEmbed(dispatch: CommandDispatch, server: Server
     dispatch.channel.send({ embeds: [embed] });
 };
 
-function generateEmbed(index: number, paginatedEmbed: PaginatedEmbed): MessageEmbed {
+export function generateEmbed<T>(index: number, paginatedEmbed: PaginatedEmbed, paginationOptions?: ApiPaginationOptions<T>): MessageEmbed {
     const embed = new Discord.MessageEmbed({ color: paginatedEmbed.color });
 
     if(paginatedEmbed.title) {
@@ -46,78 +75,40 @@ function generateEmbed(index: number, paginatedEmbed: PaginatedEmbed): MessageEm
     }
 
     if(paginatedEmbed.content.length > 1) {
+        let maxLength = paginatedEmbed.content.length;
+        
+        if(paginationOptions?.count) {
+            maxLength = Math.ceil(paginationOptions.count / paginationOptions.amountPerPage);
+        }
+
         embed.setFooter({
-            text: `Page ${index + 1}/${paginatedEmbed.content.length}`
+            text: `Page ${index + 1}/${maxLength}`
         });
     }
 
+    // console.log("Inside embeds => ", index, paginatedEmbed.content[index] && paginatedEmbed.content[index].fields?.length);
     paginatedEmbed.content[index].fields?.forEach(field => embed.addField(field.name, field.value, field.inline));
 
     return embed;
 };
 
-async function handleReactionPagination(message: Message, index: number, paginatedEmbed: PaginatedEmbed) {
-    const navigatorEmojis = {
-        next: "▶️",
-        stop: "⏹️",
-        prev: "◀️"
-    };
-
-    await message.react(navigatorEmojis.prev);
-    await message.react(navigatorEmojis.stop);
-    await message.react(navigatorEmojis.next);
-
-    const collector = new Discord.ReactionCollector(message, { 
-        time: (paginatedEmbed.time ?? DEFAULTS.PAGINATED_EMBED_TIME) * 60000 
-    });
-
-    collector.on("collect", async (reaction: MessageReaction, user: User) => {
-        if(user.bot) return;
-        
-        const contentLength = paginatedEmbed.content.length - 1;
-
-        switch(reaction.emoji.name) {
-            case navigatorEmojis.next:
-                index === contentLength ? index = 0 : index++;
-                break;
-            case navigatorEmojis.prev:
-                index === 0 ? index = contentLength : index--;
-                break;
-            case navigatorEmojis.stop: 
-                return collector.stop();
-            default:
-                return;
-        };
-
-        const embed = generateEmbed(index, paginatedEmbed);
-        reaction.message.edit({
-            content: paginatedEmbed.flavorText,
-            embeds: [embed]
-        });
-    });   
-    
-    collector.on("end", async () => {
-        message.reactions.removeAll();
-    });
-};
-
-export async function pagination(dispatch: CommandDispatch, paginatedEmbed: PaginatedEmbed, interaction?: GuildInteraction) {
+export async function pagination<T>(dispatch: CommandDispatch, paginatedEmbed: PaginatedEmbed, paginationOptions?: ApiPaginationOptions<T>) {
 
     const index = 0;
-    const embed = generateEmbed(index, paginatedEmbed);
+    const embed = generateEmbed(index, paginatedEmbed, paginationOptions);
 
     if(paginatedEmbed.content.length <= 1) {
         return dispatch.reply({ embeds: [embed] });
     }
 
-    if(interaction) {
+    if(dispatch.interaction) {
         dispatch.reply("Generating queue...");
         return dispatch.channel.send({ embeds: [embed] })
-        .then((msg: Message) => handleReactionPagination(msg, index, paginatedEmbed))
+        .then((msg: Message) => reactions.createReactionNavigator(msg, index, paginatedEmbed, paginationOptions))
         .catch((err: Error) => console.error(err));
     }
 
     dispatch.reply({ embeds: [embed] })
-    .then((msg: Message) => handleReactionPagination(msg, index, paginatedEmbed))
+    .then((msg: Message) => reactions.createReactionNavigator(msg, index, paginatedEmbed, paginationOptions))
     .catch((err: Error) => console.error(err));
 };
