12e1d73e9d95ee283f9c1e983f8481c7faa80362
Stefan Fischer <stefischer@mail.com>
2022-08-13 03:33:16 -0400

    Add Admin command category

---
+++ discord/src/commands/Admin/index.config.ts
@@ -0,0 +1,7 @@
+import { CommandCategoryFileConfig } from '../../types/commands.js';
+
+const config: CommandCategoryFileConfig = {
+    description: "Edit Fireside Settings"
+};
+
+export default config;
