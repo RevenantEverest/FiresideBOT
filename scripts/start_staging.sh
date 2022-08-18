40ddba1d21d8f9282bea17636192f621e0b48856
Stefan Fischer <stefischer@mail.com>
2022-08-17 23:24:29 -0400

    Fix start staging script and fix missing dependency in override staging yml

--- scripts/start_staging.sh
+++ scripts/start_staging.sh
@@ -4,4 +4,4 @@ cd "$(dirname "$0")/.." || exit
 
 docker-compose stop
 
-docker-compose -f docker-compose.yml -f override.dev.yml build api discord && docker-compose -f docker-compose.yml -f override.dev.yml up -d api discord
+docker-compose -f docker-compose.yml -f override.staging.yml build api discord && docker-compose -f docker-compose.yml -f override.staging.yml up -d api discord
