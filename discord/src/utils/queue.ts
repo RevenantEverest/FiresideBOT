e2693c5e530c4555e9ae77a56cf464f9b03c092a
Stefan Fischer <stefischer@mail.com>
2022-08-29 17:21:53 -0400

    Add resetQueue and handleDisconnectTimer methods to queue utils

--- discord/src/utils/queue.ts
+++ discord/src/utils/queue.ts
@@ -14,11 +14,47 @@ export function nextInQueue(bot: Client, dispatch: CommandDispatch, server: Serv
             audio.stream(bot, dispatch, server);
         }
         else {
-            server.queue.playing = false;
+            resetQueue(server);
+            handleDisconnectTimer(server);
             dispatch.channel.send("Queue concluded");
         }
     }
     catch(err) {
         console.error(err);
+    }
+};
+
+export function handleDisconnectTimer(server: Server) {
+    if(server.queue.disconnectTimer) {
+        clearTimeout(server.queue.disconnectTimer);
+        server.queue.disconnectTimer = null;
+    }
+
+    if(server.queue.playing) return;
+
+    server.queue.disconnectTimer = setTimeout(() => {
+        return resetQueue(server, true);
+    }, 10000);
+};
+
+export function resetQueue(server: Server, withConnection?: boolean) {
+    server.queue = {
+        ...server.queue,
+        playing: false,
+        isPaused: false,
+        info: [],
+        currentSongInfo: null,
+        currentSongEmbed: null,
+        genres: [],
+        player: null,
+        resource: null
+    };
+
+    if(withConnection) {
+        if(server.queue.connection && server.queue.connection.state.status !== "destroyed") {
+            server.queue.connection.destroy();
+        }
+
+        server.queue.connection = null;
     }
 };
