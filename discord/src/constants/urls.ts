ab8a75f60c024e5d1911fa930824ab657a461df5
Stefan Fischer <stefischer@mail.com>
2022-09-01 14:01:33 -0400

    Add Support Server to URLS constants

--- discord/src/constants/urls.ts
+++ discord/src/constants/urls.ts
@@ -1,6 +1,7 @@
+export const SUPPORT_SERVER = "https://discord.gg/TqKHVUa";
 export const API = {
     DISCORD: "https://discordapp.com/api",
     YOUTUBE: "https://www.googleapis.com/youtube/v3"
 };
 
-export const YOUTUBE_VIDEO = "https://www.youtube.com/watch?v="
+export const YOUTUBE_VIDEO = "https://www.youtube.com/watch?v=";
