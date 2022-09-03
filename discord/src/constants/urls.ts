e91c19b0e630d0ebaa915a84115d072a89712941
Stefan Fischer <stefischer@mail.com>
2022-09-03 08:47:50 -0400

    Create dadjoke api url and basic apiServices file

--- discord/src/constants/urls.ts
+++ discord/src/constants/urls.ts
@@ -1,7 +1,8 @@
 export const SUPPORT_SERVER = "https://discord.gg/TqKHVUa";
 export const API = {
     DISCORD: "https://discordapp.com/api",
-    YOUTUBE: "https://www.googleapis.com/youtube/v3"
+    YOUTUBE: "https://www.googleapis.com/youtube/v3",
+    ICANHAZDADJOKE: "https://icanhazdadjoke.com/"
 };
 
 export const YOUTUBE_VIDEO = "https://www.youtube.com/watch?v=";
