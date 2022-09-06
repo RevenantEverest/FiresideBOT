a4a3b2b4cda101c879ed83b7ec5468114732489f
Stefan Fischer <stefischer@mail.com>
2022-09-06 04:19:31 -0400

    Create waitFor method in promises

--- discord/src/utils/promises.ts
+++ discord/src/utils/promises.ts
@@ -9,3 +9,16 @@ export async function handle<T>(promise: Promise<T>): HandleReturn<T> {
         return [undefined, err]
     });
 };
+
+export async function waitFor(conditionFunction: () => boolean, intervalLength?: number) {
+    const poll = (resolve: (value?: unknown) => void) => {
+        if(conditionFunction()) {
+            resolve();
+        }
+        else {
+            setTimeout(() => poll(resolve), intervalLength ?? 400);
+        }
+    };
+
+    return new Promise(poll);
+};
