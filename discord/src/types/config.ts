0fa2c63bb486614b63681c21db81e655be38c9a1
Stefan Fischer <stefischer@mail.com>
2022-09-06 04:18:58 -0400

    Add userCount and guildCount to config

--- discord/src/types/config.ts
+++ discord/src/types/config.ts
@@ -2,6 +2,8 @@ import { Server } from './server';
 import { CommandFile, CommandCategory } from './commands';
 
 export interface Config {
+    userCount: number,
+    guildCount: number,
     servers: Server[],
     version: string,
     updatePending: boolean,
