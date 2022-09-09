5e8a038120d6b7d5726d4c975f93d641a0bb434a
Stefan Fischer <stefischer@mail.com>
2022-09-09 07:27:13 -0400

    Add help documentation link to urls constants

--- discord/src/constants/urls.ts
+++ discord/src/constants/urls.ts
@@ -1,4 +1,5 @@
 export const SUPPORT_SERVER = "https://discord.gg/TqKHVUa";
+export const HELP_DOCUMENTATION = "https://help.firesidebot.com";
 export const API = {
     DISCORD: "https://discordapp.com/api",
     YOUTUBE: "https://www.googleapis.com/youtube/v3",
