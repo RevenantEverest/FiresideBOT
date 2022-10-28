2313cca79dcea69a4a2d9445fcc9bf58bd780317
Stefan Fischer <stefischer@mail.com>
2022-10-28 03:16:57 -0400

    Create emojis constants

---
+++ discord/src/constants/emojis.ts
@@ -0,0 +1,6 @@
+export const NEXT = "▶️";
+export const STOP = "⏹️";
+export const PREV = "◀️";
+
+export const LOCKED = "<:Locked:624341962358652958>";
+export const UNLOCKED = "<:Unlocked:624341962425892885>";
