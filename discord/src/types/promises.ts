f2c4c3ef761a4bf271fe9912fdf0b668672ca983
Stefan Fischer <stefischer@mail.com>
2022-10-28 03:19:44 -0400

    Add axios return type to promises

--- discord/src/types/promises.ts
+++ discord/src/types/promises.ts
@@ -1,1 +1,4 @@
+import { AxiosApiError } from './api.js';
+
 export type HandleReturn<T> = Promise<[T | undefined, Error | undefined]>;
+export type HandleAxiosReturn<T> = Promise<[T | undefined, AxiosApiError | undefined]>;
