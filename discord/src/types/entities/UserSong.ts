1b2bfea3a08e0615a7ca6543f4bdbd6c9139b086
Stefan Fischer <stefischer@mail.com>
2022-10-28 03:22:43 -0400

    Add UserSong entity type

---
+++ discord/src/types/entities/UserSong.ts
@@ -0,0 +1,9 @@
+export interface UserSong {
+    id: number,
+    title: string,
+    author: string,
+    video_id: string,
+    duration: number,
+    thumbnail_url: string,
+    created_at: Date
+};
