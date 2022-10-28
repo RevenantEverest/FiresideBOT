f2c8b53041c8b7f149b616f6854c8f21281cc8af
Stefan Fischer <stefischer@mail.com>
2022-10-28 03:22:11 -0400

    Add userPlaylists and userSongs to api index

--- discord/src/api/index.ts
+++ discord/src/api/index.ts
@@ -1,2 +1,4 @@
-export * as guildSettings from './settings/guildSettings.js';
 export * as fortunes from './custom/fortunes.js';
+export * as guildSettings from './settings/guildSettings.js';
+export * as userPlaylists from './playlists/userPlaylists.js';
+export * as userSongs from './playlists/userSongs.js';
