1316fa4647e8cfcf019a49e6d3084165e89fd27a
Stefan Fischer <stefischer@mail.com>
2022-11-28 06:11:18 -0500

    Add new auth testing payload

--- api/src/__tests__/support/payloads/auth.payloads.ts
+++ api/src/__tests__/support/payloads/auth.payloads.ts
@@ -7,3 +7,11 @@ export const MAIN: AuthPayload = {
     discord_id: "253238262776799587",
     avatar: "a_dc5vv4dn77xdkcx2und65cp4ke656a95"
 };
+
+export const ALT: AuthPayload = {
+    id: "fa89adf7-9c42-43a8-9bd3-27943a57d0e2",
+    username: "alt_tester",
+    discriminator: 5432,
+    discord_id: "694907714660466699",
+    avatar: "a_dc5vv4dn77xdkcx2und65cp4ke656a95"
+};
