81c64abade6723b8a669110a3bc93c636b6aaba8
Stefan Fischer <stefischer@mail.com>
2023-01-15 14:46:05 -0500

    Fix start_api_test script to listen for exit code

--- scripts/start_api_test.sh
+++ scripts/start_api_test.sh
@@ -4,4 +4,4 @@ cd "$(dirname "$0")/.." || exit
 
 docker-compose stop
 
-docker-compose -f docker-compose.yml -f override.test.yml build api && docker-compose -f docker-compose.yml -f override.test.yml up api
+docker-compose -f docker-compose.yml -f override.test.yml build api && docker-compose -f docker-compose.yml -f override.test.yml up --exit-code-from api
