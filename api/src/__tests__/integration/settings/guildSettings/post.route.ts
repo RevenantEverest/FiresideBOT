48f221e791a28007b4be5d53c1dfd883f4152f36
Stefan Fischer <stefischer@mail.com>
2023-01-15 14:30:42 -0500

    Fix guildSettings post route tests

--- api/src/__tests__/integration/settings/guildSettings/post.route.ts
+++ api/src/__tests__/integration/settings/guildSettings/post.route.ts
@@ -15,21 +15,44 @@ function postRoute(baseEndpoint: string, app: Application, authPayload: AuthTest
     });
 
     describe("given the user is logged in", () => {
-        describe("given the user is a guild admin", () => {
-            describe("given the user is not a guild admin", () => {
-                it("should return a 403 status", async () => {
-                    extraParams.mocks.hasPermission(false);
-                    extraParams.mocks.isGuildMember(true);
-    
-                    const endpoint = `${baseEndpoint}/${extraParams.guildId}`;
-                    await supertest(app)
-                    .post(endpoint)
-                    .set(authPayload.header)
-                    .send()
-                    .expect(403)
+        describe("given the user is not a guild admin", () => {
+            it("should return a 200 status and the guild settings", async () => {
+                /* Allows any non guild member to create guild settings if they don't exist */
+                extraParams.mocks.hasPermission(false);
+                extraParams.mocks.isGuildMember(true);
+
+                const endpoint = `${baseEndpoint}/${extraParams.altGuildId}`;
+                const { body, statusCode } = await supertest(app)
+                .post(endpoint)
+                .set(authPayload.header)
+                .send()
+
+                expect(statusCode).toBe(200);
+                expect(body.results).not.toBeNull();
+
+                const { results } = body;
+
+                expect(results.id).not.toBeNull();
+                expect(results.updated_at).not.toBeNull();
+
+                expect(results).toEqual({
+                    id: results.id,
+                    guild_id: extraParams.altGuildId,
+                    prefix: DEFAULTS.GENERAL_SETTINGS.PREFIX,
+                    volume: DEFAULTS.GENERAL_SETTINGS.VOLUME,
+                    rank_increase_rate: DEFAULTS.RANK_SETTINGS.GENERAL_INCREASE_RATE,
+                    rank_complexity: DEFAULTS.RANK_SETTINGS.COMPLEXITY,
+                    rank_channel: null,
+                    currency_name: DEFAULTS.ECONOMY_SETTINGS.CURRENCY_NAME,
+                    currency_increase_rate: DEFAULTS.ECONOMY_SETTINGS.INCREASE_RATE,
+                    updated_at: results.updated_at
                 });
+
+                /* Don't set extraParams.guildSettings for altGuildId */
             });
-            
+        });
+
+        describe("given the user is a guild admin", () => {
             it("should save default settings, return a 200 status and the guild settings", async () => {
                 extraParams.mocks.hasPermission(true);
                 extraParams.mocks.isGuildMember(true);
