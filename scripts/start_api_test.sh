cb275f5952a3b9e9306539017e3a2ec79483295b
Stefan Fischer <stefischer@mail.com>
2023-01-17 07:24:34 -0500

    Test new api test script

--- scripts/start_api_test.sh
+++ scripts/start_api_test.sh
@@ -4,4 +4,6 @@ cd "$(dirname "$0")/.." || exit
 
 docker-compose stop
 
-docker-compose -f docker-compose.yml -f override.test.yml build api && docker-compose -f docker-compose.yml -f override.test.yml up --exit-code-from api
+# docker-compose -f docker-compose.yml -f override.test.yml build api && docker-compose -f docker-compose.yml -f override.test.yml up --exit-code-from api
+
+docker pull postgres:14.5 && docker run --name testing_db -e POSTGRES_USER=$DB_USERNAME POSTGRES_PASSWORD=$DB_PASSWORD POSTGRES_DB=$DB_NAME -d postgres && cd ./api && yarn test
