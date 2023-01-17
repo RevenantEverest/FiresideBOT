0b542ba1d5664e9e8ae9e07148fc7ae05f478029
Stefan Fischer <stefischer@mail.com>
2023-01-17 07:40:32 -0500

    Update script

--- scripts/start_api_test.sh
+++ scripts/start_api_test.sh
@@ -6,4 +6,4 @@ docker-compose stop
 
 # docker-compose -f docker-compose.yml -f override.test.yml build api && docker-compose -f docker-compose.yml -f override.test.yml up --exit-code-from api
 
-docker pull postgres:14.5 && docker run --name testing_db -e POSTGRES_USER=${DB_USERNAME} POSTGRES_PASSWORD=${DB_PASSWORD} POSTGRES_DB=${DB_NAME} -d postgres
+docker pull postgres:14.5 && docker run --name testing-db -e POSTGRES_USER=${DB_USERNAME} POSTGRES_PASSWORD=${DB_PASSWORD} POSTGRES_DB=${DB_NAME} -d postgres
