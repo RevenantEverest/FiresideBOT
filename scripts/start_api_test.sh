c3cedafe4b9f5c494bf9b7ce1c2f40e630be02a7
Stefan Fischer <stefischer@mail.com>
2023-01-18 18:50:02 -0500

    Update start api test to use profiles flag

--- scripts/start_api_test.sh
+++ scripts/start_api_test.sh
@@ -4,6 +4,4 @@ cd "$(dirname "$0")/.." || exit
 
 docker-compose stop
 
-# docker-compose -f docker-compose.yml -f override.test.yml build api && docker-compose -f docker-compose.yml -f override.test.yml up --exit-code-from api
-
-docker pull postgres:14.5 && docker run --name testing-db -e POSTGRES_USER="\$DB_USERNAME" -e POSTGRES_PASSWORD="\$DB_PASSWORD" -e POSTGRES_DB="\$TESTING_DB_NAME" -d postgres
+docker-compose -f docker-compose.yml -f override.test.yml --profile api build api && docker-compose -f docker-compose.yml -f override.test.yml --profile api up --exit-code-from api
