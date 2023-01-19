1c4239820e4cc2bb8a157bdb6228316c9a4fc1be
Stefan Fischer <stefischer@mail.com>
2023-01-19 14:04:16 -0500

    Move tests scripts to their own directory

--- scripts/tests/start_api_test.sh
+++ scripts/tests/start_api_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd "$(dirname "$0")/.." || exit
+cd "$(dirname "$0")/../.." || exit
 
 docker-compose stop
 docker-compose -f docker-compose.yml -f override.test.yml build api && docker-compose -f docker-compose.yml -f override.test.yml --profile api up --exit-code-from api
