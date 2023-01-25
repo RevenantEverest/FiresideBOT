743cfb66a3967f31ab0c96dcb4f67873ed0b93fd
Stefan Fischer <stefischer@mail.com>
2023-01-25 15:25:59 -0500

    Add array of heart emojis to emojis constants

--- discord/src/constants/emojis.ts
+++ discord/src/constants/emojis.ts
@@ -4,3 +4,18 @@ export const PREV = "◀️";
 
 export const LOCKED = "<:Locked:624341962358652958>";
 export const UNLOCKED = "<:Unlocked:624341962425892885>";
+
+export const CHECK_MARK = "✅";
+export const X_MARK = "❌";
+
+export const HEARTS = [
+    "❤️",
+    "❣️",
+    "💕",
+    "💞",
+    "💓",
+    "💗",
+    "💖",
+    "💘",
+    "💝"
+];
