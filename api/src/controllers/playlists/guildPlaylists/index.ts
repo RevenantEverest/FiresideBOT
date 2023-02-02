3c2fdc14fd7dfb1e6f175dfc4020658b1f6a3592
Stefan Fischer <stefischer@mail.com>
2023-02-02 01:50:27 -0500

    Add method to index export

--- api/src/controllers/playlists/guildPlaylists/index.ts
+++ api/src/controllers/playlists/guildPlaylists/index.ts
@@ -1,6 +1,7 @@
 export { default as index } from './index.action.js';
 export { default as getOne } from './getOne.action.js';
 export { default as getByGuildId } from './getByGuildId.action.js';
+export { default as getByGuildIdAndPlaylistName } from './getByGuildIdAndPlaylistName.action.js';
 export { default as create } from './create.action.js';
 export { default as update } from './update.action.js';
 export { default as destroy } from './destroy.action.js';
