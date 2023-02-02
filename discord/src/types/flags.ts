570732ab9fcc052f86417df589c0a88e5c3aa460
Stefan Fischer <stefischer@mail.com>
2023-02-02 15:13:05 -0500

    Add permissions to Flags

--- discord/src/types/flags.ts
+++ discord/src/types/flags.ts
@@ -1,6 +1,9 @@
+import { PermissionString } from 'discord.js';
+
 export interface Flag {
     name: string,
     usageSymbol: string[],
+    permissions: PermissionString[],
     description: string,
     example: string
 };
