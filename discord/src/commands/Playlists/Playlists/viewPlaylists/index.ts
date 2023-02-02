81c76b1e84936d6ebc3b5210fb65f9bc80bb0c1e
Stefan Fischer <stefischer@mail.com>
2023-02-02 02:10:13 -0500

    Update playlist command handlers to use playlist utils

--- discord/src/commands/Playlists/Playlists/viewPlaylists/index.ts
+++ discord/src/commands/Playlists/Playlists/viewPlaylists/index.ts
@@ -1,16 +1,10 @@
-import { Client } from 'discord.js';
+import { UserResolvable } from 'discord.js';
 import { CommandDispatch, CommandFile } from '../../../../types/commands.js';
-import { UserPlaylist } from '../../../../types/entities/UserPlaylist.js';
-import { PaginatedEmbed } from '../../../../types/embeds.js';
-import { ApiPaginationOptions, GetPageResponse } from '../../../../types/pagination.js';
-import { HandleReturn } from '../../../../types/promises.js';
-
 
 import * as api from '../../../../api/index.js';
-import { EMOJIS, IMAGE_RESOURCES } from '../../../../constants/index.js';
-import { colors, embeds, pagination, dates, errors } from '../../../../utils/index.js';
+import { playlists, errors } from '../../../../utils/index.js';
 
-async function viewPlaylists(dispatch: CommandDispatch, discordId: string, commandFile: CommandFile) {
+async function viewPlaylists(dispatch: CommandDispatch, discordId: UserResolvable, commandFile: CommandFile) {
 
     const [userPlaylists, err] = await api.userPlaylists.getByDiscordId(dispatch, discordId, {
         page: 1
@@ -21,65 +15,21 @@ async function viewPlaylists(dispatch: CommandDispatch, discordId: string, comma
     }
 
     if(!userPlaylists) {
-        return dispatch.reply("No Fortunes found");
+        return dispatch.reply("No Playlists found");
     }
 
     const playlistOwner = await dispatch.guild.members.fetch(discordId);
-    const amountPerPage: number = 5;
-    const paginatedEmbed: PaginatedEmbed = generatePaginatedEmbed(userPlaylists.results);
 
-    function generatePaginatedEmbed(playlists: UserPlaylist[]): PaginatedEmbed {
-        const playlistDurations = playlists.map(playlist => playlist.duration);
-        const overallDuration = playlistDurations.reduce((a, b) => a + b, 0);
-
-        return {
-            pages: embeds.generatePaginatedEmbedPages<UserPlaylist>({
-                title: `**Overall Length of Playlists:**`,
-                description: `${dates.parseSeconds(overallDuration)}\n\u200b`,
-                author: {
-                    iconURL: playlistOwner.user.avatarURL({ dynamic: true }) ?? "",
-                    name: `${playlistOwner.user.username} #${playlistOwner.user.discriminator} - Playlists`
-                },
-                thumbnail: IMAGE_RESOURCES.PLAYLIST_ICON,
-                color: colors.steelPink,
-                data: playlists,
-                amountPerPage,
-                setFieldName(playlist: UserPlaylist, index: number, startIndex: number) {
-                    const duration = dates.parseSeconds(playlist.duration);
-                    return `${(startIndex + index) + 1}. ${playlist.name} (${duration}) ${playlist.is_public ? "" : EMOJIS.LOCKED}`
-                },
-                setFieldValue(playlist: UserPlaylist) {
-                    return `${playlist.songCount} songs`;
-                },
-            })
-        };
-    };
-
-    const partialOptions = pagination.generateBasicPagiationOptions<UserPlaylist>(userPlaylists);
-    const paginationOptions: ApiPaginationOptions<UserPlaylist> = {
-        ...partialOptions,
-        amountPerPage,
-        getPage: async (page: number, data: UserPlaylist[]): HandleReturn<GetPageResponse<UserPlaylist>> => {
-            const [paginatedRes, err] = await api.userPlaylists.getByDiscordId(dispatch, discordId, {
-                page
-            });
-
-            if(err) {
-                errors.command({ dispatch, err, errMessage: err.message, commandName: commandFile.displayName });
-                return [undefined, err];
-            }
-
-            if(!paginatedRes) {
-                return [undefined, new Error("No Playlists Found")];
-            }
-
-            const getPageRes = pagination.formatGetPageResponse({ page, data, paginatedRes, generatePaginatedEmbed });
-
-            return [getPageRes, undefined];
-        }
-    };
-
-    return embeds.pagination<UserPlaylist>(dispatch, paginatedEmbed, paginationOptions);
+    return playlists.sendPlaylistsEmbed({
+        dispatch,
+        commandFile,
+        playlists: userPlaylists,
+        requestId: discordId,
+        setAuthor: () => ({
+            iconURL: playlistOwner.user.avatarURL({ dynamic: true }) ?? "",
+            name: `${playlistOwner.user.username} #${playlistOwner.user.discriminator} - Playlists`
+        })
+    });
 };
 
 export default viewPlaylists;
--- discord/src/commands/Playlists/Playlists/viewSinglePlaylist/index.ts
+++ discord/src/commands/Playlists/Playlists/viewSinglePlaylist/index.ts
@@ -1,17 +1,11 @@
 import { Client, UserResolvable } from 'discord.js';
 import { CommandDispatch, CommandFile, CommandOptions } from '../../../../types/commands.js';
 import { Server } from '../../../../types/server.js';
-import { UserSong } from '../../../../types/entities/UserSong.js';
-import { PaginatedEmbed } from '../../../../types/embeds.js';
-import { HandleReturn } from '../../../../types/promises.js';
-import { ApiPaginationOptions, GetPageResponse } from '../../../../types/pagination.js';
-
 
 import * as api from '../../../../api/index.js';
-import requestPlaylist from '../requestPlaylist/index.js';
 
-import { IMAGE_RESOURCES, EMOJIS } from '../../../../constants/index.js';
-import { colors, embeds, pagination, dates, errors } from '../../../../utils/index.js';
+import { EMOJIS, FLAGS } from '../../../../constants/index.js';
+import { songRequests, flags, playlists, errors } from '../../../../utils/index.js';
 
 export interface Params {
     bot: Client,
@@ -57,75 +51,23 @@ async function viewSinglePlaylist({ bot, dispatch, args, server, options, discor
         return dispatch.reply("No Playlist Songs found");
     }
 
-    if(args.includes("-i")) {
-        const amountPerPage: number = 5;
-        const paginatedEmbed: PaginatedEmbed = generatePaginatedEmbed(playlistSongs.results);
+    const argFlags = flags.getCommandArgFlags(dispatch, args);
 
-        function generatePaginatedEmbed(playlistSongs: UserSong[]): PaginatedEmbed {
-            let playlistDuration = "";
-
-            if(userPlaylist?.duration && userPlaylist.duration > 0) {
-                playlistDuration = `(${dates.parseSeconds(userPlaylist.duration)})`;
-            }
-
-            return {
-                pages: embeds.generatePaginatedEmbedPages<UserSong>({
-                    title: `**${userPlaylist?.name} ${playlistDuration}**`,
-                    description: `${userPlaylist?.is_public ? "Public" : `${EMOJIS.LOCKED}Private`}\n\u200b`,
-                    author: {
-                        iconURL: dispatch.author.avatarURL({ dynamic: true }) ?? "",
-                        name: `${dispatch.author.username} #${dispatch.author.discriminator}`
-                    },
-                    thumbnail: IMAGE_RESOURCES.PLAYLIST_ICON,
-                    color: colors.steelPink,
-                    data: playlistSongs, 
-                    amountPerPage, 
-                    setFieldName: (song: UserSong, index: number, startIndex: number): string => {
-                        return `${(startIndex + index) + 1}. ${song.title}`;
-                    }, 
-                    setFieldValue: (song: UserSong): string => {
-                        const createdAt = dates.format(song.created_at, {
-                            dateFormat: "MMMM D, YYYY"
-                        });
-                        return(
-                            `**Author:** ${song.author}\n` +
-                            `**Duration:** ${dates.parseSeconds(song.duration)}\n` +
-                            `**Added At:** ${createdAt.date}\n` +
-                            `**ID:** ${song.id}`
-                        );
-                    }
-                })
-            };
-        };
-
-        const partialOptions = pagination.generateBasicPagiationOptions<UserSong>(playlistSongs);  
-        const paginationOptions: ApiPaginationOptions<UserSong> = {
-            ...partialOptions,
-            amountPerPage,
-            getPage: async (page: number, data: UserSong[]): HandleReturn<GetPageResponse<UserSong>> => {
-                const [paginatedRes, err] = await api.userSongs.getByPlaylistId(dispatch, userPlaylist, {
-                    page
-                });
-        
-                if(err) {
-                    errors.command({ dispatch, err, errMessage: err.message, commandName: commandFile.displayName });
-                    return [undefined, err];
-                }
-            
-                if(!paginatedRes) {
-                    return [undefined, new Error("No Songs Found")];
-                }
-
-                const getPageRes = pagination.formatGetPageResponse({ page, data, paginatedRes, generatePaginatedEmbed });
-
-                return [getPageRes, undefined];
-            }
-        };
-
-        return embeds.pagination<UserSong>(dispatch, paginatedEmbed, paginationOptions);
+    if(flags.containsFlag(FLAGS.INFO, argFlags)) {
+        return playlists.sendSinglePlaylistEmbed({ 
+            dispatch, 
+            commandFile, 
+            playlist: userPlaylist, 
+            songs: playlistSongs,
+            setDescription: () => `${userPlaylist?.is_public ? "Public" : `${EMOJIS.LOCKED}Private`}\n\u200b`,
+            setAuthor: () => ({
+                iconURL: dispatch.author.avatarURL({ dynamic: true }) ?? "",
+                name: `${dispatch.author.username} #${dispatch.author.discriminator}`
+            })
+        });
     }
 
-    return requestPlaylist({ 
+    return songRequests.playPlaylist({ 
         bot, 
         dispatch, 
         args,
