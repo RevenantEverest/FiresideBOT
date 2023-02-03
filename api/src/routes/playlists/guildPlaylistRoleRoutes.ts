cb1c37d23da9767743967122662a5a8ec7585824
Stefan Fischer <stefischer@mail.com>
2023-02-03 17:42:42 -0500

    Use roleId validation middleware in guildPlaylistRoleRoutes

--- api/src/routes/playlists/guildPlaylistRoleRoutes.ts
+++ api/src/routes/playlists/guildPlaylistRoleRoutes.ts
@@ -13,8 +13,8 @@ router.route("/guild_id/:guildId/id/:playlistId/roles")
 .get(extractPaginationParams, validationMiddleware, permissions.isGuildMember, guildPlaylistRolesController.getByGuildIdAndPlaylistId)
 .post(validationMiddleware, permissions.isGuildAdmin, guildPlaylistRolesController.create)
 
-router.route("/guild_id/:guildId/id/:playlistId/roles/id/:id")
+router.route("/guild_id/:guildId/id/:playlistId/roles/id/:roleId")
 .get(validationMiddleware, permissions.isGuildMember, guildPlaylistRolesController.getOne)
-.delete(validationMiddleware, permissions.isGuildAdmin, guildPlaylistRolesController.destroy)
+.delete(validationMiddleware, validation.roleId, permissions.isGuildAdmin, guildPlaylistRolesController.destroy)
 
 export default router;
