afdb55e1f6507c5502af9a05b66849a2a5733b12
Stefan Fischer <stefischer@mail.com>
2023-02-20 06:59:42 -0500

    Create Balance command

---
+++ discord/src/commands/Economy/Balance/index.command.ts
@@ -0,0 +1,42 @@
+import Discord from 'discord.js';
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { CommandParams, CommandConfig } from '../../../types/commands.js';
+
+import * as api from '../../../api/index.js';
+import { IMAGE_RESOURCES } from '../../../constants/index.js';
+import { colors } from '../../../utils/index.js';
+
+async function Balance({ dispatch, guildSettings }: CommandParams) {
+    const [record, err] = await api.guildCurrencyRecords.getByGuildIdAndDiscordId(dispatch, dispatch.author.id);
+
+    if(err || !record) {
+        return;
+    }
+
+    const embed = new Discord.MessageEmbed({
+        color: colors.fireside,
+        author: {
+            iconURL: dispatch.author.avatarURL({ dynamic: true }) ?? "",
+            name: `Bank Records For ${dispatch.author.username} #${dispatch.author.discriminator}`
+        },
+        description: `**${guildSettings.currency_name}:** ${record.balance.toLocaleString()}`,
+        thumbnail: {
+            url: IMAGE_RESOURCES.BANK_ICON
+        },
+        footer: {
+            iconURL: dispatch.guild.iconURL({ dynamic: true }) ?? "",
+            text: dispatch.guild.name
+        }
+    });
+
+    return dispatch.reply({ embeds: [embed] });
+};
+
+export const config: CommandConfig = {
+    aliases: ["bal"],
+    permissions: [],
+    description: "Displays your current server currency balance",
+    example: "balance"
+};
+
+export default Balance;
