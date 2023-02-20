a4fad4913a983b4e409fff50a8820877f9dd60ca
Stefan Fischer <stefischer@mail.com>
2023-02-20 06:56:48 -0500

    Update errors utils and types

--- api/src/utils/errors.ts
+++ api/src/utils/errors.ts
@@ -1,6 +1,8 @@
+import { Response, NextFunction } from 'express';
+import { SendResponseOptions, ErrorLogOptions, HandleTupleOptions } from '../types/errors.js';
+
 import * as logs from './logs.js';
 import * as colors from './colors.js';
-import { SendResponseOptions, ErrorLogOptions, HandleTupleOptions } from '../types/errors.js';
 
 export function sendResponse({ res, next, err, status=500, message }: SendResponseOptions): void {
     const logOptions: ErrorLogOptions = {
@@ -36,4 +38,22 @@ export function handleTuple<T>({res, err, errMsg}: HandleTupleOptions<T>): Error
     }
  
     return undefined;
+};
+
+interface SendEntitiesResponseParams<T> {
+    res: Response,
+    err?: Error,
+    message: string,
+    entityReturn?: T | null,
+    missingEntityReturnMessage: string
+};
+
+export function sendEntitiesResponse<T>({ res, err, message, entityReturn, missingEntityReturnMessage }: SendEntitiesResponseParams<T>) {
+    if(err) {
+        return sendResponse({ res, err, status: 500, message: message ?? err.message });
+    }
+
+    if(!entityReturn) {
+        return sendResponse({ res, status: 404, message: missingEntityReturnMessage });
+    }
 };
