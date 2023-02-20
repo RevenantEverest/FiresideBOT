84569247482496cc28d0dc4a429e3cc5927c1f84
Stefan Fischer <stefischer@mail.com>
2023-02-20 06:56:28 -0500

    Add api methods for guildCurrencyRecord

---
+++ discord/src/api/economy/guildCurrencyRecords.ts
@@ -0,0 +1,21 @@
+import { UserResolvable } from 'discord.js';
+
+import { CommandDispatch } from '../../types/commands.js';
+import { HandleAxiosReturn } from '../../types/promises.js';
+import { ApiPaginatedResponse, ApiPaginationParams } from '../../types/api.js';
+
+import { GuildCurrencyRecord } from '../../types/entities/GuildCurrencyRecord.js';
+
+import { ENV } from '../../constants/index.js';
+import { apiRequests } from '../../utils/index.js';
+
+const baseEndpoint = ENV.API_URL + "/economy/currency/guild";
+
+export async function getByGuildIdAndDiscordId(dispatch: CommandDispatch, discordId: UserResolvable): HandleAxiosReturn<GuildCurrencyRecord> {
+    const endpoint = `${baseEndpoint}/guild_id/${dispatch.guildId}/discord_id/${discordId}`;
+    return apiRequests.request<GuildCurrencyRecord>({
+        dispatch,
+        endpoint,
+        method: "get"
+    });
+};
