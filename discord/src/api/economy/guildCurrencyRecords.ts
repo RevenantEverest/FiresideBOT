f22dc525c61237c36f08ae7c9035ba76ed29eeee
Stefan Fischer <Stefischer@mail.com>
2023-02-23 02:38:50 -0500

    Add update method to guildCurrencyRecords api

--- discord/src/api/economy/guildCurrencyRecords.ts
+++ discord/src/api/economy/guildCurrencyRecords.ts
@@ -9,7 +9,7 @@ import { GuildCurrencyRecord } from '../../types/entities/GuildCurrencyRecord.js
 import { ENV } from '../../constants/index.js';
 import { apiRequests } from '../../utils/index.js';
 
-const baseEndpoint = ENV.API_URL + "/economy/currency/guild";
+const baseEndpoint = ENV.API_URL + "/economy/currency/records/guild";
 
 export async function getByGuildIdAndDiscordId(dispatch: CommandDispatch, discordId: UserResolvable): HandleAxiosReturn<GuildCurrencyRecord> {
     const endpoint = `${baseEndpoint}/guild_id/${dispatch.guildId}/discord_id/${discordId}`;
@@ -17,5 +17,17 @@ export async function getByGuildIdAndDiscordId(dispatch: CommandDispatch, discor
         dispatch,
         endpoint,
         method: "get"
+    });
+};
+
+export async function update(dispatch: CommandDispatch, discordId: UserResolvable, balance: string): HandleAxiosReturn<GuildCurrencyRecord> {
+    const endpoint = `${baseEndpoint}/guild_id/${dispatch.guildId}/discord_id/${discordId}`;
+    return apiRequests.request<GuildCurrencyRecord, { balance: string }>({
+        dispatch,
+        endpoint,
+        method: "put",
+        data: {
+            balance
+        }
     });
 };
