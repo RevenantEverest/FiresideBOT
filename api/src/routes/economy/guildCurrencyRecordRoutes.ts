b3b04ca0a9a42466596abf2a56ce771cc4ab25ce
Stefan Fischer <Stefischer@mail.com>
2023-02-23 02:37:40 -0500

    Add put route for guildCurrencyRecord routes

--- api/src/routes/economy/guildCurrencyRecordRoutes.ts
+++ api/src/routes/economy/guildCurrencyRecordRoutes.ts
@@ -14,5 +14,18 @@ router.route("/guild_id/:guildId")
 
 router.route("/guild_id/:guildId/discord_id/:discordId")
 .get(validation.guildId, validation.discordId, permissions.isGuildMember, guildCurrencyRecordsController.getByGuildIdDiscordId)
+.put(validation.guildId, validation.discordId, permissions.isGuildMember, guildCurrencyRecordsController.update)
 
 export default router;
+
+/*
+
+    Update Routes:
+    - User updates the currency amount from a command
+        - The user should not be able to hit this endpoint and update their own currency
+        - Maybe add an element into auth payload that tells api if the request came from discord
+    - Guild admin gives a user a certain amount of currency
+
+    ** Is there a situation where a user doesn't have a currency record and when trying to create one and update it it will attempt to save 2?
+
+*/
