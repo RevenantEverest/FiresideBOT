0c762daef5be10d6cb361fe861d385d5a7e26915
Stefan Fischer <Stefischer@mail.com>
2023-02-23 02:36:20 -0500

    Create guildCurrencyRecord test skeletons

--- api/src/__tests__/integration/economy/guildCurrencyRecords/__index__.ts
+++ api/src/__tests__/integration/economy/guildCurrencyRecords/__index__.ts
@@ -1,19 +1,35 @@
 import initializeApp from '../../../../app.js';
 import issueToken from '../../../support/login.support.js';
+import { DISCORD } from '../../../support/constants/index.js';
 import * as AUTH_PAYLOADS from '../../../support/payloads/auth.payloads.js';
 import { HandleReturn } from '../../../../types/promises.js';
+import { GuildCurrencyRecordExtraParams } from '../../../support/types/extraParams/guildCurrencyRecord.params.js';
+
+import { discord } from '../../../../utils/index.js';
 
 import postRouteSpec from './post.route.js';
 import updateRouteSpec from './update.route.js';
 import getRouteSpec from './get.route.js';
+
+type MockReturn = Promise<HandleReturn<boolean>>;
 
 const authPayload = issueToken(AUTH_PAYLOADS.MAIN);
 
 const app = initializeApp();
 const baseEndpoint = "/economy/records/guild/";
 
-const extraParams = {
-
+const extraParams: GuildCurrencyRecordExtraParams = {
+    guildId: DISCORD.TESTING_SERVER_ID,
+    mocks: {
+        hasPermission: (value: boolean) => {
+            const spy = jest.spyOn(discord, "checkMemberPermissions");
+            spy.mockImplementation(async (): MockReturn => [value, undefined]);
+        },
+        isGuildMember: (value: boolean) => {
+            const spy = jest.spyOn(discord, "isGuildMember");
+            spy.mockImplementation(async (): MockReturn => [value, undefined]);
+        }
+    }
 };
 
 export default () => {
---
+++ api/src/__tests__/integration/economy/guildCurrencyRecords/update.route.ts
@@ -0,0 +1,13 @@
+import supertest from 'supertest';
+import { Application } from 'express';
+
+import * as PAYLOADS from '../../../support/payloads/guildCurrencyRecord.payloads.js';
+
+import { AuthTestingPayload } from '../../../support/types/auth.js';
+import { GuildCurrencyRecordExtraParams } from '../../../support/types/extraParams/guildCurrencyRecord.params.js';
+
+function updateRoute(baseEndpoint: string, app: Application, authPayload: AuthTestingPayload, extraParams: GuildCurrencyRecordExtraParams) {
+
+};
+
+export default updateRoute;
