f4815a274b38751e5965cb7121b5c25be658f2f6
Stefan Fischer <stefischer@mail.com>
2023-03-04 08:45:54 -0500

    Handle errors in Give command

--- discord/src/commands/Economy/Give/index.command.ts
+++ discord/src/commands/Economy/Give/index.command.ts
@@ -7,13 +7,14 @@ import * as api from '../../../api/index.js';
 import { ERROR_MESSAGES } from '../../../constants/index.js';
 import { regex, errors } from '../../../utils/index.js';
 
-async function Give({ bot, dispatch, args, guildSettings }: CommandParams) {
+async function Give({ bot, dispatch, args, guildSettings, commandFile }: CommandParams) {
     if(!dispatch.interaction && !args[0]) {
         return dispatch.reply(ERROR_MESSAGES.COMMANDS.GIVE.NO_ARGS);
     }
 
     const recipientId = dispatch.interaction?.options.getUser("user")?.id ?? regex.parseUserTag(args.join(" "));
-    const amountToGive = dispatch.interaction?.options.getNumber("amount") ?? parseInt(args.join(" ").replace(regex.userRegex, "").split(" ")[0], 10);
+    const amountToGiveArgs = args.join(" ").replace(regex.userRegex, "").replace(" ", "").split(" ")[0];
+    const amountToGive = dispatch.interaction?.options.getNumber("amount") ?? parseInt(amountToGiveArgs, 10);
 
     if(!recipientId) {
         return dispatch.reply(ERROR_MESSAGES.COMMANDS.GIVE.NO_USER_MENTION);
@@ -30,13 +31,25 @@ async function Give({ bot, dispatch, args, guildSettings }: CommandParams) {
     const [benefactorRecord, benefactorGetErr] = await api.guildCurrencyRecords.getByGuildIdAndDiscordId(dispatch, dispatch.author.id);
 
     if(benefactorGetErr || !benefactorRecord) {
-        return;
+        return errors.commandApi({ 
+            dispatch,
+            commandFile,
+            err: benefactorGetErr,
+            resource: benefactorRecord,
+            missingResourceMessage: "No Currency Record returned for benefactor"
+        });
     }
 
     const [recipientRecord, recipientGetErr] = await api.guildCurrencyRecords.getByGuildIdAndDiscordId(dispatch, recipientId);
 
     if(recipientGetErr || !recipientRecord) {
-        return;
+        return errors.commandApi({ 
+            dispatch,
+            commandFile,
+            err: recipientGetErr,
+            resource: recipientRecord,
+            missingResourceMessage: "No Currency Record returned for recipient"
+        });
     }
 
     const benefactorBalance = new BigNumber(benefactorRecord.balance);
@@ -50,28 +63,40 @@ async function Give({ bot, dispatch, args, guildSettings }: CommandParams) {
     const [benefactorRecordUpdate, benefactorUpdateErr] = await api.guildCurrencyRecords.update(dispatch, dispatch.author.id, adjustedBenefactorBalance);
 
     if(benefactorUpdateErr || !benefactorRecordUpdate) {
-        return;
+        return errors.commandApi({ 
+            dispatch,
+            commandFile,
+            err: benefactorUpdateErr,
+            resource: benefactorRecordUpdate,
+            missingResourceMessage: "No updated Currency Record returned for benefactor"
+        });
     }
 
     const adjustedRecipientBalance = recipientBalance.plus(amountToGive).toString();
     const [recipientRecordUpdate, recipientUpdateErr] = await api.guildCurrencyRecords.update(dispatch, recipientId, adjustedRecipientBalance);
 
     if(recipientUpdateErr || !recipientRecordUpdate) {
-        return;
+        return errors.commandApi({ 
+            dispatch,
+            commandFile,
+            err: recipientUpdateErr,
+            resource: recipientRecordUpdate,
+            missingResourceMessage: "No updated Currency Record returned for recipient"
+        });
     }
 
     const recipient = dispatch.interaction?.options.getUser("user") ?? await bot.users.fetch(recipientId);
 
     return dispatch.reply(`${dispatch.author} gave ${recipient} ${amountToGive.toLocaleString()} ${guildSettings.currency_name}`);
 
-    /* Handle evenantual Log embed */
+    /* Handle eventual Log embed */
 };
 
 export const config: CommandConfig = {
     aliases: [],
     permissions: [],
     description: "Give some of your server currency to someone else",
-    example: "give @Revenanteverest 100"
+    example: "give @RevenantEverest 100"
 };
 
 export const slashCommand = new SlashCommandBuilder()
@@ -81,6 +106,7 @@ export const slashCommand = new SlashCommandBuilder()
     option
     .setName("user")
     .setDescription("Who you'd like to give the currency to")
+    .setRequired(true)
 )
 .addNumberOption(option =>
     option
