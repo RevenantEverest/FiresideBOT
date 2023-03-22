8c8728f1b106187a8e4d0e5b4df754aa94fb574a
Stefan Fischer <stefischer@mail.com>
2023-03-22 15:54:14 -0400

    Add guildwars_event_tracker container to start script

--- scripts/start_dev.sh
+++ scripts/start_dev.sh
@@ -4,4 +4,4 @@ cd "$(dirname "$0")/.." || exit
 
 docker-compose stop
 
-docker-compose -f docker-compose.yml -f override.dev.yml build api discord && docker-compose -f docker-compose.yml -f override.dev.yml up api discord
+docker-compose -f docker-compose.yml -f override.dev.yml build api discord guildwars_event_tracker && docker-compose -f docker-compose.yml -f override.dev.yml up api discord guildwars_event_tracker
