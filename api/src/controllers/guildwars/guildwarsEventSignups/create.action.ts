a0d32ad3e9adc1a4a22e2a6ae54d6a28d456f5ba
Stefan Fischer <stefischer@mail.com>
2023-05-11 19:00:39 -0400

    Convert tz to utc before create gw2 signup

--- api/src/controllers/guildwars/guildwarsEventSignups/create.action.ts
+++ api/src/controllers/guildwars/guildwarsEventSignups/create.action.ts
@@ -1,15 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
-import dayjs from 'dayjs';
-import utc from 'dayjs/plugin/utc.js';
 
 import { GuildWarsEventSignup } from '../../../entities/index.js';
 import { ResponseLocalsParams, AuthenticatedResponseLocals } from '../../../types/responseLocals.js';
 import { AuthPayload } from '../../../types/auth.js';
 
 
-import { errors, entities, guildwarsEvents } from '../../../utils/index.js';
-
-dayjs.extend(utc);
+import { errors, entities, guildwarsEvents, dates } from '../../../utils/index.js';
 
 interface ReqBody {
     event_name: string,
@@ -21,13 +17,14 @@ async function create(req: Request, res: Response, next: NextFunction) {
     const auth: AuthPayload = res.locals.auth;
 
     const body: ReqBody = req.body;
-    const event = guildwarsEvents.getEvent(body.event_name, body.event_time);
+    const parsedEventTime = dates.convertToUTCFromTimezoneAndFormat(body.event_time, ["hh:mma"], body.timezone, {
+        timeFormat: "HH:mm"
+    }).time;
+    const event = guildwarsEvents.getEvent(body.event_name, parsedEventTime);
 
     if(!event) {
         return errors.sendResponse({ res, next, status: 404, message: "No event found by that title or time" });
     }
-
-    console.log(event);
 
     const [eventSignup, err] = await entities.insert<GuildWarsEventSignup>(GuildWarsEventSignup, {
         discord_id: auth.discord_id,
@@ -36,6 +33,9 @@ async function create(req: Request, res: Response, next: NextFunction) {
     });
 
     if(err || !eventSignup) {
+        if(err && err.message === 'duplicate key value violates unique constraint "UQ_1d7a8478b0fdca02d4b56dfed46"') {
+            return errors.sendResponse({ res, status: 400, message: "Already signed up for this event" });
+        }
         return errors.sendEntitiesResponse({
             res,
             err,
