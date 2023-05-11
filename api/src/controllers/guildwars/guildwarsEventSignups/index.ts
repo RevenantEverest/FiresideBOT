b77cae44291c3d326af8fb15559cff22c423dff3
Stefan Fischer <stefischer@mail.com>
2023-05-11 19:01:48 -0400

    Add new get action for gw2 signups

--- api/src/controllers/guildwars/guildwarsEventSignups/index.ts
+++ api/src/controllers/guildwars/guildwarsEventSignups/index.ts
@@ -3,5 +3,6 @@ export { default as getOne } from './getOne.action.js';
 export { default as getByDiscordId } from './getByDiscordId.action.js';
 export { default as getByEventTitle } from './getByEventTitle.action.js';
 export { default as getByEventTitleAndDiscordId } from './getByEventTitleAndDiscordId.action.js';
+export { default as getByEventTitleAndEventTime } from './getByEventTitleAndEventTime.js';
 export { default as create } from './create.action.js';
 export { default as destroy } from './destroy.action.js';
