3662687a2e0d5fb860f66185e78fb18a2dfe0dc3
Stefan Fischer <stefischer@mail.com>
2023-08-19 19:33:19 -0400

    Update start scripts to pass resources to needed modules

--- scripts/start_dev.sh
+++ scripts/start_dev.sh
@@ -2,6 +2,9 @@
 
 cd "$(dirname "$0")/.." || exit
 
+cp -a resources/ api/src/
+cp -a resources/ discord/src/
+
 docker-compose stop
 
 docker-compose -f docker-compose.yml -f override.dev.yml build api discord guildwars_event_tracker && docker-compose -f docker-compose.yml -f override.dev.yml up api discord guildwars_event_tracker
