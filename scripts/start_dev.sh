eb6d9caf32bc3eea99a49601a3088ce420b109b4
Stefan Fischer <stefischer@mail.com>
2023-08-20 19:32:48 -0400

    Add .env copy and resources copy to start dev script

--- scripts/start_dev.sh
+++ scripts/start_dev.sh
@@ -2,9 +2,14 @@
 
 cd "$(dirname "$0")/.." || exit
 
+cp .env api/
+cp .env discord/
+cp .env trackers/
+
 cp -a resources/ api/src/
 cp -a resources/ discord/src/
+cp -a resources/ trackers/src/
 
 docker-compose stop
 
-docker-compose -f docker-compose.yml -f override.dev.yml build api discord guildwars_event_tracker && docker-compose -f docker-compose.yml -f override.dev.yml up api discord guildwars_event_tracker
+docker-compose --env-file .env -f docker-compose.yml -f override.dev.yml build api discord trackers && docker-compose -f docker-compose.yml -f override.dev.yml up api discord trackers
