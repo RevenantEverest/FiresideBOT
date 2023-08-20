c51bd776fa70da73161e0bfef63929607b8d119f
Stefan Fischer <stefischer@mail.com>
2023-08-20 19:31:52 -0400

    Add trackers to deploy script

--- scripts/deployment/application_start.sh
+++ scripts/deployment/application_start.sh
@@ -4,5 +4,6 @@ cd /home/ubuntu/
 sudo cp .env FiresideBOT/
 sudo cp .env FiresideBOT/api
 sudo cp .env FiresideBOT/discord
+sudo cp .env FiresideBOT/trackers
 cd /home/ubuntu/FiresideBOT/
-docker-compose -f docker-compose.yml -f override.staging.yml build api discord && docker-compose -f docker-compose.yml -f override.staging.yml up -d api discord
+docker-compose -f docker-compose.yml -f override.staging.yml build api discord trackers && docker-compose -f docker-compose.yml -f override.staging.yml up -d api discord trackers
