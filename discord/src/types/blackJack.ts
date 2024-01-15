ea6932e21a1624972b478fdacac69fae48d9505c
Stefan Fischer <stefischer@mail.com>
2024-01-15 13:52:25 -0500

    Add blackjack types

--- discord/src/types/blackJack.ts
+++ discord/src/types/blackJack.ts
@@ -1,6 +1,23 @@
-import { Card } from './playingCards.js';
+import type { Message } from 'discord.js';
+import type { Card } from './playingCards.js';
 
 export interface BlackJackPlayerState {
     hand: Card[],
-    value: number
+    value: number,
+    title?: Message,
+    cardDisplay?: Message,
+    hasDoubledDown?: boolean,
+    hasSurrendered?: boolean,
+    hasNaturalBlackJack?: boolean
 };
+
+export interface BlackJackGameState {
+    player: BlackJackPlayerState,
+    dealer: BlackJackPlayerState,
+    deck: Card[],
+    wager: number,
+    outcomeState?: BlackJackOutcomeState,
+};
+
+export type BlackJackOutcomeState = "PLAYER_BUST" | "PUSH" | "DEALER_BUST" | "DEALER_WIN" | "PLAYER_WIN" | "PLAYER_SURRENDER"; 
+export type BlackJackCollectorReason = "DEALER_TURN" | "SURRENDER" | "PLAYER_BUST";
