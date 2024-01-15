0287f98dfc9d2adb073001a928f06d4fc638a4c8
Stefan Fischer <stefischer@mail.com>
2024-01-15 13:48:25 -0500

    Address comment concerns in guild currency routes

--- api/src/routes/economy/guildCurrencyRecordRoutes.ts
+++ api/src/routes/economy/guildCurrencyRecordRoutes.ts
@@ -1,6 +1,6 @@
 import express from 'express';
-import { guildCurrencyRecordsController } from '../../controllers/economy/index.js';
-import { extractPaginationParams, permissions, validation } from '../../middleware/index.js';
+import { guildCurrencyRecordsController } from '@@controllers/economy/index.js';
+import { extractPaginationParams, permissions, validation } from '@@middleware/index.js';
 
 const router = express.Router();
 
@@ -9,23 +9,10 @@ router.route("/")
 
 router.route("/guild_id/:guildId")
 .get(extractPaginationParams, validation.guildId, permissions.isGuildAdmin, guildCurrencyRecordsController.getByGuildId)
-.post(validation.guildId, permissions.isGuildMember, guildCurrencyRecordsController.create)
-// .put(validation.)
 
 router.route("/guild_id/:guildId/discord_id/:discordId")
 .get(validation.guildId, validation.discordId, permissions.isGuildMember, guildCurrencyRecordsController.getByGuildIdDiscordId)
 .put(validation.guildId, validation.discordId, permissions.isGuildMember, guildCurrencyRecordsController.update)
+.post(validation.guildId, validation.discordId, permissions.isGuildMember, guildCurrencyRecordsController.create)
 
 export default router;
-
-/*
-
-    Update Routes:
-    - User updates the currency amount from a command
-        - The user should not be able to hit this endpoint and update their own currency
-        - Maybe add an element into auth payload that tells api if the request came from discord
-    - Guild admin gives a user a certain amount of currency
-
-    ** Is there a situation where a user doesn't have a currency record and when trying to create one and update it it will attempt to save 2?
-
-*/
