5ef14c7bc87bd7242eb39971ac1b59c412fab2f7
Stefan Fischer <stefischer@mail.com>
2024-05-18 05:25:46 -0400

    Add jsdoc to authenticatedTestRoute function

--- api/src/__tests__/support/common/authenticatedRouteTest.ts
+++ api/src/__tests__/support/common/authenticatedRouteTest.ts
@@ -1,9 +1,15 @@
 import type { Application } from 'express';
 import supertest from 'supertest';
 
-type Method = "get" | "post" | "put" | "patch" | "delete";
+type HttpMethod = "get" | "post" | "put" | "patch" | "delete";
 
-export default function authenticatedRouteTest(app: Application, method: Method, endpoint: string) {
+/**
+ * Handles common test for an authenticated endpoint, if the user is not logged in
+ * @param app 
+ * @param method HTTP method 
+ * @param endpoint 
+ */
+export default function authenticatedRouteTest(app: Application, method: HttpMethod, endpoint: string) {
     describe("given the user is not logged in", () => {
         it("should return a 403 status", async () => {
             await supertest(app)[method](endpoint).expect(403);
