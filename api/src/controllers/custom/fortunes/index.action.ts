bc2f7b90e13b11c3a743a33e442c6b70dc8aa156
Stefan Fischer <stefischer@mail.com>
2024-05-18 05:26:13 -0400

    Use better error handling in fortunes index action

--- api/src/controllers/custom/fortunes/index.action.ts
+++ api/src/controllers/custom/fortunes/index.action.ts
@@ -1,25 +1,30 @@
-import { Request, Response, NextFunction } from 'express';
+import { 
+    Request, 
+    NextFunction, 
+    PaginatedResponse as PaginatedExpressResponse 
+} from '@@types/express.js';
 
-import { Fortune } from '../../../entities/index.js';
-import { AuthenticatedResponseLocals } from '../../../types/responseLocals.js';
+import { Fortune } from '@@entities/index.js';
 import { PaginatedResponse } from '../../../types/pagination.js';
 
 import { errors, entities, pagination } from '../../../utils/index.js';
 
-async function index(req: Request, res: Response, next: NextFunction) {
+async function index(req: Request, res: PaginatedExpressResponse, next: NextFunction) {
     
-    const { limit, offset } = res.locals as AuthenticatedResponseLocals;
+    const { limit, offset } = res.locals.pagination;
 
     const [fortunes, err] = await entities.indexAndCount<Fortune>(Fortune, {
         limit, offset
     });
 
-    if(err) {
-        return errors.sendResponse({ res, status: 500, err, message: "Error finding Fortunes" });
-    }
-
-    if(!fortunes) {
-        return errors.sendResponse({ res, status: 404, message: "No Fortunes found" });
+    if(err || !fortunes) {
+        return errors.sendEntitiesResponse({
+            res,
+            err,
+            message: "Error finding Fortunes",
+            entityReturn: fortunes,
+            missingEntityReturnMessage: "Unable to find Fortunes"
+        });
     }
 
     const response: PaginatedResponse<Fortune> = pagination.paginateResponse<Fortune>(req, res, fortunes);
