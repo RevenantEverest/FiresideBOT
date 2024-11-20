0751b3cab6e18c731f54acfc752a075b520d769e
Stefan Fischer <stefischer@mail.com>
2024-11-20 09:33:19 -0500

    Add clean_containers script

---
+++ scripts/clean_containers.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cd "$(dirname "$0")/.." || exit
+
+# Check if there are any running containers
+running_containers=$(docker ps -q -f status=running)
+
+if [ ! -z "$running_containers" ]; then
+    echo "Stopping running Docker containers..."
+    docker stop $running_containers
+else
+    echo "No docker containers running, skipping..."
+fi
+
+# Prune docker container and volumes
+docker system prune -a -f
+docker volume prune -f && docker volume ls -q -f dangling=true
+
+# Check for dangling images
+dangling_images=$(docker images -f "dangling=true" -q)
+
+# If there are dangling images, remove them
+if [[ ! -z "$dangling_images" ]]; then
+    echo "Found dangling images. Removing..."
+    docker rmi -f "$dangling_images"
+else
+    echo "No dangling images found, skipping..."
+fi
